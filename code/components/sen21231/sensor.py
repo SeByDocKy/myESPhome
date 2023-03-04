c9164b847143e217bbc5d0029293529e6875bcba
SeByDocKy <sebydocky@hotmail.com>
2023-03-04 13:54:37 +0100

    Add files via upload

---
+++ code/components/sen21231/sensor.py
@@ -0,0 +1,111 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c, sensor
+from esphome.const import (
+    CONF_ID,
+	CONF_ADDRESS,
+	STATE_CLASS_MEASUREMENT,
+	ICON_MOTION_SENSOR,
+	ICON_RULER,
+)
+
+CODEOWNERS                = ["@shreyaskarnik"]
+DEPENDENCIES              = ["i2c"]
+
+CONF_SEN21231_NFACES      = "nfaces"
+CONF_SEN21231_CONF0       = "conf0"
+CONF_SEN21231_X0          = "x0"
+CONF_SEN21231_Y0          = "y0"
+CONF_SEN21231_W0          = "w0"
+CONF_SEN21231_H0          = "h0"
+CONF_SEN21231_ISFACING0   = "isfacing0"
+CONF_SEN21231_ICON_BOX    = "mdi:face-man"
+CONF_SEN21231_ICON_PERCENT= "mdi:percent"
+CONF_SEN21231_ICON_TARGET = "mdi:target-variant"
+
+
+sen21231_ns               = cg.esphome_ns.namespace("sen21231")
+
+SEN21231Component         = sen21231_ns.class_(
+    "SEN21231Component", cg.PollingComponent, i2c.I2CDevice
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(SEN21231Component),
+			cv.Optional(CONF_ADDRESS): cv.i2c_address,
+            cv.Optional(CONF_SEN21231_NFACES): sensor.sensor_schema(
+                icon=CONF_SEN21231_ICON_BOX,
+                accuracy_decimals=0,
+				state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_SEN21231_CONF0): sensor.sensor_schema(
+                icon=CONF_SEN21231_ICON_PERCENT,
+				accuracy_decimals=0,
+				state_class=STATE_CLASS_MEASUREMENT,
+            ),
+			cv.Optional(CONF_SEN21231_X0): sensor.sensor_schema(
+			    icon=CONF_SEN21231_ICON_TARGET,
+                accuracy_decimals=0,
+				state_class=STATE_CLASS_MEASUREMENT,
+            ),
+			cv.Optional(CONF_SEN21231_Y0): sensor.sensor_schema(
+			    icon=CONF_SEN21231_ICON_TARGET,
+                accuracy_decimals=0,
+				state_class=STATE_CLASS_MEASUREMENT,
+            ),
+			cv.Optional(CONF_SEN21231_W0): sensor.sensor_schema(
+			    icon=CONF_SEN21231_ICON_TARGET,
+                accuracy_decimals=0,
+				state_class=STATE_CLASS_MEASUREMENT,
+            ),
+			cv.Optional(CONF_SEN21231_H0): sensor.sensor_schema(
+			    icon=CONF_SEN21231_ICON_TARGET,
+                accuracy_decimals=0,
+				state_class=STATE_CLASS_MEASUREMENT,
+            ),
+			cv.Optional(CONF_SEN21231_ISFACING0): sensor.sensor_schema(
+                accuracy_decimals=0,
+				state_class=STATE_CLASS_MEASUREMENT,
+				icon=CONF_SEN21231_ICON_BOX,
+            ),
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+    .extend(i2c.i2c_device_schema(0x62))
+#	cv.has_at_least_one_key(CONF_SEN21231_NFACES),
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
+	
+    if CONF_SEN21231_NFACES in config:
+        sens = await sensor.new_sensor(config[CONF_SEN21231_NFACES])
+        cg.add(var.set_nfaces_sensor(sens))
+	
+    if CONF_SEN21231_CONF0 in config:
+        sens = await sensor.new_sensor(config[CONF_SEN21231_CONF0])
+        cg.add(var.set_conf0_sensor(sens))
+		
+    if CONF_SEN21231_X0 in config:
+        sens = await sensor.new_sensor(config[CONF_SEN21231_X0])
+        cg.add(var.set_x0_sensor(sens))
+		
+    if CONF_SEN21231_Y0 in config:
+        sens = await sensor.new_sensor(config[CONF_SEN21231_Y0])
+        cg.add(var.set_y0_sensor(sens))
+		
+    if CONF_SEN21231_W0 in config:
+        sens = await sensor.new_sensor(config[CONF_SEN21231_W0])
+        cg.add(var.set_w0_sensor(sens))
+		
+    if CONF_SEN21231_H0 in config:
+        sens = await sensor.new_sensor(config[CONF_SEN21231_H0])
+        cg.add(var.set_h0_sensor(sens))
+		
+    if CONF_SEN21231_ISFACING0 in config:
+        sens = await sensor.new_sensor(config[CONF_SEN21231_ISFACING0])
+        cg.add(var.set_isfacing0_sensor(sens))
