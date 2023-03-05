7162c033446201274bc8229b596f5ea67182336a
SeByDocKy <sebydocky@hotmail.com>
2023-03-05 21:59:53 +0100

    Update sen21231.cpp

--- components/sen21231/sen21231.cpp
+++ components/sen21231/sen21231.cpp
@@ -32,7 +32,7 @@ void SEN21231Component::read_data_() {
   if (results.num_faces == 1) {
 	
 	if (this->conf0_sensor_ != nullptr) {
-	  this->conf0_sensor_->publish_state(int((float(results.faces[0].box_confidence))*(100.0/255.0)) );
+	  this->conf0_sensor_->publish_state(int(results.faces[0].box_confidence) );
 	}
 	ESP_LOGD(TAG, "conf0: %d", results.faces[0].box_confidence);
 	
