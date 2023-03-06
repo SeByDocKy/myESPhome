647bc5a470517beb1b11891d46e3e287f50f7cf2
SeByDocKy <sebydocky@hotmail.com>
2023-03-06 18:29:27 +0100

    Update sen21231.cpp

--- components/sen21231/sen21231.cpp
+++ components/sen21231/sen21231.cpp
@@ -9,7 +9,7 @@ static const char *const TAG = "sen21231";
 void SEN21231Component::setup() {
   ESP_LOGCONFIG(TAG, "Setting up SEN23231...");
   if (this->debug_='false'){
-    this->write_byte(PERSON_SENSOR_REG_DEBUG_MODE, 0x00)
+    this->write_byte(PERSON_SENSOR_REG_DEBUG_MODE, 0x00);
     ESP_LOGCONFIG(TAG, "debug mode (green led) is turned off");		  
   }
   else{
