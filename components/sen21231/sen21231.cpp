13a1ba73aaee60a40d645bef1ba4c75cdbdcb0be
SeByDocKy <sebydocky@hotmail.com>
2023-03-07 18:50:22 +0100

    Update sen21231.cpp

--- components/sen21231/sen21231.cpp
+++ components/sen21231/sen21231.cpp
@@ -151,7 +151,7 @@ void SEN21231Component::write_register(uint8_t reg, uint8_t bits, uint8_t start_
 	
 uint8_t SEN21231Component::read_register(uint8_t reg ) {
   uint8_t value;
-  if (this->read_byte(reg , &value, 1) != i2c::ERROR_OK) {
+  if (this->read_byte(reg , &value, true) != i2c::ERROR_OK) {
     ESP_LOGW(TAG, "Reading register failed!");
     return 0;
   }
