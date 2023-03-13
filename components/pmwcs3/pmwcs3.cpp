c5d9663abc472a1c7a3a5e6d09b57cc0e3f136e2
SeByDocKy <sebydocky@hotmail.com>
2023-03-13 19:43:30 +0100

    Update pmwcs3.cpp

--- components/pmwcs3/pmwcs3.cpp
+++ components/pmwcs3/pmwcs3.cpp
@@ -55,11 +55,12 @@ void PMWCS3Component::dump_config() {
 }
 
 void PMWCS3Component::read_data_() {
-  uint8_t data[2];
+  uint8_t data[8];
   uint16_t result;
   float e25, ec, temperature, vwc;
 		
-  this->read_bytes(PMWCS3_REG_READ_E25, (uint8_t *) &data, 2);
+//  this->read_bytes(PMWCS3_REG_READ_E25, (uint8_t *) &data, 2);
+  this->read_bytes(PMWCS3_REG_GET_DATA, (uint8_t *) &data, 8);	
   result = encode_uint16(data[1], data[0]);
    if (this->e25_sensor_ != nullptr) {
 	  e25 = result/100.0;
@@ -67,28 +68,31 @@ void PMWCS3Component::read_data_() {
 	  ESP_LOGD(TAG, "e25: data[0]=%d, data[1]=%d, result=%d", data[0] , data[1],result);
   }
   
-  this->read_bytes(PMWCS3_REG_READ_EC, (uint8_t *) &data, 2);
-  result = encode_uint16(data[1], data[0]);
+//  this->read_bytes(PMWCS3_REG_READ_EC, (uint8_t *) &data, 2);
+//  result = encode_uint16(data[1], data[0]);
+    result = encode_uint16(data[3], data[2]);	
   if (this->ec_sensor_ != nullptr) {
 	  ec = result/10.0;
 	  this->ec_sensor_->publish_state(ec);
-	  ESP_LOGD(TAG, "ec: data[0]=%d, data[1]=%d, result=%d", data[0] , data[1],result);
+	  ESP_LOGD(TAG, "ec: data[0]=%d, data[1]=%d, result=%d", data[2] , data[3],result);
   }
   
-  this->read_bytes(PMWCS3_REG_READ_TEMP, (uint8_t *) &data, 2);
-  result = encode_uint16(data[1], data[0]);	
+  //this->read_bytes(PMWCS3_REG_READ_TEMP, (uint8_t *) &data, 2);
+  //result = encode_uint16(data[1], data[0]);	
+  result = encode_uint16(data[5], data[4]);	
   if (this->temperature_sensor_ != nullptr) {
 	  temperature = result/100.0;
 	  this->temperature_sensor_->publish_state(temperature);
-	  ESP_LOGD(TAG, "temp: data[0]=%d, data[1]=%d, result=%d", data[0] , data[1],result); 
+	  ESP_LOGD(TAG, "temp: data[0]=%d, data[1]=%d, result=%d", data[4] , data[5],result); 
   }
 	
-  this->read_bytes(PMWCS3_REG_READ_VWC, (uint8_t *) &data, 2);
-  result = encode_uint16(data[1], data[0]);	
+  //this->read_bytes(PMWCS3_REG_READ_VWC, (uint8_t *) &data, 2);
+  //result = encode_uint16(data[1], data[0]);
+  result = encode_uint16(data[7], data[6]);	
   if (this->vwc_sensor_ != nullptr) {
 	  vwc = result/10.0;
 	  this->vwc_sensor_->publish_state(vwc);
-	  ESP_LOGD(TAG, "vwc: data[0]=%d, data[1]=%d, result=%d", data[0] , data[1],result);
+	  ESP_LOGD(TAG, "vwc: data[0]=%d, data[1]=%d, result=%d", data[6] , data[7],result);
   }
   
 }
