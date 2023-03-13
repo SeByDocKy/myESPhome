0572bd1b20f016056515353efbe9624cfcb0d964
SeByDocKy <sebydocky@hotmail.com>
2023-03-13 13:15:20 +0100

    Update pmwcs3.cpp

--- components/pmwcs3/pmwcs3.cpp
+++ components/pmwcs3/pmwcs3.cpp
@@ -65,7 +65,7 @@ void PMWCS3Component::dump_config() {
 }
 
 void PMWCS3Component::read_data_() {
-  uint16_t results[4];
+  uint8_t results[8];
   float e25, ec, temperature, vwc;
 // uint16_t results;	
 /*	
@@ -78,38 +78,45 @@ void PMWCS3Component::read_data_() {
 	
 	
 //  this->read_bytes_16(PMWCS3_REG_READ_E25, (uint16_t *) &results, 1);
-  this->read_bytes_16(PMWCS3_I2C_ADDRESS, (uint16_t *) &results, 4);
+//  this->read_bytes_16(PMWCS3_I2C_ADDRESS, (uint16_t *) &results, 4);
+  this->read_bytes(PMWCS3_I2C_ADDRESS, (uint8_t *) &results, 8);
   if (this->e25_sensor_ != nullptr) {
-	  e25 = results[0]/100.0;
+//	  e25 = results[0]/100.0;
 	  //e25 = results/100.0;
-	  this->e25_sensor_->publish_state(e25);
-	  ESP_LOGD(TAG, "e25 = %f", e25);
+//	  this->e25_sensor_->publish_state(e25);
+//	  ESP_LOGD(TAG, "e25 = %f", e25);
+	  ESP_LOGD(TAG, "e25 data[0]=%d, data[1]=%d", results[0] , results[1]);
   }
   
 //  this->read_bytes_16(PMWCS3_REG_READ_EC, (uint16_t *) &results, 1);
   if (this->ec_sensor_ != nullptr) {
-	  ec = results[1]/10.0;
+//	  ec = results[1]/10.0;
 	  //ec = results/10.0;
-	  this->ec_sensor_->publish_state(ec);
-	  ESP_LOGD(TAG, "ec = %f", ec);
+//	  this->ec_sensor_->publish_state(ec);
+//	  ESP_LOGD(TAG, "ec = %f", ec);
+	  ESP_LOGD(TAG, "ec data[2] = %d, data[3] = %d ", results[2] , results[3]);
   }
   
 //  this->read_bytes_16(PMWCS3_REG_READ_TEMP, (uint16_t *) &results, 1);
+/*	
   if (this->temperature_sensor_ != nullptr) {
 	  temperature = results[2]/100.0;
 	  //temperature = results/100.0;
 	  this->temperature_sensor_->publish_state(temperature);
 	  ESP_LOGD(TAG, "temperature = %f", temperature);
   }
-
+*/
 //  this->read_bytes_16(PMWCS3_REG_READ_VWC, (uint16_t *) &results, 1);
+/*	
+  
   if (this->vwc_sensor_ != nullptr) {
 	  vwc = results[3]/10.0;
 	  //vwc = results/10.0;
 	  this->vwc_sensor_->publish_state(vwc);
 	  ESP_LOGD(TAG, "vwc = %f", vwc);
   }
+ */ 
 }
 
-}  // namespace sen21231
+}  // namespace pmwcs3
 }  // namespace esphome
