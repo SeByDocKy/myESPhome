688a894d0980f109801bf818f0601ef419d09d4c
SeByDocKy <sebydocky@hotmail.com>
2023-03-14 12:07:50 +0100

    Update pmwcs3.cpp

--- components/pmwcs3/pmwcs3.cpp
+++ components/pmwcs3/pmwcs3.cpp
@@ -77,7 +77,7 @@ void PMWCS3Component::read_data_() {
 	  
 //    result      = encode_uint16(data[1], data[0]);
 //    e25         = result/100.0;
-    e25         = ((data[0] << 8) + data[1])/100.0;
+    e25         = ((data[1] << 8) + data[0])/100.0;
 
     this->e25_sensor_->publish_state(e25);
     ESP_LOGD(TAG, "e25: data[0]=%d, data[1]=%d, result=%d", data[0] , data[1] , result);
