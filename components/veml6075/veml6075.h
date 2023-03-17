479acbbf48c0d63dc574e29d9f3897861c5ee61b
SeByDocKy <sebydocky@hotmail.com>
2023-03-17 18:46:07 +0100

    Update veml6075.h

--- components/veml6075/veml6075.h
+++ components/veml6075/veml6075.h
@@ -146,6 +146,8 @@ class VEML6075Component : public PollingComponent, public i2c::I2CDevice {
 	
   uint16_t calc_visible_comp(void);
   uint16_t calc_ir_comp(void);
+  uint16_t calc_rawuva(void);
+  uint16_t calc_rawuva(void);
    
 protected:
 	
