a2df7252999a1b6a26fc7c00c9eb05220a0546d3
SeByDocKy <sebydocky@hotmail.com>
2023-03-18 20:38:30 +0100

    Update veml6075.cpp

--- components/veml6075/veml6075.cpp
+++ components/veml6075/veml6075.cpp
@@ -332,7 +332,6 @@ void VEML6075Component::read_data_() {
   uint16_t visible_compensation , ir_compensation;
   uint16_t rawuva , rawuvb;
   float uva , uvb , uvindex;
-
 	
   ESP_LOGD(TAG, "will read visible comp register");
 // /*
@@ -345,13 +344,14 @@ void VEML6075Component::read_data_() {
   ESP_LOGD(TAG , "VEML6075_REG_CONF: data[0]= %d, data[1]= %d" , data[0] , data[1]); 
 // */
 
+/*	
   visible_compensation  = calc_visible_comp();
   if (this->visible_comp_sensor_ != nullptr) {
 	  this->visible_comp_sensor_->publish_state(visible_compensation);
 	  ESP_LOGD(TAG, "visible_compensation: %d" , visible_compensation);
   }  
 
- /*	
+ 	
   ir_compensation       = calc_ir_comp();
   if (this->ir_comp_sensor_ != nullptr) {
 	  this->ir_comp_sensor_->publish_state(ir_compensation);
@@ -390,8 +390,6 @@ void VEML6075Component::read_data_() {
 */	
 }
 	
-
-
 
 
 }  // namespace veml6075
