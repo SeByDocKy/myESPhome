d25ba6eb5652eadb289a46d33cdb1652f52dd499
SeByDocKy <sebydocky@hotmail.com>
2023-03-19 17:44:24 +0100

    Update veml6075.cpp

--- components/veml6075/veml6075.cpp
+++ components/veml6075/veml6075.cpp
@@ -418,7 +418,7 @@ void VEML6075Component::read_data_() {
   ESP_LOGD(TAG, "will read visible comp register");
 	
 // /*  
-  data[0] = 10;
+  data[0] = 14;
   data[1] = 0;
   
   if (!this->write_bytes(VEML6075_REG_CONF, data , VEML6075_REG_SIZE )) {
