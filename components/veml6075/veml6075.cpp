0d4369f62b3fce438a060d1edf51373d9b033110
SeByDocKy <sebydocky@hotmail.com>
2023-03-21 18:23:07 +0100

    Update veml6075.cpp

--- components/veml6075/veml6075.cpp
+++ components/veml6075/veml6075.cpp
@@ -426,8 +426,8 @@ float VEML6075Component::calc_ir_comp(void){
 float VEML6075Component::calc_rawuva(void){
     uint8_t data[2] = {0, 0};
     float result;
-    if (!this->read_bytes(VEML6075_REG_UVA , data, VEML6075_REG_SIZE)){
-//    if (!this->read_bytes(VEML6075_REG_UVA , (uint8_t *) &data, VEML6075_REG_SIZE)){	    
+//    if (!this->read_bytes(VEML6075_REG_UVA , data, VEML6075_REG_SIZE)){
+    if (!this->read_bytes(VEML6075_REG_UVA , (uint8_t *) &data, VEML6075_REG_SIZE)){	    
        ESP_LOGE(TAG, "can't read VEML6075_REG_UVA  register");
        return NAN;
     }
