17f3cd817ac362e420a31cd2af03c9f120182c1b
SeByDocKy <sebydocky@hotmail.com>
2023-03-21 12:30:55 +0100

    Update veml6075.h

--- components/veml6075/veml6075.h
+++ components/veml6075/veml6075.h
@@ -149,6 +149,7 @@ class VEML6075Component : public PollingComponent, public i2c::I2CDevice {
   void highdynamic(veml6075_hd_t hd);
   void trigger(veml6075_uv_trig_t trig);
   void write_reg_00(bool stop , veml6075_af_t af , veml6075_uv_trig_t trig , veml6075_hd_t hd , veml6075_uv_it_t it);
+  uint8_t read_reg_00(void);
 	
   float calc_visible_comp(void);
   float calc_ir_comp(void);
