f0938a6dcec9fb965b96de1246d44d0cfa303443
SeByDocKy <sebydocky@hotmail.com>
2023-03-22 13:58:51 +0100

    Update veml6075.cpp

--- components/veml6075/veml6075.cpp
+++ components/veml6075/veml6075.cpp
@@ -31,7 +31,7 @@ void VEML6075Component::setup() {
 
   ESP_LOGCONFIG(TAG, "Setting up VEML6075...");
   
-/*	
+///*	
   identifychip(); // check if it's a genuine chip
  
   shutdown(true); // Shut down to change settings   VEML6075_REG_CONF(0x00) bit0-MSB/bit8 16 bit
@@ -49,7 +49,7 @@ void VEML6075Component::setup() {
   integrationtime(this->it_); // integration time  VEML6075_REG_CONF(0x00) bits 6-5-4-MSB/bits 14-13-12 16 bit
 
   shutdown(false); // Turn on chip after settings set
- */ 
+// */ 
 }
 
 void VEML6075Component::update() { 
