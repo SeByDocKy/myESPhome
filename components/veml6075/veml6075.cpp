bcdbafce7e2b746be62a5a1a455c58e2787e52bd
SeByDocKy <sebydocky@hotmail.com>
2023-03-22 16:08:40 +0100

    Update veml6075.cpp

--- components/veml6075/veml6075.cpp
+++ components/veml6075/veml6075.cpp
@@ -97,8 +97,8 @@ void VEML6075Component::shutdown(bool stop){
   data[1] = (uint8_t)((conf & 0xFF00) >> 8);
   ESP_LOGVV(TAG, "write after masking shutdown %d %d" , data[1] , data[0]);	
 
-  //this->write_register(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE , true);
-  this->write_bytes(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE);
+  this->write_register(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE , false);
+  //this->write_bytes(VEML6075_REG_CONF, data , VEML6075_REG_SIZE);
   ESP_LOGVV(TAG, "write_byte with VEML6075_REG_CONF successfull to turn on/off chip");
   
 }
@@ -122,8 +122,9 @@ void VEML6075Component::forcedmode(veml6075_af_t af){
   data[1] = (uint8_t)((conf & 0xFF00) >> 8); 	
   ESP_LOGVV(TAG, "Wil write VEML6075_REG_CONF after masking force mode with: %d %d" , data[1] , data[0]);
   
-  //this->write_register(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE , true);
-  this->write_bytes(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE);	
+  //this->write_register(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE , false);
+  this->write_register(VEML6075_REG_CONF, data , (size_t)VEML6075_REG_SIZE , false);	
+  //this->write_bytes(VEML6075_REG_CONF, data , VEML6075_REG_SIZE);	
   ESP_LOGVV(TAG, "write_bytes with VEML6075_REG_CONF successfull to set autoforce mode");
 }
 
@@ -144,8 +145,9 @@ void VEML6075Component::trigger(veml6075_uv_trig_t trig) {
   data[1] = (uint8_t)((conf & 0xFF00) >> 8);
   ESP_LOGVV(TAG, "Wil write VEML6075_REG_CONF after masking trigger  with: %d %d" , data[1] , data[0]);
 	
-  //this->write_register(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE , true);
-  this->write_bytes(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE);	
+  //this->write_register(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE , false);
+  this->write_register(VEML6075_REG_CONF, data , (size_t)VEML6075_REG_SIZE , false);
+  //this->write_bytes(VEML6075_REG_CONF, data , VEML6075_REG_SIZE);	
   ESP_LOGVV(TAG, "write_bytes with VEML6075_REG_CONF successfull to set trigger mode");
 }
 
@@ -171,8 +173,9 @@ void VEML6075Component::highdynamic(veml6075_hd_t hd){
   data[0] = (uint8_t)(conf & 0x00FF);
   data[1] = (uint8_t)((conf & 0xFF00) >> 8);
   ESP_LOGVV(TAG, "Wil write VEML6075_REG_CONF after masking high dynamic with: %d %d" , data[1] , data[0]);
-  //this->write_register(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE , true);
-  this->write_bytes(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE);	
+  //this->write_register(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE , fase);
+  this->write_register(VEML6075_REG_CONF, data , (size_t)VEML6075_REG_SIZE , false);	
+  //this->write_bytes(VEML6075_REG_CONF, data , VEML6075_REG_SIZE);	
   ESP_LOGVV(TAG, "write_byte with VEML6075_REG_CONF successfull to set high dynamic mode");
 }  
 		
@@ -192,8 +195,9 @@ void VEML6075Component::integrationtime(veml6075_uv_it_t it){
   data[0] = (uint8_t)(conf & 0x00FF);
   data[1] = (uint8_t)((conf & 0xFF00) >> 8);
   ESP_LOGVV(TAG, "Wil write VEML6075_REG_CONF after masking integration time  with: %d %d" , data[1] , data[0]);
-  //this->write_register(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE , true);
-  this->write_bytes(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE);	
+  //this->write_register(VEML6075_REG_CONF, (uint8_t *) &data , (size_t)VEML6075_REG_SIZE , false);
+  this->write_register(VEML6075_REG_CONF, data , (size_t)VEML6075_REG_SIZE , false);	
+  //this->write_bytes(VEML6075_REG_CONF, data , VEML6075_REG_SIZE);	
   ESP_LOGVV(TAG, "write_bytes with VEML6075_REG_CONF successfull to set integration time mode");
 	
   this->uva_responsivity_ = (float)VEML6075_UVA_RESPONSIVITY[(uint8_t)it];
