86c9f318d2df15dc85fbebaa530ef3fb402439fd
SeByDocKy <sebydocky@hotmail.com>
2023-03-23 17:31:19 +0100

    Update pmwcs3.cpp

--- components/pmwcs3/pmwcs3.cpp
+++ components/pmwcs3/pmwcs3.cpp
@@ -66,17 +66,17 @@ void PMWCS3Component::dump_config() {
 void PMWCS3Component::read_data_() {
   uint8_t data[8];
   float e25, ec, temperature, vwc;
-/*  	
+  	
  /////// Super important !!!! first activate reading PMWCS3_REG_READ_START (if not, return always the same values) ////
 	
-  //if (!this->write_bytes(PMWCS3_REG_READ_START, nullptr, 0)) {
-  if (!this->write(&PMWCS3_REG_READ_START, 0 , false)) {	  
+  if (!this->write_bytes(PMWCS3_REG_READ_START, nullptr, 0)) {
+  //if (!this->write(&PMWCS3_REG_READ_START, 0 , false)) {	  
       this->status_set_warning();
       ESP_LOGD(TAG, "Failed to write into REG_READ_START register !!!");
       return;
     }
   delay(100);	
-*/	
+	
   if (!this->read_bytes(PMWCS3_REG_GET_DATA, (uint8_t *) &data, 8)){
      ESP_LOGD(TAG, "Error reading PMWCS3_REG_GET_DATA registers");
      this->mark_failed();
