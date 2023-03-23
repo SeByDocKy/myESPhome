dbce76e80f902b571cfa4f18c33a676d54536475
SeByDocKy <sebydocky@hotmail.com>
2023-03-23 21:20:46 +0100

    Update veml6075.cpp

--- components/veml6075/veml6075.cpp
+++ components/veml6075/veml6075.cpp
@@ -246,7 +246,7 @@ uint16_t VEML6075Component::calc_rawuva(void){
     this->read_register(VEML6075_REG_UVA , (uint8_t *) &data, (size_t)VEML6075_REG_SIZE , false);
     result = (data[0] | (data[1] << 8)); 
     ESP_LOGVV(TAG , "calc_rawuva read successfully data[0]: %d, data[1]: %d, result: %d" , data[0] , data[1] , result);
-    this->read_register(VEML6075_REG_UVA , (uint8_t *) &data, (size_t)0 , true);	 // close connexion	
+    this->read_register(VEML6075_REG_UVA , (uint8_t *) &data, (size_t)1 , true);	 // dummy reading to close connexion	
     return result;	    
 }
 	
@@ -256,7 +256,7 @@ uint16_t VEML6075Component::calc_rawuvb(void){
     this->read_register(VEML6075_REG_UVB , (uint8_t *) &data, (size_t) VEML6075_REG_SIZE , false);
     result = (data[0] | (data[1] << 8)); 
     ESP_LOGVV(TAG , "calc_rawuvb read successfully data[0]: %d, data[1]: %d, result: %d" , data[0] , data[1] , result); 
-    this->read_register(VEML6075_REG_UVA , (uint8_t *) &data, (size_t)0 , true);	 // close connexion	
+    this->read_register(VEML6075_REG_UVA , (uint8_t *) &data, (size_t)1 , true);	 // close connexion	
     return result;	    
 }
 	
@@ -266,7 +266,7 @@ uint16_t VEML6075Component::calc_visible_comp(void){
     this->read_register(VEML6075_REG_VISIBLE_COMP, (uint8_t *) &data, (size_t) VEML6075_REG_SIZE , false);
     result = (data[0] | (data[1] << 8)); 
     ESP_LOGVV(TAG , "calc_visible_comp read successfully data[0]: %d, data[1]: %d, result: %d" , data[0] , data[1] , result);
-    this->read_register(VEML6075_REG_UVA , (uint8_t *) &data, (size_t)0 , true);	 // close connexion	
+    this->read_register(VEML6075_REG_UVA , (uint8_t *) &data, (size_t)1 , true);	 // close connexion	
     return result;
 }
 
@@ -276,7 +276,7 @@ uint16_t VEML6075Component::calc_ir_comp(void){
     this->read_register(VEML6075_REG_IR_COMP, (uint8_t *) &data, (size_t)VEML6075_REG_SIZE , false);
     result = (data[0] | (data[1] << 8)); 
     ESP_LOGVV(TAG , "calc_ir_comp read successfully data[0]: %d, data[1]: %d, result: %d" , data[0] , data[1] , result);
-    this->read_register(VEML6075_REG_UVA , (uint8_t *) &data, (size_t)0 , true);	 // close connexion	
+    this->read_register(VEML6075_REG_UVA , (uint8_t *) &data, (size_t)1 , true);	 // close connexion	
     return result;
 }
 			
