649433ba4faf314b0391adde45852e246963e62c
SeByDocKy <sebydocky@hotmail.com>
2023-05-12 08:07:40 +0200

    Add files via upload

--- components/statistics/statistics.cpp
+++ components/statistics/statistics.cpp
@@ -40,7 +40,8 @@ void STATISTICSComponent::loop() {
 void STATISTICSComponent::reset() {
   auto t = this->time_->now();
   this->last_day_of_year_ = t.day_of_year;
-  this->last_statistics_state_ = 0;
+  this->stats_ = 0;
+  this->last_statistics_state_
   this->last_n_ = 0;
   this->publish_state_and_save(0);
 }
