ff7ab081bfa8c896a2bd4359a3293668dbecf853
SeByDocKy <sebydocky@hotmail.com>
2023-11-21 17:18:59 +0100

    Fix 2

--- components/jsy193/jsy193.cpp
+++ components/jsy193/jsy193.cpp
@@ -7,13 +7,13 @@ namespace jsy193 {
 static const char *const TAG = "jsy193";
 
 static const uint8_t JSY193_CMD_READ_IN_REGISTERS = 0x04;
-static const uint8_t JSY193_CMD_START_REGISTER = 0x100;
+static const uint8_t JSY193_CMD_START_ADDRESS = 0x100;
 static const uint8_t JSY193_CMD_RESET_ENERGY = 0x42;
-static const uint8_t JSY193_REGISTER_COUNT = 20;  // 10x 16-bit registers
+static const uint8_t JSY193_REGISTER_COUNT = 20;  // 20x 16-bit registers
 
 void JSY193::on_modbus_data(const std::vector<uint8_t> &data) {
   if (data.size() < JSY193_REGISTER_COUNT*2) {
-    ESP_LOGW(TAG, "Invalid size for JSY193!");
+    ESP_LOGW(TAG, "Invalid size for JSY193 data!");
     return;
   }
 
@@ -38,12 +38,11 @@ void JSY193::on_modbus_data(const std::vector<uint8_t> &data) {
   float pos_energy1 = static_cast<float>(jsy193_get_32bit(8))/1000.0f;
   float neg_energy1 = static_cast<float>(jsy193_get_32bit(12))/1000.0f;
 
-  uint16_t raw_power_factor1 = jsy193_get_16bit(16);
+  uint16_t raw_power_factor = jsy193_get_16bit(16);
   float power_factor = raw_power_factor / 1000.0f;
 
   uint16_t raw_frequency = jsy193_get_16bit(18);
   float frequency1 = raw_frequency / 100.0f;
-  
   
   
   uint16_t raw_voltage = jsy193_get_16bit(20);
@@ -52,14 +51,13 @@ void JSY193::on_modbus_data(const std::vector<uint8_t> &data) {
   uint16_t raw_sign = jsy193_get_16bit(26); //0 for positive, 1 for negative 
   
   uint16_t raw_current = jsy193_get_16bit(22);  
-  float current2 = ((1 - raw_sign)*raw_current - raw_sign*raw_current)/100.0f;  // max 4294967.295 A
+  float current2 = ((1 - raw_sign)*raw_current - raw_sign*raw_current)/100.0f;  // max 
   
   uint16_t raw_power   = jsy193_get_16bit(24);
-  float power2 = (1 - raw_sign)*raw_power - raw_sign*raw_power;  // max 4294967.295 A
+  float power2 = (1 - raw_sign)*raw_power - raw_sign*raw_power;  // max
     
-
-  float pos_energy2 = static_cast<float>(jsy193_get_32bit(28));
-  float neg_energy2 = static_cast<float>(jsy193_get_32bit(32));
+  float pos_energy2 = static_cast<float>(jsy193_get_32bit(28))/1000.0f;
+  float neg_energy2 = static_cast<float>(jsy193_get_32bit(32))/1000.0f;
 
   uint16_t raw_power_factor = jsy193_get_16bit(36);
   float power_factor2 = raw_power_factor / 1000.0f;
@@ -68,7 +66,6 @@ void JSY193::on_modbus_data(const std::vector<uint8_t> &data) {
   float frequency2 = raw_frequency / 100.0f;
 
   
-
   ESP_LOGD(TAG, "JSY193: V1=%.1f V, I1=%.3f A, P1=%.1f W, E1+=%.1f kWh , E1-=%.1f kWh, F1=%.1f Hz, PF1=%.2f , V2=%.1f V, I2=%.3f A, P2=%.1f W, E2+=%.1f kWh , E2-=%.1f kWh, F2=%.1f Hz, PF2=%.2f", voltage1, current1, power1,
            pos_energy1, neg_energy1, frequency1, power_factor1, voltage2, current2, power2, pos_energy2, neg_energy2, frequency2, power_factor2);
   if (this->voltage1_sensor_ != nullptr)
@@ -102,8 +99,8 @@ void JSY193::on_modbus_data(const std::vector<uint8_t> &data) {
     this->power_factor2_sensor_->publish_state(power_factor2);
 }
 
-void PZEMAC::update() { this->send(JSY193_CMD_READ_IN_REGISTERS, JSY193_CMD_START_REGISTER , JSY193_REGISTER_COUNT); }
-void PZEMAC::dump_config() {
+void JSY193::update() { this->send(JSY193_CMD_READ_IN_REGISTERS, JSY193_CMD_START_ADDRESS , JSY193_REGISTER_COUNT); }
+void JSY193::dump_config() {
   ESP_LOGCONFIG(TAG, "JSY193:");
   ESP_LOGCONFIG(TAG, "  Address: 0x%02X", this->address_);
   LOG_SENSOR("", "Voltage1", this->voltage1_sensor_);
