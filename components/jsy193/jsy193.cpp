24959dd2c0430708888f74266fbe68b49ec774af
SeByDocKy <sebydocky@hotmail.com>
2023-11-25 10:59:29 +0100

    Add files via upload

--- components/jsy193/jsy193.cpp
+++ components/jsy193/jsy193.cpp
@@ -18,6 +18,7 @@ void JSY193::setup() {
   ESP_LOGCONFIG(TAG, "Setting up JSY193...");
   this->read_data_ = false;
   // this->send(JSY193_CMD_READ_IN_REGISTERS, JSY193_REGISTER_SETTINGS_START , 1);
+  std::vector<uint8_t> cmd;
   cmd.push_back(0x00); // this->address_
   cmd.push_back(JSY193_CMD_READ_IN_REGISTERS);
   cmd.push_back(0x00);  
