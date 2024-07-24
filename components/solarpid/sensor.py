dd340f1a1f8beef5ce674fc639e50a4ff457f2e4
SeByDocKy <sebydocky@hotmail.com>
2024-07-24 10:59:05 +0200

    Update sensor.py

--- components/solarpid/sensor.py
+++ components/solarpid/sensor.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import automation
+from esphome.automation import maybe_simple_id
 from esphome.components import sensor, switch, output, time
 from esphome.const import (
     CONF_ID,
