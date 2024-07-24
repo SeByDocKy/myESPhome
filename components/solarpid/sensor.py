472151f939200c4c0b0bb000e531c867fef287ef
SeByDocKy <sebydocky@hotmail.com>
2024-07-24 10:17:03 +0200

    Update sensor.py

--- components/solarpid/sensor.py
+++ components/solarpid/sensor.py
@@ -36,7 +36,7 @@ DEPENDENCIES = ["time"]
 ### Sensor component ####
 solarpid_ns = cg.esphome_ns.namespace("solarpid")
 SOLARPID = solarpid_ns.class_(
-    "SOLARPID", sensor.Sensor, cg.Component
+    "SOLARPID", sensor.Sensor, binary_sensor.Sensor, cg.Component
 )
 
 ### Actions ###
