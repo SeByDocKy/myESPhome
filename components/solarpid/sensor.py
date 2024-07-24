b69f62834d91c4e4d04d01d6fd84481f9b4c1893
SeByDocKy <sebydocky@hotmail.com>
2024-07-24 11:54:51 +0200

    Update sensor.py

--- components/solarpid/sensor.py
+++ components/solarpid/sensor.py
@@ -44,13 +44,13 @@ SOLARPID = solarpid_ns.class_(
 SetPointAction = solarpid_ns.class_('SetPointAction', automation.Action)
 
 SetKpAction = solarpid_ns.class_('SetKpAction', automation.Action)
-# SetKiAction = solarpid_ns.class_('SetKiAction', automation.Action)
-# SetKdAction = solarpid_ns.class_('SetKdAction', automation.Action)
+SetKiAction = solarpid_ns.class_('SetKiAction', automation.Action)
+SetKdAction = solarpid_ns.class_('SetKdAction', automation.Action)
 
-# SetOutputMinAction = solarpid_ns.class_('SetOutputMinAction', automation.Action)
-# SetOutputMaxAction = solarpid_ns.class_('SetOutputMaxAction', automation.Action)
+SetOutputMinAction = solarpid_ns.class_('SetOutputMinAction', automation.Action)
+SetOutputMaxAction = solarpid_ns.class_('SetOutputMaxAction', automation.Action)
 
-# SetPwmRestartAction = solarpid_ns.class_('SetPwmRestartAction', automation.Action)
+SetPwmRestartAction = solarpid_ns.class_('SetPwmRestartAction', automation.Action)
 
 # PidUpdateAction = solarpid_ns.class_('PidUpdateAction', automation.Action)
 
@@ -165,94 +165,94 @@ async def set_kp_to_code(config, action_id, template_arg, args):
     return var
 
 
-# @automation.register_action(
-#     "solarpid.set_ki",
-#     SetKiAction,
-#     maybe_simple_id(
-#         {
-#             cv.Required(CONF_ID): cv.use_id(SOLARPID),
-# 	    cv.Required(CONF_NEW_KI): cv.templatable(cv.float_),
-#         }
-#     ),
-# ) 
+@automation.register_action(
+    "solarpid.set_ki",
+    SetKiAction,
+    maybe_simple_id(
+        {
+            cv.Required(CONF_ID): cv.use_id(SOLARPID),
+	    cv.Required(CONF_NEW_KI): cv.templatable(cv.float_),
+        }
+    ),
+) 
 
-# async def set_ki_to_code(config, action_id, template_arg, args):
-#     parent = await cg.get_variable(config[CONF_ID])
-#     var = cg.new_Pvariable(action_id, template_arg , parent)
-#     template_new_ki = await cg.templatable(config[CONF_NEW_KI], args, float_) 
-#     cg.add(var.set_new_ki(template_new_ki))	
-#     return var
-
-
-# @automation.register_action(
-#     "solarpid.set_kd",
-#     SetKdAction,
-#     maybe_simple_id(
-#         {
-#             cv.Required(CONF_ID): cv.use_id(SOLARPID),
-# 	    cv.Required(CONF_NEW_KD): cv.templatable(cv.float_),
-#         }
-#     ),
-# )
-# async def set_kd_to_code(config, action_id, template_arg, args):
-#     parent = await cg.get_variable(config[CONF_ID])
-#     var = cg.new_Pvariable(action_id, template_arg , parent)
-#     template_new_kd = await cg.templatable(config[CONF_NEW_KD], args, float_) 
-#     cg.add(var.set_new_kd(template_new_kd))	
-#     return var
+async def set_ki_to_code(config, action_id, template_arg, args):
+    parent = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg , parent)
+    template_new_ki = await cg.templatable(config[CONF_NEW_KI], args, float_) 
+    cg.add(var.set_new_ki(template_new_ki))	
+    return var
 
 
-# @automation.register_action(
-#     "solarpid.set_output_min",
-#     SetOutputMinAction,
-#     maybe_simple_id(
-#         {
-#             cv.Required(CONF_ID): cv.use_id(SOLARPID),
-# 	    cv.Required(CONF_NEW_OUTPUT_MIN): cv.templatable(cv.float_range(min=0.0, max=1.0)),
-#         }
-#     ),
-# )
-# async def set_output_min_to_code(config, action_id, template_arg, args):
-#     parent = await cg.get_variable(config[CONF_ID])
-#     var = cg.new_Pvariable(action_id, template_arg , parent)
-#     template_new_output_min = await cg.templatable(config[CONF_NEW_OUTPUT_MIN], args, float_) 
-#     cg.add(var.set_new_output_min(template_new_output_min))	
-#     return var
+@automation.register_action(
+    "solarpid.set_kd",
+    SetKdAction,
+    maybe_simple_id(
+        {
+            cv.Required(CONF_ID): cv.use_id(SOLARPID),
+	    cv.Required(CONF_NEW_KD): cv.templatable(cv.float_),
+        }
+    ),
+)
+async def set_kd_to_code(config, action_id, template_arg, args):
+    parent = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg , parent)
+    template_new_kd = await cg.templatable(config[CONF_NEW_KD], args, float_) 
+    cg.add(var.set_new_kd(template_new_kd))	
+    return var
 
 
-# @automation.register_action(
-#     "solarpid.set_output_max",
-#     SetOutputMaxAction,
-#     maybe_simple_id(
-#         {
-#             cv.Required(CONF_ID): cv.use_id(SOLARPID),
-# 	    cv.Required(CONF_NEW_OUTPUT_MAX): cv.templatable(cv.float_range(min=0.0, max=1.0)),
-#         }
-#     ),
-# )
-# async def set_output_max_to_code(config, action_id, template_arg, args):
-#     parent = await cg.get_variable(config[CONF_ID])
-#     var = cg.new_Pvariable(action_id, template_arg , parent)
-#     template_new_output_max = await cg.templatable(config[CONF_NEW_OUTPUT_MAX], args, float_) 
-#     cg.add(var.set_new_output_max(template_new_output_max))	
-#     return var
+@automation.register_action(
+    "solarpid.set_output_min",
+    SetOutputMinAction,
+    maybe_simple_id(
+        {
+            cv.Required(CONF_ID): cv.use_id(SOLARPID),
+	    cv.Required(CONF_NEW_OUTPUT_MIN): cv.templatable(cv.float_range(min=0.0, max=1.0)),
+        }
+    ),
+)
+async def set_output_min_to_code(config, action_id, template_arg, args):
+    parent = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg , parent)
+    template_new_output_min = await cg.templatable(config[CONF_NEW_OUTPUT_MIN], args, float_) 
+    cg.add(var.set_new_output_min(template_new_output_min))	
+    return var
 
-# @automation.register_action(
-#     "solarpid.set_pwm_restart",
-#     SetPwmRestartAction,
-#     maybe_simple_id(
-#         {
-#             cv.Required(CONF_ID): cv.use_id(SOLARPID),
-# 	    cv.Required(CONF_NEW_PWM_RESTART): cv.templatable(cv.float_range(min=0.0, max=1.0)),
-#         }
-#     ),
-# )
-# async def set_pwm_restart_to_code(config, action_id, template_arg, args):
-#     parent = await cg.get_variable(config[CONF_ID])
-#     var = cg.new_Pvariable(action_id, template_arg , parent)
-#     template_new_pwm_restart = await cg.templatable(config[CONF_PWM_RESTART], args, float_) 
-#     cg.add(var.set_new_pwm_restart(template_new_pwm_restart))	
-#     return var
+
+@automation.register_action(
+    "solarpid.set_output_max",
+    SetOutputMaxAction,
+    maybe_simple_id(
+        {
+            cv.Required(CONF_ID): cv.use_id(SOLARPID),
+	    cv.Required(CONF_NEW_OUTPUT_MAX): cv.templatable(cv.float_range(min=0.0, max=1.0)),
+        }
+    ),
+)
+async def set_output_max_to_code(config, action_id, template_arg, args):
+    parent = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg , parent)
+    template_new_output_max = await cg.templatable(config[CONF_NEW_OUTPUT_MAX], args, float_) 
+    cg.add(var.set_new_output_max(template_new_output_max))	
+    return var
+
+@automation.register_action(
+    "solarpid.set_pwm_restart",
+    SetPwmRestartAction,
+    maybe_simple_id(
+        {
+            cv.Required(CONF_ID): cv.use_id(SOLARPID),
+	    cv.Required(CONF_NEW_PWM_RESTART): cv.templatable(cv.float_range(min=0.0, max=1.0)),
+        }
+    ),
+)
+async def set_pwm_restart_to_code(config, action_id, template_arg, args):
+    parent = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg , parent)
+    template_new_pwm_restart = await cg.templatable(config[CONF_PWM_RESTART], args, float_) 
+    cg.add(var.set_new_pwm_restart(template_new_pwm_restart))	
+    return var
 
 # @automation.register_action(
 #     "solarpid.pid_update",
