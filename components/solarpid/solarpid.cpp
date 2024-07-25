96a10fd7628c8ca10d6526caff55fc7a4cd06562
SeByDocKy <sebydocky@hotmail.com>
2024-07-25 08:40:51 +0200

    Update solarpid.cpp

--- components/solarpid/solarpid.cpp
+++ components/solarpid/solarpid.cpp
@@ -53,7 +53,7 @@ void SOLARPID::pid_update() {
     this->integral_ += error * dt;
     this->derivative_ = (error - this->previous_error_) / dt;
     this->previous_error_ = error;
-    if( (this->current_power_ != NaN) && (this->current_power_ < 2.0f) &&  (this->previous_pwm_output_ > this->pwm_restart_) ) {
+    if( (!std::isnan(this->current_power_)) && (this->current_power_ < 2.0f) &&  (this->previous_pwm_output_ > this->pwm_restart_) ) {
       pwm_output = this->pwm_restart_;
       ESP_LOGI(TAG, "restart branch");
     }
