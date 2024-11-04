8691854061e9238317ac518a93d1c63525f96ede
SeByDocKy <sebydocky@hotmail.com>
2024-11-04 15:37:18 +0100

    Update gp8403.h

--- components/gp8403/gp8403.h
+++ components/gp8403/gp8403.h
@@ -16,12 +16,14 @@ class GP8403 : public Component, public i2c::I2CDevice {
   void setup() override;
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
+  bool get_gp8413 {return gp8413_ ;}
   void set_gp8413(bool gp8413) { gp8413_ = gp8413; }
   void set_voltage(gp8403::GP8403Voltage voltage) { this->voltage_ = voltage; }
-  bool gp8413_; 
+   
 
  protected:
   GP8403Voltage voltage_;
+  bool gp8413_;
   
 };
 
