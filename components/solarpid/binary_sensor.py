ad4087a2b69074ac8af333c1f36814a5f436e641
SeByDocKy <sebydocky@hotmail.com>
2024-11-09 23:22:36 +0100

    Update binary_sensor.py

--- components/solarpid/binary_sensor.py
+++ components/solarpid/binary_sensor.py
@@ -22,3 +22,13 @@ CONFIG_SCHEMA = cv.Schema(
         ),
     }
 )
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    
+    bin_sens = await cg.get_variable(config[CONF_THERMOSTAT_CUT])
+    cg.add(var.set_thermostat_cut_binary_sensor(bin_sens))
+
+
+
