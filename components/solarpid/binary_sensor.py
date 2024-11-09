5c3577f417afea1f8c2896146595c99cf4195f8d
SeByDocKy <sebydocky@hotmail.com>
2024-11-09 21:29:48 +0100

    Update binary_sensor.py

---
+++ components/solarpid/binary_sensor.py
@@ -0,0 +1,8 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import binary_sensor
+from esphome.const import (
+    CONF_ID,
+    DEVICE_CLASS_COLD,
+    DEVICE_CLASS_PROBLEM,
+)
