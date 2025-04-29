fa0f75272ee20f55943a5b3f1c6f23fd8fc7519b
SeByDocKy <sebydocky@hotmail.com>
2025-04-29 22:34:31 +0200

    Add files via upload

--- components/offsr/offsr.cpp
+++ components/offsr/offsr.cpp
@@ -9,6 +9,7 @@ static const char *const TAG = "offsr";
 // /*
 void OFFSRComponent::setup() { 
   ESP_LOGCONFIG(TAG, "Setting up OFFSRComponent...");
+  
   if (this->battery_current_sensor_ != nullptr) {
     this->battery_current_sensor_->add_on_state_callback([this](float state) {
       this->current_battery_current_ = state;
@@ -21,18 +22,21 @@ void OFFSRComponent::setup() {
     });
     this->current_battery_voltage_ = this->battery_voltage_sensor_->state;
   }
-  if (this->device_output_ != nullptr) {
-    this->device_output_->add_on_state_callback([this](float state) {
-      this->current_output_ = state;
-    });
-    this->current_output_ = this->device_output_->state;
-  }
+  
   if (this->power_sensor_ != nullptr) {
     this->power_sensor_->add_on_state_callback([this](float state) {
       this->current_power_ = state;
     });
     this->current_power_ = this->power_sensor_->state;
   }
+/*  
+  if (this->device_output_ != nullptr) {
+    this->device_output_->add_on_state_callback([this](float state) {
+      this->current_output_ = state;
+    });
+    this->current_output_ = this->device_output_->state;
+  }
+*/  
 }
 // */
 
