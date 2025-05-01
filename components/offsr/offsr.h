cb6eb3245c032d5c95147c08d50fe0d7804a64eb
SeByDocKy <sebydocky@hotmail.com>
2025-05-01 19:11:53 +0200

    Add files via upload

--- components/offsr/offsr.h
+++ components/offsr/offsr.h
@@ -107,12 +107,12 @@ void set_output(float output);
   // binary_sensor::BinarySensor *current_thermostat_cut_{nullptr};
 #endif  
 
-#ifdef USE_SENSOR
+// #ifdef USE_SENSOR
   float current_error_ = 0.0f;
   float current_output_ = 0.0f;
 //  sensor::Sensor *current_error_{nullptr};
 //  sensor::Sensor *current_output_{nullptr};
-#endif   
+// #endif   
 };
 	
 	
