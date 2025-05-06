9251782e4da49a6726142ef19a04512b578bf5c7
SeByDocKy <sebydocky@hotmail.com>
2025-05-06 22:52:43 +0200

    Add files via upload

--- components/offsr/sensor/target_sensor.h
+++ components/offsr/sensor/target_sensor.h
@@ -22,7 +22,7 @@ class TargetSensor : public Component, sensor::Sensor, public Parented<OFFSRComp
  public:
   void dump_config() override;
   void update(); //override
-  void set_target_sensor(sensor::Sensor *output_sensor) { this->target_sensor_ = target_sensor; };
+  void set_target_sensor(sensor::Sensor *target_sensor) { this->target_sensor_ = target_sensor; };
 
  protected:
   sensor::Sensor *target_sensor_{nullptr};
