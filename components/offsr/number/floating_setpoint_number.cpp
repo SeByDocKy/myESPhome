4306abfc3088c0735fee5ba8bfce2c61c77338ee
SeByDocKy <sebydocky@hotmail.com>
2025-05-06 18:01:38 +0200

    Add files via upload

---
+++ components/offsr/number/floating_setpoint_number.cpp
@@ -0,0 +1,12 @@
+#include "charging_setpoint_number.h"
+
+namespace esphome {
+namespace offsr {
+
+void FloatingSetpointNumber::control(float value) {
+  this->publish_state(value);
+  this->parent_->set_floating_setpoint(value);
+}
+
+}  // namespace offsr
+}  // namespace esphome
