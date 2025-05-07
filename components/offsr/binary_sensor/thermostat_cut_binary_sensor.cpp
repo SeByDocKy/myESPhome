e275f3e1ad928d821b59eb88819a89e477d0b73d
SeByDocKy <sebydocky@hotmail.com>
2025-05-07 21:27:57 +0200

    Add files via upload

--- components/offsr/binary_sensor/thermostat_cut_binary_sensor.cpp
+++ components/offsr/binary_sensor/thermostat_cut_binary_sensor.cpp
@@ -8,7 +8,8 @@ namespace offsr {
 static const char *const TAG = "offsr.binary_sensor";
 
 void ThermostatcutBinarySensor::setup() {
-  this->parent_->add_on_pid_computed_callback([this]() { this->parent_->get_thermostat_cut(); });
+  this->parent_->add_on_pid_computed_callback([this]() { this->publish_state(this->parent_->get_thermostat_cut()); });	
+  // this->parent_->add_on_pid_computed_callback([this]() { this->parent_->get_thermostat_cut(); });
 }
 
 // void ThermostatcutBinarySensor::update() { this->publish_state(this->parent_->get_thermostat_cut()); }
