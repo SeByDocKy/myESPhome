963336973a5e71f26146dbf21e59fb641777d2a3
SeByDocKy <sebydocky@hotmail.com>
2025-05-09 19:29:32 +0200

    Add files via upload

--- components/offsr/offsr.cpp
+++ components/offsr/offsr.cpp
@@ -137,7 +137,7 @@ void OFFSRComponent::pid_update() {
 #endif  
 
     if (!std::isnan(this->current_battery_voltage_)){
-	  ESP_LOGV(TAG, "starting battery voltage = %2.2f" , this->current_starting_battery_voltage_);	
+	  ESP_LOGV(TAG, "battery_voltage = %2.2f, starting battery voltage = %2.2f" , this->current_battery_voltage_, this->current_starting_battery_voltage_);	
       if (this->current_battery_voltage_ < this->current_starting_battery_voltage_){
         output_ = 0.0f;
       }
