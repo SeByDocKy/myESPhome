e9412f12f4a6b70051b40d2af592501059fb4cf2
SeByDocKy <sebydocky@hotmail.com>
2025-05-09 22:47:26 +0200

    Add files via upload

--- components/offsr/binary_sensor/__init__.py
+++ components/offsr/binary_sensor/__init__.py
@@ -26,4 +26,7 @@ async def to_code(config):
     offsr_component = await cg.get_variable(config[CONF_OFFSR_ID])
     if thermostat_cut_config := config.get(CONF_THERMOSTAT_CUT):
         bsens = await binary_sensor.new_binary_sensor(thermostat_cut_config)
-        cg.add(offsr_component.set_thermostat_cut_binary_sensor(bsens))
+        await cg.register_component(bsens, config)
+        cg.add(sens.set_parent(offsr_component))
+        # cg.add(offsr_component.set_thermostat_cut_binary_sensor(bsens))
+        
