43c54b51346ae7f99b004ee07b7722643c1ac07e
SeByDocKy <sebydocky@hotmail.com>
2025-05-11 11:26:01 +0200

    Add files via upload

--- components/offsr/offsr.h
+++ components/offsr/offsr.h
@@ -16,8 +16,8 @@
 
 #include "esphome/core/automation.h"
 #include "esphome/core/helpers.h"
-#include "esphome/components/output/float_output.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/output/float_output.h"
 #include "esphome/components/time/real_time_clock.h"
 
 
@@ -26,10 +26,10 @@ namespace offsr {
 	
 class OFFSRComponent : public Component{
 
-/* #ifdef USE_SWITCH
+#ifdef USE_SWITCH
 SUB_SWITCH(activation)
 SUB_SWITCH(manual_override)
-#endif */
+#endif
 
 #ifdef USE_NUMBER
 SUB_NUMBER(manual_level)
@@ -51,56 +51,107 @@ SUB_NUMBER(output_max)
 SUB_NUMBER(output_restart)
 #endif
 
+
+#ifdef USE_BINARY_SENSOR
+SUB_BINARY_SENSOR(thermostat_cut)
+#endif
+
+#ifdef USE_SENSOR
+SUB_SENSOR(error)
+SUB_SENSOR(output)
+SUB_SENSOR(target)
+#endif
+
  public:
+  
+  // OFFSRComponent() = default;
   
   void setup() override;
   void dump_config() override;
   
-  void set_pid_mode(bool pid_mode) {this->current_pid_mode_ = pid_mode; }
-  void set_battery_current_sensor(sensor::Sensor *battery_current_sensor) {this->battery_current_sensor_ = battery_current_sensor; }
-  void set_battery_voltage_sensor(sensor::Sensor *battery_voltage_sensor) {this->battery_voltage_sensor_ = battery_voltage_sensor; }
-  void set_power_sensor(sensor::Sensor *power_sensor) {this->power_sensor_ = power_sensor; }
-  void set_device_output(output::FloatOutput *device_output) {this->device_output_ = device_output; }
+  void set_pid_mode(bool pid_mode) { current_pid_mode_ = pid_mode; }
+  void set_battery_current_sensor(sensor::Sensor *battery_current_sensor) { battery_current_sensor_ = battery_current_sensor; }
+  void set_battery_voltage_sensor(sensor::Sensor *battery_voltage_sensor) { battery_voltage_sensor_ = battery_voltage_sensor; }
+  void set_power_sensor(sensor::Sensor *power_sensor) { power_sensor_ = power_sensor; }
+  void set_device_output(output::FloatOutput *device_output) { device_output_ = device_output; }
   void pid_update();
   
   void add_on_pid_computed_callback(std::function<void()> &&callback) {
     pid_computed_callback_.add(std::move(callback));
   }
   
-// #ifdef USE_SWITCH 
-  void set_activation(bool enable) {this->current_activation_ = enable;}
-  void set_manual_override(bool enable) {this->current_manual_override_ = enable;}
-// #endif
+/*   void add_on_output_callback(std::function<void()> &&callback) {
+    output_callback_.add(std::move(callback));
+  }
+  void add_on_error_callback(std::function<void()> &&callback) {
+    error_callback_.add(std::move(callback));
+  }
+  void add_on_target_callback(std::function<void()> &&callback) {
+    target_callback_.add(std::move(callback));
+  }
+  void add_on_thermostat_cut_callback(std::function<void()> &&callback) {
+    thermostat_cut_callback_.add(std::move(callback));
+  } */
+  
+#ifdef USE_SWITCH
+  
+  void set_activation(bool enable) {current_activation_ = enable;}
+  void set_manual_override(bool enable) {current_manual_override_ = enable;}
+ 
+/*   void set_activation(bool enable);
+  void set_manual_override(bool enable); */
+#endif
 
 #ifdef USE_NUMBER
 
-  void set_manual_level(float value) {this->current_manual_level_ = value; }
-  
-  void set_charging_setpoint(float value) {this->current_charging_setpoint_ = value;}
-  void set_absorbing_setpoint(float value) {this->current_absorbing_setpoint_ = value;}
-  void set_floating_setpoint(float value) {this->current_floating_setpoint_ = value;}
 
-  void set_starting_battery_voltage(float value) {this->current_starting_battery_voltage_ = value;}
-  void set_charged_battery_voltage(float value) {this->current_charged_battery_voltage_ = value;}
-  void set_discharged_battery_voltage(float value) {this->current_discharged_battery_voltage_ = value;}
+  void set_manual_level(float value) {current_manual_level_ = value; }
   
-  void set_kp(float value) {this->current_kp_ = value;}
-  void set_ki(float value) {this->current_ki_ = value;}
-  void set_kd(float value) {this->current_kd_ = value;}
+  void set_charging_setpoint(float value) {current_charging_setpoint_ = value;}
+  void set_absorbing_setpoint(float value) {current_absorbing_setpoint_ = value;}
+  void set_floating_setpoint(float value) {current_floating_setpoint_ = value;}
+
+  void set_starting_battery_voltage(float value) {current_starting_battery_voltage_ = value;}
+  void set_charged_battery_voltage(float value) {current_charged_battery_voltage_ = value;}
+  void set_discharged_battery_voltage(float value) {current_discharged_battery_voltage_ = value;}
   
-  void set_output_min(float value) {this->current_output_min_ = value;}
-  void set_output_max(float value) {this->current_output_max_ = value;}
-  void set_output_restart(float value) {this->current_output_restart_ = value;}
+  void set_kp(float value) {current_kp_ = value;}
+  void set_ki(float value) {current_ki_ = value;}
+  void set_kd(float value) {current_kd_ = value;}
   
+  void set_output_min(float value) {current_output_min_ = value;}
+  void set_output_max(float value) {current_output_max_ = value;}
+  void set_output_restart(float value) {current_output_restart_ = value;}
+  
+  
+/*   void set_manual_level(float value);
+  
+  void set_charging_setpoint(float value);
+  void set_absorbing_setpoint(float value);
+  void set_floating_setpoint(float value);
+
+  void set_starting_battery_voltage(float value);
+  void set_charged_battery_voltage(float value);
+  void set_discharged_battery_voltage(float value);
+  
+  void set_kp(float value);
+  void set_ki(float value);
+  void set_kd(float value);
+  
+  void set_output_min(float value);
+  void set_output_max(float value);
+  void set_output_restart(float value); */
 #endif
 
-// #ifdef USE_BINARY_SENSOR
-  bool get_thermostat_cut(void){return this->current_thermostat_cut_;}
-// #endif
+#ifdef USE_BINARY_SENSOR
+bool get_thermostat_cut(void){return current_thermostat_cut_;}
+#endif
 
-  float get_error(void) { return this->current_error_; }
-  float get_output(void) { return this->current_output_; }
-  float get_target(void) { return this->current_target_; }
+#ifdef USE_SENSOR
+float get_error(void){return current_error_;}
+float get_output(void){return current_output_;}
+float get_target(void){return current_target_;}
+#endif
 
  protected:
   uint32_t last_time_ = 0;
@@ -112,12 +163,12 @@ SUB_NUMBER(output_restart)
   float integral_= 0.0f; 
   float derivative_ = 0.0f;
   
-  float current_battery_current_ = 0.0f;
-  float current_power_ = 0.0f;
-  float current_battery_voltage_ = 54.0f;
+  float current_battery_current_;
+  float current_power_;
+  float current_battery_voltage_;
   float current_device_output_;
   
-  bool current_pid_mode_ = true;
+  bool current_pid_mode_;
   
   sensor::Sensor *battery_voltage_sensor_;
   sensor::Sensor *battery_current_sensor_;
@@ -125,6 +176,10 @@ SUB_NUMBER(output_restart)
   output::FloatOutput *device_output_;
   
   CallbackManager<void()> pid_computed_callback_;
+/*   CallbackManager<void()> output_callback_;
+  CallbackManager<void()> error_callback_;
+  CallbackManager<void()> target_callback_;
+  CallbackManager<void()> thermostat_cut_callback_; */
   
 #ifdef USE_SWITCH  
   bool current_activation_ = false;
@@ -135,10 +190,11 @@ SUB_NUMBER(output_restart)
   bool current_thermostat_cut_ = false;
 #endif  
 
+#ifdef USE_SENSOR
   float current_error_ = 0.0f;
   float current_output_ = 0.0f;
   float current_target_ = 0.0f;
-
+#endif
 #ifdef USE_NUMBER
   float current_manual_level_ = 0.0f;
   
