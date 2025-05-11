320650d094cd0e0f477b6c03da4022cafa493c3c
SeByDocKy <sebydocky@hotmail.com>
2025-05-11 10:52:05 +0200

    Add files via upload

--- components/offsr/switch/__init__.py
+++ components/offsr/switch/__init__.py
@@ -42,26 +42,26 @@ async def to_code(config):
     # offsr_component = await cg.get_variable(config[CONF_OFFSR_ID])
     # cg.add(var.set_parent(offsr_component))
   
-    var = await cg.get_variable(config[CONF_OFFSR_ID])
-    if CONF_ACTIVATION in config:
-        s = await switch.new_switch(config[CONF_ACTIVATION])
-        await cg.register_parented(s, config[CONF_OFFSR_ID])
-        cg.add(var.set_activation_switch(s))
+    # var = await cg.get_variable(config[CONF_OFFSR_ID])
+    # if CONF_ACTIVATION in config:
+        # s = await switch.new_switch(config[CONF_ACTIVATION])
+        # await cg.register_parented(s, config[CONF_OFFSR_ID])
+        # cg.add(var.set_activation_switch(s))
         
-    if CONF_MANUAL_OVERRIDE in config:
-        s = await switch.new_switch(config[CONF_MANUAL_OVERRIDE])
-        await cg.register_parented(s, config[CONF_OFFSR_ID])
-        cg.add(var.set_manual_override_switch(s))
+    # if CONF_MANUAL_OVERRIDE in config:
+        # s = await switch.new_switch(config[CONF_MANUAL_OVERRIDE])
+        # await cg.register_parented(s, config[CONF_OFFSR_ID])
+        # cg.add(var.set_manual_override_switch(s))
         
 
-    # offsr_component = await cg.get_variable(config[CONF_OFFSR_ID])
-    # if activation_config := config.get(CONF_ACTIVATION):
-        # s = await switch.new_switch(activation_config)
-        # await cg.register_parented(s, config[CONF_OFFSR_ID])
-        # cg.add(offsr_component.set_activation_switch(s))
-    # if manual_override_config := config.get(CONF_MANUAL_OVERRIDE):
-        # s = await switch.new_switch(manual_override_config)
-        # await cg.register_parented(s, config[CONF_OFFSR_ID])
-        # cg.add(offsr_component.set_manual_override_switch(s))    
+    offsr_component = await cg.get_variable(config[CONF_OFFSR_ID])
+    if activation_config := config.get(CONF_ACTIVATION):
+        s = await switch.new_switch(activation_config)
+        await cg.register_parented(s, config[CONF_OFFSR_ID])
+        cg.add(offsr_component.set_activation_switch(s))
+    if manual_override_config := config.get(CONF_MANUAL_OVERRIDE):
+        s = await switch.new_switch(manual_override_config)
+        await cg.register_parented(s, config[CONF_OFFSR_ID])
+        cg.add(offsr_component.set_manual_override_switch(s))    
        
     
