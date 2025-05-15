4b748dd5ea4de4c7f0830e201572260de9e3edf9
SeByDocKy <sebydocky@hotmail.com>
2025-05-15 14:31:42 +0200

    Add files via upload

--- components/offsr/binary_sensor/__init__.py
+++ components/offsr/binary_sensor/__init__.py
@@ -10,12 +10,12 @@ from esphome.const import (
 DEPENDENCIES = ["offsr"]
 CONF_THERMOSTAT_CUT = "thermostat_cut"
 
+ICON_HOT_WATER_TANK = "mdi:water-boiler-alert"
+
 from .. import CONF_OFFSR_ID, OFFSRComponent, offsr_ns
 
 OFFSRBinarySensor = offsr_ns.class_("OFFSRBinarySensor", cg.Component)
 
-# ThermostatcutBinarySensor = offsr_ns.class_("ThermostatcutBinarySensor", binary_sensor.BinarySensor, cg.Component
-# )
 
 CONFIG_SCHEMA = {
     cv.GenerateID(): cv.declare_id(OFFSRBinarySensor),
@@ -23,6 +23,7 @@ CONFIG_SCHEMA = {
     cv.GenerateID(CONF_OFFSR_ID): cv.use_id(OFFSRComponent),
     cv.Optional(CONF_THERMOSTAT_CUT): binary_sensor.binary_sensor_schema(
         device_class=DEVICE_CLASS_OCCUPANCY,
+        icon = ICON_HOT_WATER_TANK,
     ),
 }
 
@@ -35,11 +36,4 @@ async def to_code(config):
     if CONF_THERMOSTAT_CUT in config:
         bsens = await binary_sensor.new_binary_sensor(config[CONF_THERMOSTAT_CUT])
         cg.add(var.set_thermostat_cut_binary_sensor(bsens))
-    
-    # offsr_component = await cg.get_variable(config[CONF_OFFSR_ID])
-    # if thermostat_cut_config := config.get(CONF_THERMOSTAT_CUT):
-        # bsens = await binary_sensor.new_binary_sensor(thermostat_cut_config)
-        # await cg.register_component(bsens, config)
-        # cg.add(bsens.set_parent(offsr_component))
-
         
