ab58a1484728627d2aa6674602b6edcc74a3260d
SeByDocKy <sebydocky@hotmail.com>
2025-05-16 10:05:49 +0200

    Add files via upload

--- components/offsr/switch/pid_mode_switch.cpp
+++ components/offsr/switch/pid_mode_switch.cpp
@@ -7,6 +7,7 @@ void PidModeSwitch::setup() {
   bool state;
   this->pref_ = global_preferences->make_preference<bool>(this->get_object_id_hash());
   if (!this->pref_.load(&state)) state = this->parent_->get_pid_mode();
+  this->parent_->set_pid_mode(state);
   this->publish_state(state);
 }
 void PidModeSwitch::write_state(bool state) {
