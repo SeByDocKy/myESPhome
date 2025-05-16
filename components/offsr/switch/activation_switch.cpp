788a5498cc539ff2cb4bd4802a64c6d4e07bf4dd
SeByDocKy <sebydocky@hotmail.com>
2025-05-16 10:01:48 +0200

    Add files via upload

--- components/offsr/switch/activation_switch.cpp
+++ components/offsr/switch/activation_switch.cpp
@@ -6,7 +6,8 @@ void ActivationSwitch::setup() {
   bool state;
   this->pref_ = global_preferences->make_preference<bool>(this->get_object_id_hash());
   if (!this->pref_.load(&state)) state = this->parent_->get_activation();
-  this->publish_state(state);	
+  this->publish_state(state);
+  this->parent_->set_activation(state);
 }
 
 void ActivationSwitch::write_state(bool state) {
