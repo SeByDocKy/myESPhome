90aa4cd3ccba97dd36e1496dafe5f89eb08c46fd
SeByDocKy <sebydocky@hotmail.com>
2025-06-08 17:31:55 +0200

    Add files via upload

--- components/dmtcp/dmtcp.cpp
+++ components/dmtcp/dmtcp.cpp
@@ -52,7 +52,7 @@ namespace dmtcp {
     }
 	
 	// Decode value based on byte order
-    float value = ( response[9] << 8) | response[10];
+    float value = static_cast<float>(( response[9] << 8) | response[10])*0.1f;
 
 	
 	
