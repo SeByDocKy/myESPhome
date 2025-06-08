8f22ced9748b3de392a32f2d3e41443fbbdaffdb
SeByDocKy <sebydocky@hotmail.com>
2025-06-08 22:57:56 +0200

    Add files via upload

--- components/dmtcp/output/dmtcp_output.cpp
+++ components/dmtcp/output/dmtcp_output.cpp
@@ -1,5 +1,4 @@
 #include "dmtcp_output.h"
-
 #include "esphome/core/log.h"
 
 namespace esphome {
@@ -11,11 +10,43 @@ namespace dmtcp {
     ESP_LOGCONFIG(TAG, "DMTCP Output:");
   }
   void DMTCPOutput::write_state(float state) {
- 
-  }
-
+   	WiFiClient client;
 	
-
-
+	if (!client.connect(this->ip_address_.c_str(), this->port_)) {
+      ESP_LOGE("modbus_tcp", "Failed to connect to Modbus server %s:%d", this->ip_address_.c_str(), this->port_);
+      return;
+    }
+	
+	uint8_t request[] = {
+        0x00, 0x01,            // Transaction ID
+        0x00, 0x00,            // Protocol ID
+        0x00, 0x06,            // Length
+        this->write_unit_id_,  // Unit ID
+        this->write_fcn_code_, // Function Code (write Holding Registers)
+        (uint8_t)((this->start_modbus_address_ >> 8) & 0xFF),  // Start Address (High Byte)
+        (uint8_t)(this->start_modbus_address_ & 0xFF),         // Start Address (Low Byte)
+        (uint8_t)((this->nb_bytes_to_read_ >> 8) & 0xFF),      // (High Byte)
+        (uint8_t)(this->nb_bytes_to_read_ & 0xFF),             // (Low Byte)       
+    };
+	
+	client.write(request, sizeof(request));
+    delay(100);
+	
+	uint8_t response[SIZE_RESPONSE];
+    size_t response_len = client.read(response, sizeof(response));
+    if (response_len < 9) {
+      ESP_LOGE("modbus_tcp", "Invalid response length: %d", response_len);
+      return;
+    }
+	// Debugging: Print raw response data
+    ESP_LOGD("modbus_tcp", "Raw Response Data:");
+    for (size_t i = 0; i < response_len; i++) {
+      ESP_LOGD("modbus_tcp", "Byte %d: 0x%02X", i, response[i]);
+    }
+    if (response[7] != this->write_fcn_code_) {
+      ESP_LOGE("modbus_tcp", "Unexpected function code: 0x%02X", response[7]);
+      return;
+    } 
+  }
  }
 }
--- components/dmtcp/output/dmtcp_output.h
+++ components/dmtcp/output/dmtcp_output.h
@@ -15,6 +15,10 @@ class DMTCPOutput : public Component, public output::FloatOutput, public Parente
 
  protected:
   // uint8_t channel_;
+  uint8_t write_unit_id_ = 0x02;
+  uint8_t write_fcn_code_= 0x06;
+  uint16_t start_modbus_address_ = 0x0028;
+  uint16_t nb_bytes_to_read_ = 0x0001;
 };	
 	
 	
