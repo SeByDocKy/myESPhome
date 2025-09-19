e3dd23121a8b8e289cc82a5be6d0b250413c687a
SeByDocKy <sebydocky@hotmail.com>
2025-09-19 14:31:05 +0200

    Update __init__.py

--- components/hoymiles_inverter/__init__.py
+++ components/hoymiles_inverter/__init__.py
@@ -133,8 +133,8 @@ async def to_code(config):
     cg.add_platformio_option("build_unflags", ["-std=gnu++11", "-fno-exceptions"])
     cg.add_library("SPI", None)
     cg.add_library("esphome-hoymiles-libs", None, "https://github.com/nedyarrd/esphome-hoymiles-libs")
-    # cg.add_library("Hoymiles", None, "https://github.com/nedyarrd/esphome-hoymiles-main")
-    cg.add_library("CMT2300A", None, "https://github.com/nedyarrd/esphome-CMT2300A")
+    cg.add_library("Hoymiles", None, "https://github.com/nedyarrd/esphome-hoymiles-main")
+    # cg.add_library("CMT2300A", None, "https://github.com/nedyarrd/esphome-CMT2300A")
     var = cg.new_Pvariable(config[CONF_ID])
 
     for inv_conf in config[CONF_INVERTERS]:
