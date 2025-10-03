118b28819a00c5eae8896a69fd6b1e2de5de7f66
SeByDocKy <sebydocky@hotmail.com>
2025-10-03 11:55:20 +0200

    Add files via upload

--- components/cmt2300a/__init__.py
+++ components/cmt2300a/__init__.py
@@ -1,19 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
-from esphome.const import CONF_ID
 from esphome import pins
 
 DEPENDENCIES = ["spi"]
 CODEOWNERS = ["@your_github"]
 AUTO_LOAD = []
 
-cmt2300a_ns = cg.esphome_ns.namespace("cmt2300a")
-CMT2300AComponent = cmt2300a_ns.class_(
-    "CMT2300AComponent", 
-    cg.Component, 
-    spi.SPIDevice
+from esphome.const import (
+    CONF_ID,
+    CONF_NAME,
 )
+
+
+cmt2300a_ns = cg.esphome_ns.namespace("cmt2300a")
+CMT2300AComponent = cmt2300a_ns.class_("CMT2300AComponent", cg.Component, spi.SPIDevice)
 
 CONF_SPI_ID = "spi_id"
 CONF_CS_PIN = "cs_pin"
@@ -40,19 +41,21 @@ DATA_RATES = {
     "250kbps": 250000,
 }
 
-CONFIG_SCHEMA = cv.Schema({
+CONFIG_SCHEMA = cv.Schema(
+  {
     cv.GenerateID(): cv.declare_id(CMT2300AComponent),
     cv.GenerateID(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-    cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
-    cv.Required(CONF_FCS_PIN): pins.gpio_output_pin_schema,
-    cv.Optional(CONF_GPIO1_PIN): pins.gpio_input_pin_schema,
-    cv.Optional(CONF_GPIO2_PIN): pins.gpio_input_pin_schema,
-    cv.Optional(CONF_GPIO3_PIN): pins.gpio_input_pin_schema,
+    cv.Required(CONF_CS_PIN): pins.internal_gpio_input_pin_schema, #pins.gpio_output_pin_schema, 
+    cv.Required(CONF_FCS_PIN): pins.internal_gpio_input_pin_schema,
+    cv.Optional(CONF_GPIO1_PIN): pins.internal_gpio_input_pin_schema,
+    cv.Optional(CONF_GPIO2_PIN): pins.internal_gpio_input_pin_schema,
+    cv.Optional(CONF_GPIO3_PIN): pins.internal_gpio_input_pin_schema,
     cv.Optional(CONF_FREQUENCY, default="868MHz"): cv.enum(FREQUENCIES),
     cv.Optional(CONF_DATA_RATE, default="250kbps"): cv.enum(DATA_RATES),
     cv.Optional(CONF_TX_POWER, default=20): cv.int_range(min=0, max=20),
     cv.Optional(CONF_ENABLE_CRC, default=True): cv.boolean,
-}).extend(cv.COMPONENT_SCHEMA).extend(spi.spi_device_schema(cs_pin_required=False))
+  }
+).extend(cv.COMPONENT_SCHEMA).extend(spi.spi_device_schema(cs_pin_required=False))
 
 
 async def to_code(config):
@@ -86,3 +89,4 @@ async def to_code(config):
     cg.add(var.set_data_rate(config[CONF_DATA_RATE]))
     cg.add(var.set_tx_power(config[CONF_TX_POWER]))
     cg.add(var.set_enable_crc(config[CONF_ENABLE_CRC]))
+    await cg.register_component(var, config)
