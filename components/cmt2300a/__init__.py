1a33b04267e3ab50a50ccef50adc09d5fb263c29
SeByDocKy <sebydocky@hotmail.com>
2025-10-04 09:07:04 +0200

    Update __init__.py

--- components/cmt2300a/__init__.py
+++ components/cmt2300a/__init__.py
@@ -36,20 +36,23 @@ DATA_RATES = {
     "250kbps": 250000,
 }
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(CMT2300AComponent),
-    cv.Required(CONF_SCLK_PIN): pins.gpio_output_pin_schema,
-    cv.Required(CONF_SDIO_PIN): pins.gpio_pin_schema,  # Bidirectionnel
-    cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
-    cv.Required(CONF_FCS_PIN): pins.gpio_output_pin_schema,
-    cv.Optional(CONF_GPIO1_PIN): pins.gpio_input_pin_schema,
-    cv.Optional(CONF_GPIO2_PIN): pins.gpio_pin_schema,
-    cv.Optional(CONF_GPIO3_PIN): pins.gpio_pin_schema,
-    cv.Optional(CONF_FREQUENCY, default="868MHz"): cv.enum(FREQUENCIES),
-    cv.Optional(CONF_DATA_RATE, default="250kbps"): cv.enum(DATA_RATES),
-    cv.Optional(CONF_TX_POWER, default=20): cv.int_range(min=0, max=20),
-    cv.Optional(CONF_ENABLE_CRC, default=True): cv.boolean,
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = (
+   cv.Schema(
+    {
+       cv.GenerateID(): cv.declare_id(CMT2300AComponent),
+       cv.Required(CONF_SCLK_PIN): pins.gpio_output_pin_schema,
+       cv.Required(CONF_SDIO_PIN): pins.gpio_pin_schema,  # Bidirectionnel
+       cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
+       cv.Required(CONF_FCS_PIN): pins.gpio_output_pin_schema,
+       cv.Optional(CONF_GPIO1_PIN): pins.gpio_input_pin_schema,
+       cv.Optional(CONF_GPIO2_PIN): pins.gpio_pin_schema,
+       cv.Optional(CONF_GPIO3_PIN): pins.gpio_pin_schema,
+       cv.Optional(CONF_FREQUENCY, default="868MHz"): cv.enum(FREQUENCIES),
+       cv.Optional(CONF_DATA_RATE, default="250kbps"): cv.enum(DATA_RATES),
+       cv.Optional(CONF_TX_POWER, default=20): cv.int_range(min=0, max=20),
+      cv.Optional(CONF_ENABLE_CRC, default=True): cv.boolean,
+  }).extend(cv.COMPONENT_SCHEMA)
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -79,6 +82,7 @@ async def to_code(config):
     # Ajout des includes ESP-IDF nécessaires
     cg.add_platformio_option("lib_deps", [])
     cg.add_build_flag("-DUSE_ESP_IDF")
+
 
 
 
