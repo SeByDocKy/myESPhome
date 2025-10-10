f56661f282fd7b787318c1225821a3a651b8b1b8
SeByDocKy <sebydocky@hotmail.com>
2025-10-10 11:23:39 +0200

    Update hoymiles_inverter.h

--- components/hoymiles_inverter/hoymiles_inverter.h
+++ components/hoymiles_inverter/hoymiles_inverter.h
@@ -38,7 +38,6 @@ class HoymilesChannel : public esphome::Component {
         void set_voltage_sensor(esphome::sensor::Sensor* sensor) { this->voltage_ = sensor; }
         void set_current_sensor(esphome::sensor::Sensor* sensor) { this->current_ = sensor; }
 
-        void set_serial(uint64_t sn){this->serial_ = sn;}
         void setup() override;
         void updateSensors(bool connected, StatisticsParser* stat, ChannelType_t typ, ChannelNum_t num);
 };
@@ -67,8 +66,8 @@ class HoymilesInverter : public esphome::Component {
         void set_limit_absolute_number(HoymilesNumber* number);
         void set_is_reachable_sensor(esphome::binary_sensor::BinarySensor* sensor) { this->is_reachable_sensor_ = sensor; }
         
-        void set_serial_no(std::string serial) { this->serial_ = std::stoll(serial, nullptr, 16); }
-        // void set_serial_no(std::string serial) { this->serial_ = serial; }
+        //void set_serial_no(std::string serial) { this->serial_ = std::stoll(serial, nullptr, 16); }
+        void set_serial_no(std::string serial) { this->serial_ = 13831264035630; }
         uint64_t serial() { return this->serial_; }
         void set_inverter(std::shared_ptr<InverterAbstract> inverter) { this->inverter_ = inverter; }
         void loop() override;
