a00426b09e18d6e019a742498a83a39350ef6d4c
SeByDocKy <sebydocky@hotmail.com>
2025-10-12 00:14:22 +0200

    Update hoymiles_inverter.h

--- components/hoymiles_inverter/hoymiles_inverter.h
+++ components/hoymiles_inverter/hoymiles_inverter.h
@@ -54,10 +54,10 @@ class HoymilesInverter : public esphome::Component {
 
         std::shared_ptr<InverterAbstract> inverter_ = nullptr;
 
-       /*
+       // /*
         std::unique_ptr<CMT2300A> radio_;
         float rssi_;
-        */
+        // */
 
         uint32_t system_conf_last_update_ = 0;
         uint32_t dev_info_last_update_ = 0;
