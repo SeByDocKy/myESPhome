0339e9dccfc277e4b0cd58012aea709b88c39f7b
SeByDocKy <sebydocky@hotmail.com>
2025-10-12 11:05:52 +0200

    Update hoymiles_inverter.cpp

--- components/hoymiles_inverter/hoymiles_inverter.cpp
+++ components/hoymiles_inverter/hoymiles_inverter.cpp
@@ -143,7 +143,7 @@ void HoymilesChannel::updateSensors(bool connected, StatisticsParser* stat, Chan
     if (this->temperature_ != nullptr) {
         auto field = FieldId_t::FLD_T;
         float temp = stat->getChannelFieldValue(typ, num, field);
-        ESP_LOGD("HMS" , "temperature = %2.2f" , temp);
+        ESP_LOGD("HMS" , "type channel %s, temperature = %2.2f" , typ, temp);
         this->temperature_->publish_state(connected? stat->getChannelFieldValue(typ, num, field): NAN);
     }
 }
