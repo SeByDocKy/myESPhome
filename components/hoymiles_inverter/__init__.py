71b90b5373db155f423053834d9789a41f2091bc
SeByDocKy <sebydocky@hotmail.com>
2025-10-18 21:39:07 +0200

    Update __init__.py

--- components/hoymiles_inverter/__init__.py
+++ components/hoymiles_inverter/__init__.py
@@ -107,7 +107,7 @@ INVERTER_SCHEMA = cv.Schema({
     cv.Optional(CONF_INVERTER_CHANNEL): CHANNEL_SCHEMA,
     cv.Optional(CONF_OUTPUT_PERCENT): output.FLOAT_OUTPUT_SCHEMA({
         cv.Required(CONF_ID): cv.declare_id(_out_cls),
-    }).extend(cv.COMPONENT_SCHEMA),
+    }),
     cv.Optional(CONF_LIMIT_PERCENT): number.number_schema(
         _percent_cls, #_num_cls,
         entity_category="config",
