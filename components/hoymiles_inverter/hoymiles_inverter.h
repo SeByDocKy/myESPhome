c8ac5c7cd3d97d90b85edbe21b25b9a0924ed3cc
SeByDocKy <sebydocky@hotmail.com>
2025-10-18 15:04:06 +0200

    Update hoymiles_inverter.h

--- components/hoymiles_inverter/hoymiles_inverter.h
+++ components/hoymiles_inverter/hoymiles_inverter.h
@@ -33,6 +33,7 @@ class HoymilesNumber : public esphome::number::Number {
         void control(float value) override;
         void add_control_callback(std::function<void(float)> &&cb) { this->control_callback_.add(std::move(cb)); }
         float get_percent_power_limit(void){return this->current_percent_power_limit_;}
+        void set_percent_power_limit(float value){this->current_percent_power_limit_ = value;}
 };
 
 class HoymilesChannel : public esphome::Component {
