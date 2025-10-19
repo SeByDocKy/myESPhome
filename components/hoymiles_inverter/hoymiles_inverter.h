c49da2dbbbe4ad80b13dd58ae4bc646159318afd
SeByDocKy <sebydocky@hotmail.com>
2025-10-19 20:27:48 +0200

    Update hoymiles_inverter.h

--- components/hoymiles_inverter/hoymiles_inverter.h
+++ components/hoymiles_inverter/hoymiles_inverter.h
@@ -144,7 +144,7 @@ class HoymilesInverter : public esphome::Component {
         // void set_limit_absolute_number(HoymilesNumber* number);
 
 
-        void set_percent_output(PercentFloatOutput* output) {this->percent_output_ = output;}
+        void set_limit_percent_output(PercentFloatOutput* output) {this->percent_output_ = output;}
         void set_limit_percent_number(PercentNumber* number){this->limit_percent_number_ = number;}
         void set_limit_absolute_number(AbsoluteNumber* number){this->limit_absolute_number_ = number;}
 
