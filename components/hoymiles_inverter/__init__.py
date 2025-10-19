d86900811adbaa6cb1edbd5b54d7d596c159d4e3
SeByDocKy <sebydocky@hotmail.com>
2025-10-19 21:28:16 +0200

    Update __init__.py

--- components/hoymiles_inverter/__init__.py
+++ components/hoymiles_inverter/__init__.py
@@ -218,10 +218,10 @@ async def to_code(config):
         if CONF_LIMIT_ABSOLUTE in inv_conf:
             cg.add(inv_var.set_limit_absolute_number(await number.new_number(inv_conf[CONF_LIMIT_ABSOLUTE], min_value=0, max_value=2000, step=20)))
         if CONF_PERCENT_OUTPUT in inv_conf:
-            # await output.register_output(var, config)
+            await output.register_output(var, config)
             # out_var = cg.new_Pvariable(inv_conf[CONF_OUTPUT_PERCENT][CONF_ID])
             # await output.register_output(out_var, inv_conf[CONF_OUTPUT_PERCENT])
-            cg.add(inv_var.set_limit_percent_output(await output.register_output(var, inv_conf[CONF_PERCENT_OUTPUT])))
+            # cg.add(inv_var.set_limit_percent_output(await output.register_output(var, inv_conf[CONF_PERCENT_OUTPUT])))
             # cg.add(out_var.set_parent(inv_var))
         if CONF_REACHABLE in inv_conf:
             cg.add(inv_var.set_is_reachable_sensor(await binary_sensor.new_binary_sensor(inv_conf[CONF_REACHABLE])))
