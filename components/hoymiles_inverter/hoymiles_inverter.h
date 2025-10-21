a910997e88f9efb55b8b085682d64dc7edc3d229
SeByDocKy <sebydocky@hotmail.com>
2025-10-21 11:31:52 +0200

    Update hoymiles_inverter.h

--- components/hoymiles_inverter/hoymiles_inverter.h
+++ components/hoymiles_inverter/hoymiles_inverter.h
@@ -40,14 +40,14 @@ class PercentFloatOutput : public output::FloatOutput, public Component  {   // 
    // esphome::CallbackManager<void(float)> control_callback_;
    void write_state(float value) override;
    // float current_percent_output_;
-   // HoymilesInverter *parent_;
-   HoymilesPlatform *parent_;
+   HoymilesInverter *parent_;
+   // HoymilesPlatform *parent_;
    
   public:
     // void add_control_callback(std::function<void(float)> &&cb) { this->control_callback_.add(std::move(cb)); }
     // void write_state(float value) override;
-    //void set_parent(HoymilesInverter *parent) { this->parent_ = parent; }
-    void set_parent(HoymilesPlatform *parent) { this->parent_ = parent; }
+    void set_parent(HoymilesInverter *parent) { this->parent_ = parent; }
+    // void set_parent(HoymilesPlatform *parent) { this->parent_ = parent; }
 
    
   // float get_percent_power_limit(void){return this->current_percent_power_limit_;}
