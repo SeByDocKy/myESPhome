d3068c0440c0bc1883d54c5e3a1065dc2f38d7d3
SeByDocKy <sebydocky@hotmail.com>
2025-10-21 15:39:40 +0200

    Update hoymiles_inverter.cpp

--- components/hoymiles_inverter/hoymiles_inverter.cpp
+++ components/hoymiles_inverter/hoymiles_inverter.cpp
@@ -59,7 +59,7 @@ void HoymilesInverter::setup() {
 void HoymilesInverter::write_float(float value){
      if (value != NAN){ //NULL
        this->inverter_->sendActivePowerControlRequest(value*100, PowerLimitControlType::RelativNonPersistent);
-       this->active_ = true;
+       this->inverter_->active_ = true;
      }
 
 }
