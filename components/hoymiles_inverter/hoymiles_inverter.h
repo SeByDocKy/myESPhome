147b3b15eb5a0c12504256b35ec1cf8e67dff9fd
SeByDocKy <sebydocky@hotmail.com>
2025-10-21 11:53:03 +0200

    Update hoymiles_inverter.h

--- components/hoymiles_inverter/hoymiles_inverter.h
+++ components/hoymiles_inverter/hoymiles_inverter.h
@@ -34,7 +34,7 @@ class EsphLogPrint : public Print {
 // };
 
 
-class PercentFloatOutput : public output::FloatOutput, public Component  {   // , public Parented<HoymilesInverter>  
+class PercentFloatOutput : public output::FloatOutput, public Component, public HoymilesInverter  {   // , public Parented<HoymilesInverter>  
 
  private:
    // esphome::CallbackManager<void(float)> control_callback_;
