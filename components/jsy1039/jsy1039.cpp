e765fd541350074235e4d3b99fa80b3c51c29a0a
SeByDocKy <sebydocky@hotmail.com>
2025-10-24 11:44:10 +0200

    Add files via upload

---
+++ components/jsy1039/jsy1039.cpp
@@ -0,0 +1,177 @@
+#include "jsy1039.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace jsy1039 {
+
+static const char *const TAG = "jsy1039";
+static const uint8_t JSY1039_CMD_READ_IN_REGISTERS = 0x03;   // multiple registers
+static const uint8_t JSY1039_CMD_WRITE_IN_REGISTERS = 0x10;
+static const uint8_t JSY1039_REGISTER_SETTINGS_START = 0x04; // modbus address & databaud
+static const uint8_t JSY1039_REGISTER_SETTINGS_COUNT = 0x01;  // 1 x 16-bit setting registers
+static const uint8_t JSY1039_RESET_RESET_ENERGY_LB = 0x04; // 0x0104;
+static const uint16_t JSY1039_REGISTER_DATA_START = 0x048;  
+static const uint8_t JSY1039_REGISTER_DATA_COUNT = 9;  // 20 x 16-bit data registers
+
+void JSY1039::setup() { 
+  ESP_LOGCONFIG(TAG, "Setting up JSY1039..."); 
+}
+
+void JSY1039::on_modbus_data(const std::vector<uint8_t> &data) {
+  if ((this->read_data_ == 1) & (data.size() < JSY1039_REGISTER_DATA_COUNT*2)) {
+    ESP_LOGW(TAG, "Invalid size for JSY1039 data!");
+    return;
+  }
+
+  auto jsy1039_get_16bit = [&](size_t i) -> uint16_t {
+    return (uint16_t(data[i + 0]) << 8) | (uint16_t(data[i + 1]) << 0);
+  };
+  auto jsy1039_get_32bit = [&](size_t i) -> uint32_t {
+    return (uint32_t(jsy1039_get_16bit(i + 0)) << 16) | (uint32_t(jsy1039_get_16bit(i + 2)) << 0);
+  };
+ 
+  if (this->read_data_ == 1){
+    float raw_sign = static_cast<float>(jsy1039_get_16bit(6)); //0 for positive, 1 for negative  
+    float voltage = static_cast<float>(jsy1039_get_16bit(0))/100.0f;  // max 655.35 V
+    uint16_t raw_current = jsy1039_get_16bit(2);  
+    float current = ((1.0f - raw_sign)*raw_current - raw_sign*raw_current)/100.0f;  // min -655.35 A, max 655.35 A
+    uint16_t raw_power   = jsy1039_get_16bit(4);
+    float power = (1.0f - raw_sign)*raw_power - raw_sign*raw_power;  // min 65535 W max 65535 W
+    float pos_energy = static_cast<float>(jsy1039_get_32bit(8))/100.0f; // max 42 949 673 kWh
+    float neg_energy = static_cast<float>(jsy1039_get_32bit(12))/100.0f; // max 42 949 673 kWh
+    float power_factor = static_cast<float>(jsy1039_get_16bit(16))/1000.0f;   // max 65.535
+    float frequency = static_cast<float>(jsy1039_get_16bit(18))/100.0f;  // max 655.35 Hz
+  
+    ESP_LOGVV(TAG, "modbus address=%d, V=%.1f V, I=%.3f A, P=%.1f W, E+=%.1f kWh , E-=%.1f kWh, F=%.1f Hz, PF=%.2f", int(this->address_), voltage, current, power, pos_energy, neg_energy, frequency, power_factor);
+
+    if (this->voltage_sensor_ != nullptr)
+      this->voltage_sensor_->publish_state(voltage);
+    if (this->current_sensor_ != nullptr)
+      this->current_sensor_->publish_state(current);
+    if (this->power_sensor_ != nullptr)
+      this->power_sensor_->publish_state(power);
+    if (this->pos_energy_sensor_ != nullptr)
+      this->pos_energy_sensor_->publish_state(pos_energy);
+    if (this->neg_energy_sensor_ != nullptr)
+      this->neg_energy_sensor_->publish_state(neg_energy);
+    if (this->frequency_sensor_ != nullptr)
+      this->frequency_sensor_->publish_state(frequency);
+    if (this->power_factor_sensor_ != nullptr)
+      this->power_factor_sensor_->publish_state(power_factor);
+  
+  }
+  else if(this->read_data_ == 2){ // read 0x04 register
+   if ( (data[0]>=1) & (data[0] <= 255) & (data[1]>=3) & (data[0] <= 8)){
+	  this->current_address_ = data[0];
+	  this->current_baudrate_= data[1];
+	  ESP_LOGD(TAG, "JSY1039: Read 0x04 register with address=%d, baudrate = %d", this->current_address_, this->current_baudrate_);
+	}
+    else{
+	  ESP_LOGD(TAG, "JSY1039: Read bad values from 0x04 : address=%d, baudrate = %d, keep current address =%d, baudrate %d", data[0], data[1] , this->current_address_ , this->current_baudrate_);
+    }	
+	this->read_data_ = 1;
+  }
+  else if(this->read_data_ == 3){ // write 0x04 register
+   	if ( data.size() < 2){
+	  ESP_LOGD(TAG, "JSY1039: failed to write new values 0x04 register with address=%d, baudrate = %d", this->new_address_, this->new_baudrate_);
+	}
+    else{
+	  ESP_LOGD(TAG, "JSY1039: wrote new values into 0x04 register with address=%d, baudrate = %d", this->new_address_ , this->new_baudrate_);
+    }	
+	this->read_data_ = 1; 
+  }
+  else if(this->read_data_ == 4){ // Reset Energy1
+   	if ( data.size() < 2){
+	  ESP_LOGD(TAG, "JSY1039: failed to reset Energy1");
+	}
+    else{
+	  ESP_LOGD(TAG, "JSY1039: successfully reset Energy1");
+    }	
+	this->read_data_ = 1; 
+  }
+  else if(this->read_data_ == 5){ // Reset Energy2
+   	if ( data.size() < 2){
+	  ESP_LOGD(TAG, "JSY1039: failed to reset Energy2");
+	}
+    else{
+	  ESP_LOGD(TAG, "JSY1039: successfully reset Energy2");
+    }	
+	this->read_data_ = 1; 
+  }  
+}
+
+void JSY1039::update() { this->send(JSY1039_CMD_READ_IN_REGISTERS, JSY1039_REGISTER_DATA_START , JSY1039_REGISTER_DATA_COUNT); }
+
+void JSY1039::dump_config() {
+  ESP_LOGCONFIG(TAG, "JSY1039:");
+  ESP_LOGCONFIG(TAG, "  Address: 0x%02X", this->address_);
+  LOG_SENSOR("", "Voltage", this->voltage_sensor_);
+  LOG_SENSOR("", "Current", this->current_sensor_);
+  LOG_SENSOR("", "Power", this->power_sensor_);
+  LOG_SENSOR("", "Positive Energy", this->pos_energy_sensor_);
+  LOG_SENSOR("", "Negative Energy", this->neg_energy_sensor_);
+  LOG_SENSOR("", "Frequency", this->frequency_sensor_);
+  LOG_SENSOR("", "Power Factor", this->power_factor_sensor_);
+}
+
+void JSY1039::read_register04() {
+  this->read_data_ = 2;
+  std::vector<uint8_t> cmd;
+  cmd.push_back(this->address_); 
+  cmd.push_back(JSY1039_CMD_READ_IN_REGISTERS);
+  cmd.push_back(0x00);  
+  cmd.push_back(JSY1039_REGISTER_SETTINGS_START);
+  cmd.push_back(0x00);
+  cmd.push_back(JSY1039_REGISTER_SETTINGS_COUNT);
+  ESP_LOGD(TAG, "JSY1039: reading values from 0x04 register"); 
+  this->send_raw(cmd);
+}
+
+void JSY1039::write_register04(uint8_t new_address , uint8_t new_baudrate) {
+  if ((new_address>=1) & (new_address <= 255) & (new_baudrate>=3) & (new_baudrate <= 8)){
+    this->read_data_ = 3;
+	std::vector<uint8_t> cmd;
+    cmd.push_back(0x00);  // broadcast address
+    cmd.push_back(JSY1039_CMD_WRITE_IN_REGISTERS);
+    cmd.push_back(0x00);  
+    cmd.push_back(JSY1039_REGISTER_SETTINGS_START);
+    cmd.push_back(0x00);
+    cmd.push_back(0x01); 
+    cmd.push_back(0x02);
+    cmd.push_back(new_address);
+    cmd.push_back(new_baudrate);
+    ESP_LOGD(TAG, "JSY1039: writing values into 0x04 register: address=%d, baudrate = %d", new_address_, new_baudrate); 
+    this->send_raw(cmd);
+  } 
+  else{
+	 ESP_LOGD(TAG, "JSY1039: attempt to write bad values into 0x04 : Address=%d, baudrate = %d", new_address_, new_baudrate); 
+  }
+}
+
+void JSY1039::reset_energy() {
+  this->read_data_ = 4;	
+  std::vector<uint8_t> cmd;
+  cmd.push_back(this->address_);
+  cmd.push_back(JSY1039_CMD_WRITE_IN_REGISTERS);
+  cmd.push_back(0x01);  
+  cmd.push_back(JSY193_RESET_RESET_ENERGY_LB);
+  cmd.push_back(0x00);
+  cmd.push_back(0x04); 
+  cmd.push_back(0x08);
+  
+  cmd.push_back(0x00);
+  cmd.push_back(0x00);
+  cmd.push_back(0x00);
+  cmd.push_back(0x00);
+  
+  cmd.push_back(0x00);
+  cmd.push_back(0x00);
+  cmd.push_back(0x00);
+  cmd.push_back(0x00);
+  ESP_LOGD(TAG, "JSY1039: sending reset Energy1 command"); 
+  this->send_raw(cmd);
+}
+
+
+}  // namespace jsy1039
+}  // namespace esphome
