1243d4e566359f59e3b4dba39c345aec9bf2a396
SeByDocKy <sebydocky@hotmail.com>
2025-10-27 10:49:40 +0100

    Update __init__.py

--- components/hms_inverter/__init__.py
+++ components/hms_inverter/__init__.py
@@ -181,6 +181,7 @@ async def to_code(config):
     cg.add_build_flag("-std=c++17")
     cg.add_build_flag("-std=gnu++17")
     cg.add_build_flag("-fexceptions")
+    cg.add_build_flag("-DHMS_INVERTER")
     cg.add_platformio_option("build_unflags", ["-std=gnu++11", "-fno-exceptions"])
 
     ############# with new OpenDTU lib #############
