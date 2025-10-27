3cd9222010977627b1235a2b36fcd3e348d8f01f
SeByDocKy <sebydocky@hotmail.com>
2025-10-27 13:55:07 +0100

    Update hm_inverter.cpp

--- components/hm_inverter/hm_inverter.cpp
+++ components/hm_inverter/hm_inverter.cpp
@@ -173,18 +173,18 @@ void HmInverter::loop() {
         }
         
        
-   //      if (rssi_ !=nullptr){
-			// rssi_->publish_state(this->radio_->getPALevel());
-   //      }
+        if (rssi_ !=nullptr){
+			rssi_->publish_state(this->radio_->getPALevel());
+        }
     }
 
         
-    if (this->get_oldpalevel() != this->get_palevel()){
-        // this->palevel_number_->publish_state(this->get_palevel());
-        this->radio_->setPALevel(this->get_palevel());
-        ESP_LOGI("HM" , "Set PALevel: %d dBm" , this->get_palevel());
-        this->set_oldpalevel(this->get_palevel());
-    }   
+    // if (this->get_oldpalevel() != this->get_palevel()){
+    //     // this->palevel_number_->publish_state(this->get_palevel());
+    //     this->radio_->setPALevel(this->get_palevel());
+    //     ESP_LOGI("HM" , "Set PALevel: %d dBm" , this->get_palevel());
+    //     this->set_oldpalevel(this->get_palevel());
+    // }   
     
    if (this->first_ && this->inverter_->isReachable()){
 
@@ -301,6 +301,7 @@ void HmPlatform::loop() {
 }
 
 }
+
 
 
 
