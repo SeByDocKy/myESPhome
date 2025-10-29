8217033cea23d174bca9b347e00a1ae296a53e94
SeByDocKy <sebydocky@hotmail.com>
2025-10-29 08:26:54 +0100

    Update sensor.py

--- components/victron_smart_shunt/sensor.py
+++ components/victron_smart_shunt/sensor.py
@@ -153,22 +153,22 @@ CONFIG_SCHEMA = cv.Schema(
             accuracy_decimals=3,
             device_class=DEVICE_CLASS_CURRENT,
         ),
-        cv.Optional(CONF_CHARGER_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional(CONF_CHARGER_TEXT): text_sensor.text_sensor_schema(    #TEXT_SENSOR_SCHEMA.extend
             {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
         ),
-        cv.Optional(CONF_ERROR_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional(CONF_ERROR_TEXT): text_sensor.text_sensor_schema(
             {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
         ),
-        cv.Optional(CONF_ALARM_REASON_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional(CONF_ALARM_REASON_TEXT): text_sensor.text_sensor_schema(
             {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
         ),
-        cv.Optional(CONF_TRACKER_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional(CONF_TRACKER_TEXT): text_sensor.text_sensor_schema(
             {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
         ),
-        cv.Optional(CONF_FW_VERSION): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional(CONF_FW_VERSION): text_sensor.text_sensor_schema(
             {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
         ),
-        cv.Optional(CONF_PID): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional(CONF_PID): text_sensor.text_sensor_schema(
             {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
         ),
         cv.Optional(CONF_INSTANTANEOUS_POWER): sensor.sensor_schema(
