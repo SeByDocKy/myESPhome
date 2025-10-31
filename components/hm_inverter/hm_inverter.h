fc8b599159fb3c242eafeaf309ccc450c0bbf4f9
SeByDocKy <sebydocky@hotmail.com>
2025-10-31 09:51:17 +0100

    Update hm_inverter.h

--- components/hm_inverter/hm_inverter.h
+++ components/hm_inverter/hm_inverter.h
@@ -112,7 +112,6 @@ class HmChannel : public esphome::Component {
 class HmInverter : public esphome::Component {
     private:
         uint64_t serial_;
-        int8_t rssi_dB;
         std::vector<HmChannel*> channels_ = {};
         HmChannel *inverter_channel_ = nullptr, *ac_channel_ = nullptr;
 
@@ -193,6 +192,7 @@ class HmPlatform : public esphome::PollingComponent {
 }
 
 }
+
 
 
 
