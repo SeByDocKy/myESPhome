32ff01fd49f1a392667696718e6416c8f2246a0b
SeByDocKy <sebydocky@hotmail.com>
2025-11-10 23:36:10 +0100

    Update max_current_output.cpp

--- components/emerson_r48/output/max_current_output.cpp
+++ components/emerson_r48/output/max_current_output.cpp
@@ -4,13 +4,8 @@
 namespace esphome {
 namespace emerson_r48 {
 
-/* static const int8_t SET_VOLTAGE_FUNCTION = 0x0;
-static const int8_t SET_CURRENT_FUNCTION = 0x3;
-static const int8_t SET_INPUT_CURRENT_FUNCTION = 0x4;
- */
 void EmersonR48MaxCurrentOutput::write_state(float value) {
 	this->parent_->write_float(value);
-	// this->parent_->set_level(value);
 }
 
 }  // namespace emerson_r48
