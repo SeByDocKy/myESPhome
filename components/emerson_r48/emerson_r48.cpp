8790ba473cdb4d7d6b71a348178966c6fd3f4738
SeByDocKy <sebydocky@hotmail.com>
2025-11-10 20:34:37 +0100

    Update emerson_r48.cpp

--- components/emerson_r48/emerson_r48.cpp
+++ components/emerson_r48/emerson_r48.cpp
@@ -64,6 +64,12 @@ void EmersonR48Component::gimme5(){
 }
 
 
+void EmersonR48Component::write_float(float value){
+
+
+
+}
+
 void EmersonR48Component::setup() {
   Automation<std::vector<uint8_t>, uint32_t, bool> *automation;
   LambdaAction<std::vector<uint8_t>, uint32_t, bool> *lambdaaction;
