a3ca020f9a277516e06134702d336feca49c14f6
SeByDocKy <sebydocky@hotmail.com>
2025-11-10 13:12:36 +0100

    Add files via upload

---
+++ components/emerson_r48/output/emerson_r48_max_current_output.cpp
@@ -0,0 +1,16 @@
+#include "emerson_r48_max_current_output.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace emerson_r48 {
+
+static const int8_t SET_VOLTAGE_FUNCTION = 0x0;
+static const int8_t SET_CURRENT_FUNCTION = 0x3;
+static const int8_t SET_INPUT_CURRENT_FUNCTION = 0x4;
+
+void EmersonR48MaxCurrentOutput::write_state(float value) {
+	parent_->write_float(value);
+}
+
+}  // namespace emerson_r48
+}  // namespace esphome
