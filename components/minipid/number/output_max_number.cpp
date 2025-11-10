91a31731c158d8d902f36c5aac5591d60d970add
SeByDocKy <sebydocky@hotmail.com>
2025-11-10 10:01:25 +0100

    Update output_max_number.cpp

--- components/minipid/number/output_max_number.cpp
+++ components/minipid/number/output_max_number.cpp
@@ -7,9 +7,8 @@ void OutputMaxNumber::setup() {
   float value;
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
   if (!this->pref_.load(&value)) value = this->parent_->get_output_max();
-  this->parent_->set_output_max(value);
-  this->publish_state(value*0.01);	
- 
+  this->parent_->set_output_max(value*0.01);
+  this->publish_state(value);	
 }
 
 void OutputMaxNumber::control(float value) {
@@ -20,6 +19,7 @@ void OutputMaxNumber::control(float value) {
 
 }  // namespace minipid
 }  // namespace esphome
+
 
 
 
