52f1d955c4ba36fc4ec41914f114eaeec9e47e5e
SeByDocKy <sebydocky@hotmail.com>
2025-11-11 20:23:51 +0100

    Update max_current_output.cpp

--- components/emerson_r48/output/max_current_output.cpp
+++ components/emerson_r48/output/max_current_output.cpp
@@ -5,8 +5,8 @@ namespace esphome {
 namespace emerson_r48 {
 
 void EmersonR48MaxCurrentOutput::write_state(float value) {
-	this->parent_->set_max_output_current(value*100,false);
-	ESP_LOGI("R48", "max output current new value %f %", value*100);
+	this->parent_->set_max_output_current(value*100.0f,false);
+	ESP_LOGI("R48", "max output current new value %f", value*100.0f);
 }
 
 }  // namespace emerson_r48
