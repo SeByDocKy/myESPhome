03399df23401466d95b19c6abb257b6dc2e1088c
SeByDocKy <sebydocky@hotmail.com>
2025-11-11 18:05:43 +0100

    Update emerson_r48_number.cpp

--- components/emerson_r48/number/emerson_r48_number.cpp
+++ components/emerson_r48/number/emerson_r48_number.cpp
@@ -10,10 +10,31 @@ static const int8_t SET_INPUT_CURRENT_FUNCTION = 0x4;
 
 void EmersonR48Number::setup() {
   float value;
-  this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
-  if (!this->pref_.load(&value)) value = this->parent_->get_output_max();
-  this->parent_->set_output_max(value*0.01);
-  this->publish_state(value);	
+  switch (this->functionCode_) {
+    case SET_VOLTAGE_FUNCTION:
+      this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+      if (!this->pref_.load(&value)) {
+        value = this->parent_->get_current_output_voltage(); 
+      }
+      this->parent_->set_current_output_voltage(value);
+      this->publish_state(value);
+    
+    case SET_CURRENT_FUNCTION:
+      this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+      if (!this->pref_.load(&value)) {
+        value = this->parent_->get_current_max_output_current(); 
+      }
+      this->parent_->set_current_max_output_current(value);
+      this->publish_state(value);
+
+    case SET_INPUT_CURRENT_FUNCTION:
+      this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+      if (!this->pref_.load(&value)) {
+        value = this->parent_->get_current_max_input_current(); 
+      }
+      this->parent_->set_current_max_input_current(value);
+      this->publish_state(value); 
+  }  
 }
 
 
