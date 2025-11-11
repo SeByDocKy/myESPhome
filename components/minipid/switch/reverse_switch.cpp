74ff2f21404c9b7e59dbed0190fe98b11b80a2bb
SeByDocKy <sebydocky@hotmail.com>
2025-11-11 20:37:46 +0100

    Update reverse_switch.cpp

--- components/minipid/switch/reverse_switch.cpp
+++ components/minipid/switch/reverse_switch.cpp
@@ -7,15 +7,16 @@ void ReverseSwitch::setup() {
   this->pref_ = global_preferences->make_preference<bool>(this->get_object_id_hash());
   if (!this->pref_.load(&state)) state = this->parent_->get_reverse();
   this->publish_state(state);
-  this->parent_->set_activation(state);
+  this->parent_->set_reverse(state);
 }
 
 void ReverseSwitch::write_state(bool state) {
   this->publish_state(state);
-  this->parent_->set_activation(state);
+  this->parent_->set_reverse(state);
   this->pref_.save(&state);
 }
 
 }  // namespace minipid
 }  // namespace esphome
+
 
