8e55d2e29f4ae1a4d404bf00ec8502bfde09a11d
SeByDocKy <sebydocky@hotmail.com>
2025-11-13 22:38:44 +0100

    Update output_min_number.cpp

--- components/dualpid/number/output_min_number.cpp
+++ components/dualpid/number/output_min_number.cpp
@@ -7,17 +7,18 @@ void OutputMinNumber::setup() {
   float value;
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
   if (!this->pref_.load(&value)) value = this->parent_->get_output_min();
-  this->parent_->set_output_min(value);
+  this->parent_->set_output_min(value0.01);
   this->publish_state(value);
 }
 
 void OutputMinNumber::control(float value) {
   this->publish_state(value);
-  this->parent_->set_output_min(value);
+  this->parent_->set_output_min(value0.01);
   this->pref_.save(&value);
 }
 
 }  // namespace dualpid
 }  // namespace esphome
+
 
 
