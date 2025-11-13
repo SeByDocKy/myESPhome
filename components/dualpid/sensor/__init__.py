85de7c1b6a43efb45bd2d31dd7337e36cf19096c
SeByDocKy <sebydocky@hotmail.com>
2025-11-13 22:25:44 +0100

    Update __init__.py

--- components/dualpid/sensor/__init__.py
+++ components/dualpid/sensor/__init__.py
@@ -6,6 +6,7 @@ from esphome.const import (
     STATE_CLASS_MEASUREMENT,
     DEVICE_CLASS_CURRENT,
     UNIT_AMPERE,
+    UNIT_WATT,
     UNIT_PERCENT,
 )
 
@@ -89,6 +90,7 @@ async def to_code(config):
     if CONF_TARGET in config:
         sens = await sensor.new_sensor(config[CONF_TARGET])
         cg.add(var.set_target_sensor(sens))
+
 
 
 
