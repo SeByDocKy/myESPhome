3d8dbe27eeaf0246a6957212a75508420511c192
SeByDocKy <sebydocky@hotmail.com>
2025-11-14 09:23:20 +0100

    Update charging_epoint_number.cpp

--- components/dualpid/number/charging_epoint_number.cpp
+++ components/dualpid/number/charging_epoint_number.cpp
@@ -7,18 +7,19 @@ void ChargingEpointNumber::setup() {
 	float value;
 	this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
 	if (!this->pref_.load(&value)) value = this->parent_->get_charging_epoint();
-	this->parent_->set_charging_epoint(value*0.01);
-	this->publish_state(value);
+	this->parent_->set_charging_epoint(value);
+	this->publish_state(value*100.0f);
 }
 
 void ChargingEpointNumber::control(float value) {
   this->publish_state(value);
-  this->parent_->set_charging_epoint(value*0.01);
+  this->parent_->set_charging_epoint(value*0.01f);
   this->pref_.save(&value);
 }
 
 }  // namespace dualpid
 }  // namespace esphome
+
 
 
 
