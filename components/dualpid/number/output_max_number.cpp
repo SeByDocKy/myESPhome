4f98eb3f6f78a53250c3396f10969a9f069dbceb
SeByDocKy <sebydocky@hotmail.com>
2025-11-14 09:17:04 +0100

    Update output_max_number.cpp

--- components/dualpid/number/output_max_number.cpp
+++ components/dualpid/number/output_max_number.cpp
@@ -7,18 +7,19 @@ void OutputMaxNumber::setup() {
   float value;
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
   if (!this->pref_.load(&value)) value = this->parent_->get_output_max();
-  this->parent_->set_output_max(value*0.01);
-  this->publish_state(value);
+  this->parent_->set_output_max(value);
+  this->publish_state(value*0.01);
 }
 
 void OutputMaxNumber::control(float value) {
   this->publish_state(value);
-  this->parent_->set_output_max(value*0.01);
+  this->parent_->set_output_max(value);
   this->pref_.save(&value);
 }
 
 }  // namespace dualpid
 }  // namespace esphome
+
 
 
 
