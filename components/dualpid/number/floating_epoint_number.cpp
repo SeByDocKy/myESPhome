488926e14a878412a3fbb0bc2f695158c12a67bf
SeByDocKy <sebydocky@hotmail.com>
2025-11-14 09:23:52 +0100

    Update floating_epoint_number.cpp

--- components/dualpid/number/floating_epoint_number.cpp
+++ components/dualpid/number/floating_epoint_number.cpp
@@ -7,17 +7,18 @@ void FloatingEpointNumber::setup() {
   float value;
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
   if (!this->pref_.load(&value)) value = this->parent_->get_floating_epoint();
-  this->parent_->set_floating_epoint(value*0.01);
-  this->publish_state(value);	
+  this->parent_->set_floating_epoint(value);
+  this->publish_state(value*100.0f);	
 }
 
 
 void FloatingEpointNumber::control(float value) {
   this->publish_state(value);
-  this->parent_->set_floating_epoint(value*0.01);
+  this->parent_->set_floating_epoint(value*0.01f);
   this->pref_.save(&value);
 }
 
 }  // namespace dualpid
 }  // namespace esphome
+
 
