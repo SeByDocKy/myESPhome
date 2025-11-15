b07ec7e6edbfd9b01fc37f04a32058867e34d72a
SeByDocKy <sebydocky@hotmail.com>
2025-11-15 20:45:03 +0100

    Update emerson_switch.cpp

--- components/emerson_r48/switch/emerson_switch.cpp
+++ components/emerson_r48/switch/emerson_switch.cpp
@@ -13,9 +13,12 @@ static const int8_t SET_DC_FUNCTION = 0x1;
 static const int8_t SET_FAN_FUNCTION = 0x2;
 static const int8_t SET_LED_FUNCTION = 0x3;
 
-
-
 void EmersonR48Switch::setup() {
+    // this->pref_ = global_preferences->make_preference<bool>(this->get_object_id_hash());
+    // if (!this->pref_.load(&state)) {
+    //     state = this->parent_->get_current_ac_switch(); 
+    // }
+    // this->parent_->set_current_ac_switch(state);
 
 }
 
@@ -25,12 +28,7 @@ void EmersonR48Switch::write_state(bool state) {
 
     switch (this->functionCode_) {
         case SET_AC_FUNCTION:
-           // this->pref_ = global_preferences->make_preference<bool>(this->get_object_id_hash());
-           // if (!this->pref_.load(&state)) {
-           //   state = this->parent_->get_current_ac_switch(); 
-           // }
-           // this->parent_->set_current_ac_switch(state);
-            
+             
             parent_->acOff_ = state;
             msgv = parent_->dcOff_ << 7 | parent_->fanFull_ << 4 | parent_->flashLed_ << 3 | parent_->acOff_ << 2 | 1;
             parent_->set_control(msgv);
