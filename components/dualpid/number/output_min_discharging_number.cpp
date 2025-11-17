82771e912420b73a3092d7bb36083b22210b2b0d
SeByDocKy <sebydocky@hotmail.com>
2025-11-17 18:36:13 +0100

    Update output_min_discharging_number.cpp

--- components/dualpid/number/output_min_discharging_number.cpp
+++ components/dualpid/number/output_min_discharging_number.cpp
@@ -7,8 +7,8 @@ void OutputMinDischargingNumber::setup() {
   float value;
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
   if (!this->pref_.load(&value)) value = this->parent_->get_output_min_discharging();
-  this->parent_->set_output_min_discharging(value);
-  this->publish_state(value*0.01f);
+  this->parent_->set_output_min_discharging(value*0.01f);
+  this->publish_state(value);
 }
 
 void OutputMinDischargingNumber::control(float value) {
@@ -19,6 +19,7 @@ void OutputMinDischargingNumber::control(float value) {
 
 }  // namespace dualpid
 }  // namespace esphome
+
 
 
 
