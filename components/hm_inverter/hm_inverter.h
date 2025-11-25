32077e163a648cf0268a04f5344645fa4230e1e3
SeByDocKy <sebydocky@hotmail.com>
2025-11-25 08:48:35 +0100

    Update hm_inverter.h

--- components/hm_inverter/hm_inverter.h
+++ components/hm_inverter/hm_inverter.h
@@ -121,6 +121,7 @@ class HmInverter : public esphome::Component {
         PalevelNumber *palevel_number_ = nullptr;
 
         esphome::binary_sensor::BinarySensor *is_reachable_sensor_ = nullptr;
+        esphome::binary_sensor::BinarySensor *is_producing_sensor_ = nullptr;
         esphome::sensor::Sensor *rssi_ = nullptr;
  
         std::shared_ptr<InverterAbstract> inverter_ = nullptr;
@@ -159,6 +160,7 @@ class HmInverter : public esphome::Component {
         int8_t get_oldpalevel() {return this->former_palevel_ ;}
 
         void set_is_reachable_sensor(esphome::binary_sensor::BinarySensor* sensor) { this->is_reachable_sensor_ = sensor; }
+        void set_is_producing_sensor(esphome::binary_sensor::BinarySensor* sensor) { this->is_producing_sensor_ = sensor; }
         void set_serial_no(std::string serial) { this->serial_ = std::stoll(serial, nullptr, 16); }
         uint64_t serial() { return this->serial_; }
         void set_inverter(std::shared_ptr<InverterAbstract> inverter) { this->inverter_ = inverter; }
@@ -196,6 +198,7 @@ class HmPlatform : public esphome::PollingComponent {
 }
 
 }
+
 
 
 
