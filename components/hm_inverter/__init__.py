8ef162ba39724c7c0891b619be5f02bb288dd348
SeByDocKy <sebydocky@hotmail.com>
2025-11-25 08:47:11 +0100

    Update __init__.py

--- components/hm_inverter/__init__.py
+++ components/hm_inverter/__init__.py
@@ -53,6 +53,7 @@ CONF_LIMIT_ABSOLUTE = "limit_absolute"
 CONF_PERCENT_OUTPUT = "percent_output" 
 CONF_PALEVEL = "palevel"
 CONF_REACHABLE = "reachable"
+CONF_PRODUCING = "producing"
 CONF_RESTART = "restart"
 
 CONF_POWER = "power"
@@ -137,6 +138,11 @@ INVERTER_SCHEMA = cv.Schema({
         unit_of_measurement="dBm",
     ),
     cv.Optional(CONF_REACHABLE): binary_sensor.binary_sensor_schema(
+        binary_sensor.BinarySensorInitiallyOff,
+        entity_category="diagnostic",
+        device_class="connectivity",
+    ),
+    cv.Optional(CONF_PRODUCING): binary_sensor.binary_sensor_schema(
         binary_sensor.BinarySensorInitiallyOff,
         entity_category="diagnostic",
         device_class="connectivity",
@@ -248,6 +254,8 @@ async def to_code(config):
             cg.add(btn.set_parent(inv_var))
         if CONF_REACHABLE in inv_conf:
             cg.add(inv_var.set_is_reachable_sensor(await binary_sensor.new_binary_sensor(inv_conf[CONF_REACHABLE])))
+        if CONF_PRODUCING in inv_conf:
+            cg.add(inv_var.set_is_producing_sensor(await binary_sensor.new_binary_sensor(inv_conf[CONF_PRODUCING])))    
 
     cg.add(var.set_mosi(await cg.gpio_pin_expression(config[CONF_PINS][CONF_MOSI])))
     cg.add(var.set_miso(await cg.gpio_pin_expression(config[CONF_PINS][CONF_MISO])))
@@ -256,6 +264,7 @@ async def to_code(config):
     cg.add(var.set_en(await cg.gpio_pin_expression(config[CONF_PINS][CONF_EN])))
 
     cg.add(var.set_irq(await cg.gpio_pin_expression(config[CONF_PINS][CONF_IRQ])))
+
 
 
 
