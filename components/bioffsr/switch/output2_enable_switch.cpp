0d84802d0f36598fbe058e872800c0c46387edd9
SeByDocKy <sebydocky@hotmail.com>
2025-11-27 11:09:50 +0100

    Update output2_enable_switch.cpp

--- components/bioffsr/switch/output2_enable_switch.cpp
+++ components/bioffsr/switch/output2_enable_switch.cpp
@@ -1,9 +1,9 @@
-#include "output2_activation_switch.h"
+#include "output2_enable_switch.h"
 
 namespace esphome {
 namespace bioffsr {
 	
-void Output2ActivationSwitch::setup() {
+void Output2EnableSwitch::setup() {
   bool state;
   this->pref_ = global_preferences->make_preference<bool>(this->get_object_id_hash());
   if (!this->pref_.load(&state)) state = this->parent_->get_output2_enable();
@@ -11,7 +11,7 @@ void Output2ActivationSwitch::setup() {
   this->parent_->set_output2_enable(state);
 }
 
-void Output2ActivationSwitch::write_state(bool state) {
+void Output2EnableSwitch::write_state(bool state) {
   this->publish_state(state);
   this->parent_->set_output2_enable(state);
   this->pref_.save(&state);
@@ -19,5 +19,6 @@ void Output2ActivationSwitch::write_state(bool state) {
 
 }  // namespace bioffsr
 }  // namespace esphome
+
 
 
