b4fca86d4574dd535bde947bf41bb57cfaa5b376
SeByDocKy <sebydocky@hotmail.com>
2025-12-06 16:17:21 +0100

    Refactor setup method and uncomment code

--- components/minipid/number/output_min_number.cpp
+++ components/minipid/number/output_min_number.cpp
@@ -7,13 +7,13 @@ void OutputMinNumber::setup() {
   float value;
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
 
-  if (!this->pref_.load(&value)) value = this->parent_->get_output_min()*1.0f;  // should be in [0-100%]
-  this->parent_->set_output_min(value);
-  this->publish_state(value*100.0f);	
+  // if (!this->pref_.load(&value)) value = this->parent_->get_output_min()*1.0f;  // should be in [0-100%]
+  // this->parent_->set_output_min(value);
+  // this->publish_state(value*100.0f);	
   
-  // if (!this->pref_.load(&value)) value = this->parent_->get_output_min();
-  // this->parent_->set_output_min(value*0.01);
-  // this->publish_state(value);
+  if (!this->pref_.load(&value)) value = this->parent_->get_output_min();
+  this->parent_->set_output_min(value*0.01);
+  this->publish_state(value);
 }
 
 void OutputMinNumber::control(float value) {
@@ -24,6 +24,7 @@ void OutputMinNumber::control(float value) {
 
 }  // namespace minipid
 }  // namespace esphome
+
 
 
 
