d6bea6202fbbbdf4144f5e795c61ea019b437421
SeByDocKy <sebydocky@hotmail.com>
2025-12-06 14:50:04 +0100

    Refactor OutputMaxNumber setup and control methods
    Updated comments to reflect percentage range and improved code formatting.

--- components/minipid/number/output_max_number.cpp
+++ components/minipid/number/output_max_number.cpp
@@ -6,12 +6,12 @@ namespace minipid {
 void OutputMaxNumber::setup() {
   float value;
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
-  if (!this->pref_.load(&value)) value = this->parent_->get_output_max()*100.0f;  // should be in [0-1]
+  if (!this->pref_.load(&value)) value = this->parent_->get_output_max()*100.0f;  // should be in [0-100%]
   this->parent_->set_output_max(value);
   this->publish_state(value*100.0f);	
 
 
-  // if (!this->pref_.load(&value)) value = this->parent_->get_output_max();  // should be in [0-1]
+  // if (!this->pref_.load(&value)) value = this->parent_->get_output_max();  // should be in [0-100%]
   // this->parent_->set_output_max(value*0.01);
   // this->publish_state(value);	
 }
@@ -24,6 +24,7 @@ void OutputMaxNumber::control(float value) {
 
 }  // namespace minipid
 }  // namespace esphome
+
 
 
 
