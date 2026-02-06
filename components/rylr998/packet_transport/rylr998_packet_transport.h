9c3a32f8121522c0a3f7607c470f836081cc4c9b
SeByDocKy <sebydocky@hotmail.com>
2026-02-06 21:11:27 +0100

    Refactor RYLR998PacketTransportComponent parent type

--- components/rylr998/packet_transport/rylr998_packet_transport.h
+++ components/rylr998/packet_transport/rylr998_packet_transport.h
@@ -7,17 +7,25 @@
 namespace esphome {
 namespace rylr998 {
 
-class RYLR998PacketTransportComponent : public packet_transport::PacketTransport, public Parented<RYLR998>, public RYLR998Listener {
+class RYLR998PacketTransportComponent : public packet_transport::PacketTransport, public Parented<RYLR998Component>, public RYLR998Listener {
  public:
-  void set_parent(RYLR998Component *parent) { this->parent_ = parent; }
   
   void setup() override;
-  void dump_config() override;
+
+  // void set_parent(RYLR998Component *parent) { this->parent_ = parent; }
+
+  void on_packet(const std::vector<uint8_t> &packet, float rssi, float snr) override;
+  float get_setup_priority() const override { return setup_priority::AFTER_WIFI; }
   
-  void send_packet(const uint8_t *data, size_t len) override;
+  // void dump_config() override;
+  
+  // void send_packet(const uint8_t *data, size_t len) override;
   
  protected:
-  RYLR998Component *parent_{nullptr};
+  // RYLR998Component *parent_{nullptr};
+  void send_packet(const std::vector<uint8_t> &buf) const override;
+  bool should_send() override { return true; }
+  size_t get_max_packet_size() override { return this->parent_->get_max_packet_size(); }
 };
 
 }  // namespace rylr998
