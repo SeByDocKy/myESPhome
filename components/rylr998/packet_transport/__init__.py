1ceec1bb66018dbff8ca4644dde3171ced61c824
SeByDocKy <sebydocky@hotmail.com>
2026-02-06 20:13:36 +0100

    Refactor __init__.py by removing unused code
    Removed commented-out code and cleaned up the init file.

--- components/rylr998/packet_transport/__init__.py
+++ components/rylr998/packet_transport/__init__.py
@@ -8,29 +8,13 @@ from esphome.components.packet_transport import (
 )
 from esphome.const import CONF_ID
 
-# Import from parent rylr998 component
 from .. import CONF_RYLR998_ID, RYLR998Component, rylr998_ns
 
 DEPENDENCIES = ["rylr998"]
 
-# RYLR998PacketTransportComponent = rylr998_ns.class_(
-#     "RYLR998PacketTransportComponent", packet_transport.PacketTransportComponent
-# )
-
-
 RYLR998PacketTransportComponent = rylr998_ns.class_(
     "RYLR998PacketTransportComponent", PacketTransport
 )
-
-
-# CONFIG_SCHEMA = packet_transport.PACKET_TRANSPORT_SCHEMA.extend(
-#     {
-#         cv.GenerateID(): cv.declare_id(RYLR998PacketTransportComponent),
-#         cv.GenerateID(packet_transport.CONF_PACKET_TRANSPORT_ID): cv.use_id(
-#             RYLR998Component
-#         ),
-#     }
-# )
 
 
 CONFIG_SCHEMA = transport_schema(RYLR998PacketTransportComponent).extend(
@@ -38,22 +22,6 @@ CONFIG_SCHEMA = transport_schema(RYLR998PacketTransportComponent).extend(
         cv.GenerateID(CONF_RYLR998_ID): cv.use_id(RYLR998Component),
     }
 )
-
-
-# async def to_code(config):
-#     var = cg.new_Pvariable(config[CONF_ID])
-#     await packet_transport.register_packet_transport(var, config)
-    
-#     parent = await cg.get_variable(config[packet_transport.CONF_PACKET_TRANSPORT_ID])
-#     cg.add(var.set_parent(parent))
-
-
-
-# async def to_code(config):
-#     parent = await cg.get_variable(config[CONF_RYLR998_ID])
-#     var = await new_packet_transport(config, parent)
-#     parent = await cg.get_variable(config[CONF_RYLR998_ID])
-#     cg.add(var.set_parent(parent)) 
 
 
 async def to_code(config):
