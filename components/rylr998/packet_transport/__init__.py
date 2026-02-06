c93070c702df23eac7787caf5a84b1eebb889987
SeByDocKy <sebydocky@hotmail.com>
2026-02-06 19:44:32 +0100

    Refactor packet transport initialization and code structure

--- components/rylr998/packet_transport/__init__.py
+++ components/rylr998/packet_transport/__init__.py
@@ -1,11 +1,11 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import packet_transport
-# from esphome.components.packet_transport import (
-#     PacketTransport,
-#     new_packet_transport,
-#     transport_schema,
-# )
+from esphome.components.packet_transport import (
+    PacketTransport,
+    new_packet_transport,
+    transport_schema,
+)
 from esphome.const import CONF_ID
 
 # Import from parent rylr998 component
@@ -13,23 +13,24 @@ from .. import CONF_RYLR998_ID, RYLR998Component, rylr998_ns
 
 DEPENDENCIES = ["rylr998"]
 
-RYLR998PacketTransportComponent = rylr998_ns.class_(
-    "RYLR998PacketTransportComponent", packet_transport.PacketTransportComponent
-)
-
 # RYLR998PacketTransportComponent = rylr998_ns.class_(
-#     "RYLR998PacketTransportComponent", PacketTransport
+#     "RYLR998PacketTransportComponent", packet_transport.PacketTransportComponent
 # )
 
 
-CONFIG_SCHEMA = packet_transport.PACKET_TRANSPORT_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(RYLR998PacketTransportComponent),
-        cv.GenerateID(packet_transport.CONF_PACKET_TRANSPORT_ID): cv.use_id(
-            RYLR998Component
-        ),
-    }
+RYLR998PacketTransportComponent = rylr998_ns.class_(
+    "RYLR998PacketTransportComponent", PacketTransport
 )
+
+
+# CONFIG_SCHEMA = packet_transport.PACKET_TRANSPORT_SCHEMA.extend(
+#     {
+#         cv.GenerateID(): cv.declare_id(RYLR998PacketTransportComponent),
+#         cv.GenerateID(packet_transport.CONF_PACKET_TRANSPORT_ID): cv.use_id(
+#             RYLR998Component
+#         ),
+#     }
+# )
 
 
 # CONFIG_SCHEMA = transport_schema(RYLR998PacketTransportComponent).extend(
@@ -39,9 +40,15 @@ CONFIG_SCHEMA = packet_transport.PACKET_TRANSPORT_SCHEMA.extend(
 # )
 
 
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await packet_transport.register_packet_transport(var, config)
+# async def to_code(config):
+#     var = cg.new_Pvariable(config[CONF_ID])
+#     await packet_transport.register_packet_transport(var, config)
     
-    parent = await cg.get_variable(config[packet_transport.CONF_PACKET_TRANSPORT_ID])
-    cg.add(var.set_parent(parent))
+#     parent = await cg.get_variable(config[packet_transport.CONF_PACKET_TRANSPORT_ID])
+#     cg.add(var.set_parent(parent))
+
+
+async def to_code(config):
+    parent = await cg.get_variable(config[CONF_RYLR998_ID])
+    var = await new_packet_transport(config, parent)
+
