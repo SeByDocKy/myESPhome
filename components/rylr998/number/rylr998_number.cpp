f018db2be7403af4287b7707f13027d1b391336d
SeByDocKy <sebydocky@hotmail.com>
2026-02-18 19:22:32 +0100

    Update rylr998_number.cpp

--- components/rylr998/number/rylr998_number.cpp
+++ components/rylr998/number/rylr998_number.cpp
@@ -12,21 +12,16 @@ void RYLR998TxPowerNumber::setup() {
   uint8_t value;
   this->pref_ = global_preferences->make_preference<uint8_t>(this->get_object_id_hash());
   if (!this->pref_.load(&value)) value = this->parent_->get_tx_power();
-  this->parent_->set_tx_power(value);
+  this->parent_->apply_tx_power(value);
   this->publish_state(value);
-
-
 }
 
 
 void RYLR998TxPowerNumber::control(float value) {
   // value est déjà clampée par ESPHome entre min_value et max_value
   uint8_t power = static_cast<uint8_t>(value);
-
   ESP_LOGD(TAG, "TX power control: %.0f dBm requested", value);
-
-  this->parent_->set_tx_power(power);
-  
+  this->parent_->apply_tx_power(power);
   this->publish_state(value);
 }
 
