b3a7edd844f5266f003db54aa3d0a0f1ba36eb27
SeByDocKy <sebydocky@hotmail.com>
2026-02-18 19:19:47 +0100

    Update rylr998_number.cpp

--- components/rylr998/number/rylr998_number.cpp
+++ components/rylr998/number/rylr998_number.cpp
@@ -7,16 +7,26 @@ namespace rylr998 {
 
 static const char *const TAG = "rylr998.number";
 
+void RYLR998TxPowerNumber::setup() {
+
+  uint8_t value;
+  this->pref_ = global_preferences->make_preference<uint8_t>(this->get_object_id_hash());
+  if (!this->pref_.load(&value)) value = this->parent_->get_tx_power();
+  this->parent_->set_tx_power(value);
+  this->publish_state(value);
+
+
+}
+
+
 void RYLR998TxPowerNumber::control(float value) {
   // value est déjà clampée par ESPHome entre min_value et max_value
   uint8_t power = static_cast<uint8_t>(value);
 
   ESP_LOGD(TAG, "TX power control: %.0f dBm requested", value);
 
-  // Envoie AT+CRFOP=<power> et met à jour tx_power_ dans le composant
-  this->parent_->apply_tx_power(power);
-
-  // Publie la nouvelle valeur dans Home Assistant
+  this->parent_->set_tx_power(power);
+  
   this->publish_state(value);
 }
 
