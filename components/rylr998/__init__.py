ef9ed65890dcb1a3733823b3dacea2077ad6f89f
SeByDocKy <sebydocky@hotmail.com>
2026-03-22 15:51:22 +0100

    Update __init__.py

--- components/rylr998/__init__.py
+++ components/rylr998/__init__.py
@@ -85,7 +85,7 @@ CONFIG_SCHEMA = (
             cv.Optional(CONF_PREAMBLE_LENGTH, default=12): cv.int_range(min=4, max=24),
             cv.Optional(CONF_NETWORK_ID, default=18): validate_network_id,
             cv.Optional(CONF_TX_POWER, default=22): cv.int_range(min=0, max=22),
-            cv.Optional(CONF_AIR_TIME, default=True): cv.boolean(min=0, max=22),
+            cv.Optional(CONF_AIR_TIME, default=False): cv.boolean,
             cv.Optional(CONF_ON_PACKET): automation.validate_automation(
                 {
                     cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(RYLR998PacketTrigger),
@@ -111,6 +111,7 @@ async def to_code(config):
     cg.add(var.set_preamble_length(config[CONF_PREAMBLE_LENGTH]))
     cg.add(var.set_network_id(config[CONF_NETWORK_ID]))
     cg.add(var.set_tx_power(config[CONF_TX_POWER]))
+    cg.add(var.set_air_time(config[CONF_AIR_TIME]))
 
     for conf in config.get(CONF_ON_PACKET, []):
         trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID])
