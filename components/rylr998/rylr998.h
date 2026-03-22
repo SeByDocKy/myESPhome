568c56961119694d198a067e602f97a0e1072fb2
SeByDocKy <sebydocky@hotmail.com>
2026-03-22 15:06:37 +0100

    Update rylr998.h

--- components/rylr998/rylr998.h
+++ components/rylr998/rylr998.h
@@ -77,7 +77,7 @@ class RYLR998Component : public Component, public uart::UARTDevice {
 
   // TX rate limiting
   uint32_t last_tx_time_{0};
-  # static const uint32_t TX_MIN_INTERVAL_MS = 600;
+  // static const uint32_t TX_MIN_INTERVAL_MS = 600;
 
   bool send_command_(const std::string &command, uint32_t timeout_ms = 1000);
   void send_raw_(const std::string &command);
