997bc2003600adfd9cfc7e07b452092d2410a56f
SeByDocKy <sebydocky@hotmail.com>
2026-04-12 21:29:48 +0200

    Update dualpidpcm_binary_sensor.h

---
+++ components/dualpidpcm/binary_sensor/dualpidpcm_binary_sensor.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "../dualpidpcm.h"
+
+namespace esphome {
+namespace dualpidpcm {
+
+class DUALPIDPCMBinarySensor : public Component, public Parented<DUALPIDPCMComponent> {
+ public:
+  void dump_config() override;
+  void setup() override;
+  
+  void set_parent(DUALPIDPCMComponent *parent) { this->parent_ = parent; }
+  void set_deadband_binary_sensor(binary_sensor::BinarySensor *bs) { this->deadband_binary_sensor_ = bs; }; 
+
+ protected:
+  
+  binary_sensor::BinarySensor *deadband_binary_sensor_{nullptr};
+  DUALPIDPCMComponent *parent_;
+  
+  void publish_data_();
+};
+
+} // dualpidpcm
+} // esphome
