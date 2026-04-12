9d972bc77b0759b5411e1e7cc5ff12261fd63281
SeByDocKy <sebydocky@hotmail.com>
2026-04-12 21:32:47 +0200

    Update dualpidpcm_binary_sensor.cpp

---
+++ components/dualpidpcm/binary_sensor/dualpidpcm_binary_sensor.cpp
@@ -0,0 +1,25 @@
+#include "dualpidpcm_binary_sensor.h"
+#include "esphome/core/helpers.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace dualpidpcm {
+	
+static const char *const TAG = "dualpidpcm.binarysensor";
+
+void DUALPIDPCMBinarySensor::setup() {	
+  this->parent_->add_on_pid_computed_callback([this]() { this->publish_data_(); });
+}
+
+void DUALPIDPCMBinarySensor::dump_config() {
+  ESP_LOGCONFIG(TAG, "DUALPIDPCM Binary Sensor:");
+  LOG_BINARY_SENSOR("  ", "deadband", this->deadband_binary_sensor_);
+}	
+
+void DUALPIDPCMBinarySensor::publish_data_() {
+  if (this->deadband_binary_sensor_ != nullptr)
+    this->deadband_binary_sensor_->publish_state(this->parent_->get_deadband());  
+}
+
+} // dualpidpcm
+} // esphome
