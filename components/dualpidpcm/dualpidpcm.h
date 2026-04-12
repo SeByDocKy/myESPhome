7cd0ad263575887a147f2b6728dd8cdb99b2517d
SeByDocKy <sebydocky@hotmail.com>
2026-04-12 13:11:55 +0200

    Update dualpidpcm.h

--- components/dualpidpcm/dualpidpcm.h
+++ components/dualpidpcm/dualpidpcm.h
@@ -79,7 +79,6 @@ SUB_NUMBER(output_max_discharging)
     pid_computed_callback_.add(std::move(callback));
   }
   
-#ifdef USE_SWITCH 
   void set_activation(bool enable) {this->current_activation_ = enable;}
   bool get_activation(void){return this->current_activation_;}
   void set_manual_override(bool enable) {this->current_manual_override_ = enable;}
@@ -88,27 +87,26 @@ SUB_NUMBER(output_max_discharging)
   bool get_pid_mode(void){return this->current_pid_mode_;}
   void set_reverse(bool enable) {this->current_reverse_ = enable;}
   bool get_reverse(void){return this->current_reverse_;}
-#endif
 
-#ifdef USE_NUMBER
+
 
   void set_setpoint(float value) {this->current_setpoint_ = value;}
   float get_setpoint(void){return this->current_setpoint_;}
   
-  void set_charging_epoint(float value) {this->current_charging_epoint_ = value;}
-  float get_charging_epoint(void){return this->current_charging_epoint_;}
-  void set_absorbing_epoint(float value) {this->current_absorbing_epoint_ = value;}
-  float get_absorbing_epoint(void){return this->current_absorbing_epoint_;}
-  void set_floating_epoint(float value) {this->current_floating_epoint_ = value;}
-  float get_floating_epoint(void){return this->current_floating_epoint_;}
+  // void set_charging_epoint(float value) {this->current_charging_epoint_ = value;}
+  // float get_charging_epoint(void){return this->current_charging_epoint_;}
+  // void set_absorbing_epoint(float value) {this->current_absorbing_epoint_ = value;}
+  // float get_absorbing_epoint(void){return this->current_absorbing_epoint_;}
+  // void set_floating_epoint(float value) {this->current_floating_epoint_ = value;}
+  // float get_floating_epoint(void){return this->current_floating_epoint_;}
   
   void set_starting_battery_voltage(float value) {this->current_starting_battery_voltage_ = value;}
   float get_starting_battery_voltage(void){return this->current_starting_battery_voltage_;}
   
-  void set_charged_battery_voltage(float value) {this->current_charged_battery_voltage_ = value;}
-  float get_charged_battery_voltage(void){return this->current_charged_battery_voltage_;}
-  void set_discharged_battery_voltage(float value) {this->current_discharged_battery_voltage_ = value;}
-  float get_discharged_battery_voltage(void){return this->current_discharged_battery_voltage_;}
+  // void set_charged_battery_voltage(float value) {this->current_charged_battery_voltage_ = value;}
+  // float get_charged_battery_voltage(void){return this->current_charged_battery_voltage_;}
+  // void set_discharged_battery_voltage(float value) {this->current_discharged_battery_voltage_ = value;}
+  // float get_discharged_battery_voltage(void){return this->current_discharged_battery_voltage_;}
   
   void set_kp_charging(float value) {this->current_kp_charging_ = value;}
   float get_kp_charging(void){return this->current_kp_charging_;}
@@ -124,10 +122,10 @@ SUB_NUMBER(output_max_discharging)
   void set_kd_discharging(float value) {this->current_kd_discharging_ = value;}
   float get_kd_discharging(void){return this->current_kd_discharging_;}
   
-  void set_output_min(float value) {this->current_output_min_ = value;}
-  float get_output_min(void){return this->current_output_min_;}
-  void set_output_max(float value) {this->current_output_max_ = value;}
-  float get_output_max(void){return this->current_output_max_;}
+  // void set_output_min(float value) {this->current_output_min_ = value;}
+  // float get_output_min(void){return this->current_output_min_;}
+  // void set_output_max(float value) {this->current_output_max_ = value;}
+  // float get_output_max(void){return this->current_output_max_;}
 
   void set_output_min_charging(float value) {this->current_output_min_charging_ = value;}
   float get_output_min_charging(void){return this->current_output_min_charging_;}
@@ -139,28 +137,19 @@ SUB_NUMBER(output_max_discharging)
   void set_output_max_discharging(float value) {this->current_output_max_discharging_ = value;}
   float get_output_max_discharging(void){return this->current_output_max_discharging_;}
   
-#endif
-
-
-#ifdef USE_SENSOR
   float get_error(void) { return this->current_error_; }
-  // float get_output(void) { return this->current_output_; }
   float get_output_charging(void) { return this->current_output_charging_; }
   float get_output_discharging(void) { return this->current_output_discharging_; }  
-  // float get_target(void) { return this->current_target_; }
-  // float get_epoint(void) { return this->current_epoint_; }
   float get_input(void)  { return this->current_input_; }
-#endif
 
  protected:
   uint32_t last_time_ = 0;
   float dt_;
   float error_ = 0.0f;
   float previous_error_ = 0.0f;
-  // float output_ = 0.5f;
   float output_charging_ = 0.0f;
   float output_discharging_ = 0.0f;
-  float previous_output_ = 0.5f;
+  // float previous_output_ = 0.5f;
   float previous_output_charging_ = 0.0f;
   float previous_output_discharging_ = 0.0f;
   float integral_= 0.0f; 
@@ -168,9 +157,9 @@ SUB_NUMBER(output_max_discharging)
   float current_min_charging_ = 5.0f;
   float current_min_discharging_ = 5.0f;
   
-  float current_charging_epoint_ = 50.0f; // 0.5f;
-  float current_absorbing_epoint_ = 2.0f; // 0.2f;
-  float current_floating_epoint_ = 0.0f;// 0.0f;
+  // float current_charging_epoint_ = 50.0f; // 0.5f;
+  // float current_absorbing_epoint_ = 2.0f; // 0.2f;
+  // float current_floating_epoint_ = 0.0f;// 0.0f;
   
   // float current_input_ = 0.0f;
   float current_battery_voltage_ = 54.0f;
@@ -186,33 +175,25 @@ SUB_NUMBER(output_max_discharging)
     
   CallbackManager<void()> pid_computed_callback_;
 
-#ifdef USE_SENSOR
   float current_error_ = 0.0f;
-  // float current_output_ = 0.0f;
   float current_output_charging_ = 0.0f;
   float current_output_discharging_ = 0.0f;
-  // float current_target_ = 0.0f;
-  // float current_epoint_ = 50.0f;
   float current_input_ = 0.0f;
-#endif   
-
-#ifdef USE_SWITCH  
+ 
   bool current_activation_ = false;
   bool current_manual_override_ = false;
   bool current_pid_mode_ = false;
   bool current_reverse_ = false;
-#endif  
-  
-#ifdef USE_NUMBER
-  float current_setpoint_ = 0.0f;
 
+
+  float current_setpoint_ = 0.0f;
   float current_starting_battery_voltage_ = 50.0f;
   float current_charged_battery_voltage_ = 55.8f;
   float current_discharged_battery_voltage_ = 55.6f;
   
-  float current_kp_ = 3.0f;
-  float current_ki_ = 0.0f;
-  float current_kd_ = 0.0f;
+  // float current_kp_ = 3.0f;
+  // float current_ki_ = 0.0f;
+  // float current_kd_ = 0.0f;
   
   float current_kp_charging_ = 3.0f;
   float current_ki_charging_ = 0.0f;
@@ -222,16 +203,11 @@ SUB_NUMBER(output_max_discharging)
   float current_ki_discharging_ = 0.0f;
   float current_kd_discharging_ = 0.0f;  
   
-  float current_output_max_ = 100.0f;
-  float current_output_min_ = 0.0f;
-
   float current_output_max_charging_ = 100.0f;
   float current_output_min_charging_ = 0.0f;
 
   float current_output_max_discharging_ = 100.0f;
   float current_output_min_discharging_ = 0.0f;
-
-#endif
 
 };
 		
