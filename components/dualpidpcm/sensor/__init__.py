807fd21be193398e117ecb4a9a6f40d1f342e736
SeByDocKy <sebydocky@hotmail.com>
2026-04-12 12:20:43 +0200

    Update __init__.py

--- components/dualpidpcm/sensor/__init__.py
+++ components/dualpidpcm/sensor/__init__.py
@@ -41,14 +41,7 @@ CONFIG_SCHEMA = {
                 unit_of_measurement=UNIT_WATT,
                 device_class=DEVICE_CLASS_POWER,
                 state_class=STATE_CLASS_MEASUREMENT,
-             ),
-    # cv.Optional(CONF_OUTPUT): sensor.sensor_schema(
-    #             accuracy_decimals=2,
-    #             unit_of_measurement=UNIT_PERCENT,
-    #             icon = ICON_PERCENT,
-    #             state_class=STATE_CLASS_MEASUREMENT,
-    #          ),
-             
+             ),           
     cv.Optional(CONF_OUTPUT_CHARGING): sensor.sensor_schema(
                 accuracy_decimals=2,
                 unit_of_measurement=UNIT_PERCENT,
@@ -62,20 +55,7 @@ CONFIG_SCHEMA = {
                 icon = ICON_PERCENT,
                 state_class=STATE_CLASS_MEASUREMENT,
              ),             
-    # cv.Optional(CONF_TARGET): sensor.sensor_schema(
-    #             unit_of_measurement=UNIT_WATT,
-    #             icon=ICON_TARGET,
-    #             accuracy_decimals=1,
-    #             device_class=DEVICE_CLASS_POWER,
-    #             state_class=STATE_CLASS_MEASUREMENT,
-    #          ),
-    #  cv.Optional(CONF_EPOINT): sensor.sensor_schema(
-    #             accuracy_decimals=1,
-    #             unit_of_measurement=UNIT_PERCENT,
-    #             icon = ICON_PERCENT,
-    #             state_class=STATE_CLASS_MEASUREMENT,
-    #          ),
-     cv.Optional(CONF_INPUT): sensor.sensor_schema(
+    cv.Optional(CONF_INPUT): sensor.sensor_schema(
                 unit_of_measurement=UNIT_WATT,
                 icon=ICON_INPORT,
                 accuracy_decimals=1,
@@ -93,10 +73,6 @@ async def to_code(config):
     if CONF_ERROR in config:
         sens = await sensor.new_sensor(config[CONF_ERROR])
         cg.add(var.set_error_sensor(sens))
-
-    # if CONF_OUTPUT in config:
-    #     sens = await sensor.new_sensor(config[CONF_OUTPUT])
-    #     cg.add(var.set_output_sensor(sens))
         
     if CONF_OUTPUT_CHARGING in config:
         sens = await sensor.new_sensor(config[CONF_OUTPUT_CHARGING])
@@ -106,14 +82,6 @@ async def to_code(config):
         sens = await sensor.new_sensor(config[CONF_OUTPUT_DISCHARGING])
         cg.add(var.set_output_discharging_sensor(sens))        
         
-    # if CONF_TARGET in config:
-    #     sens = await sensor.new_sensor(config[CONF_TARGET])
-    #     cg.add(var.set_target_sensor(sens))
-   
-    # if CONF_EPOINT in config:
-    #     sens = await sensor.new_sensor(config[CONF_EPOINT])
-    #     cg.add(var.set_epoint_sensor(sens))
-
     if CONF_INPUT in config:
         sens = await sensor.new_sensor(config[CONF_INPUT])
         cg.add(var.set_input_sensor(sens))
