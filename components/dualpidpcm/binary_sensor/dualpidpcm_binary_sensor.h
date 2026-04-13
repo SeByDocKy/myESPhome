d3b6b17b9be8737e838898baadaaf31a365a2450
SeByDocKy <sebydocky@hotmail.com>
2026-04-13 11:19:17 +0200

    Update dualpidpcm_binary_sensor.h

--- components/dualpidpcm/binary_sensor/dualpidpcm_binary_sensor.h
+++ components/dualpidpcm/binary_sensor/dualpidpcm_binary_sensor.h
@@ -12,11 +12,13 @@ class DUALPIDPCMBinarySensor : public Component, public Parented<DUALPIDPCMCompo
   void setup() override;
   
   void set_parent(DUALPIDPCMComponent *parent) { this->parent_ = parent; }
-  void set_deadband_binary_sensor(binary_sensor::BinarySensor *bs) { this->deadband_binary_sensor_ = bs; }; 
+  void set_deadband_binary_sensor(binary_sensor::BinarySensor *bs) { this->deadband_binary_sensor_ = bs; }
+  void set_swap_binary_sensor(binary_sensor::BinarySensor *bs) { this->swap_binary_sensor_ = bs; }
 
  protected:
   
   binary_sensor::BinarySensor *deadband_binary_sensor_{nullptr};
+  binary_sensor::BinarySensor *swap_binary_sensor_{nullptr};
   DUALPIDPCMComponent *parent_;
   
   void publish_data_();
