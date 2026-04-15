a57bc88d2b2a4ac07cfba1e7489b367a9e33bc8a
SeByDocKy <sebydocky@hotmail.com>
2026-04-15 22:07:59 +0200

    Update dualpidpcm_sensor.h

--- components/dualpidpcm/sensor/dualpidpcm_sensor.h
+++ components/dualpidpcm/sensor/dualpidpcm_sensor.h
@@ -13,6 +13,7 @@ class DUALPIDPCMSensor : public Component, public Parented<DUALPIDPCMComponent> 
   void set_parent(DUALPIDPCMComponent *parent) { this->parent_ = parent; }
   void set_input_sensor(sensor::Sensor *sensor) { this->input_sensor_ = sensor; }
   void set_error_sensor(sensor::Sensor *sensor) { this->error_sensor_ = sensor; }
+  void set_output_sensor(sensor::Sensor *sensor) { this->output_sensor_ = sensor; }
   void set_output_charging_sensor(sensor::Sensor *sensor) { this->output_charging_sensor_ = sensor; }
   void set_output_discharging_sensor(sensor::Sensor *sensor) { this->output_discharging_sensor_ = sensor; }
   
