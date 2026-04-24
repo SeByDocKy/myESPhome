80df24fa93294452a04c6f7d0b4fab6309f64354
SeByDocKy <sebydocky@hotmail.com>
2026-04-24 21:28:59 +0200

    Update dualpidpcm.cpp

--- components/dualpidpcm/dualpidpcm.cpp
+++ components/dualpidpcm/dualpidpcm.cpp
@@ -104,7 +104,7 @@ namespace dualpidpcm {
 
 
 
-      if ((this->current_output_ < this->epoint_ + 0*(this->offcharge_ != 0)*this->eub_) & (this->offcharge_ >= 0) & (this->offcharge_ < MAX_OFFCHARGE) & (this->offdischarge_ == 0)){  // charge   offcharge
+      if ((this->current_output_ < this->epoint_ ) ){  // charge   offcharge
 
 	    this->current_kp_ = this->current_kp_charging_;
 	    this->current_ki_ = this->current_ki_charging_;
@@ -142,7 +142,7 @@ namespace dualpidpcm {
 	    // this->previous_output_    = this->current_epoint_;  
   
 	  }
-	  if ( (this->current_output_ >= this->epoint_   - 0*(this->offdischarge_ != 0)*this->elb_ ) & (this->offdischarge_ >= 0) & (this->offdischarge_ < MAX_OFFDISCHARGE) & (this->offcharge_ == 0)) {// if (this->current_output_ > this->epoint_ + this->eub_){ //discharge
+	  if ( (this->current_output_ >= this->epoint_    ) ) {// if (this->current_output_ > this->epoint_ + this->eub_){ //discharge
 	    this->current_kp_ = this->current_kp_discharging_;
 	    this->current_ki_ = this->current_ki_discharging_;
 	    this->current_kd_ = this->current_kd_discharging_;
@@ -377,13 +377,13 @@ namespace dualpidpcm {
 	
 	  ESP_LOGI(TAG, "Final computed output_charging_=%1.6f, output_discharging_=%1.6f" , this->output_charging_, this->output_discharging_);  
 
-	  if ((this->output_charging_ != this->previous_output_charging_) & (this->onoff_switch_->state==true) & (this->offcharge_==0) ){
+	  if ((this->output_charging_ != this->previous_output_charging_) & (this->onoff_switch_->state==true)  ){
         if (this->output_charging_ > 0.0f){ 
 		  this->device_charging_output_->set_level(this->output_charging_);          // send command to PCM must be in [0.0 - 1.0] //
 	      delay(SET_OUTPUT_DELAY);
 		}
 	  }
-	  if ((this->output_discharging_ != this->previous_output_discharging_) & (this->onoff_switch_->state==true) & (this->offdischarge_==0)){  
+	  if ((this->output_discharging_ != this->previous_output_discharging_) & (this->onoff_switch_->state==true) ){  
 	    if (this->output_discharging_ > 0.0f){ 
 		  this->device_discharging_output_->set_level(this->output_discharging_);    // send command to PCM, must be in [0.0 - 1.0] //
           delay(SET_OUTPUT_DELAY);
