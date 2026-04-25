83d0661491a98b112932f0a9ce53a7b018e9a695
SeByDocKy <sebydocky@hotmail.com>
2026-04-25 09:38:58 +0200

    Update __init__.py

--- components/dualpidpcm/number/__init__.py
+++ components/dualpidpcm/number/__init__.py
@@ -22,13 +22,21 @@ from .. import CONF_DUALPIDPCM_ID, DUALPIDPCMComponent, dualpidpcm_ns
 
 SetpointNumber = dualpidpcm_ns.class_("SetpointNumber", number.Number, cg.Component)
 StartingBatteryVoltageNumber = dualpidpcm_ns.class_("StartingBatteryVoltageNumber", number.Number, cg.Component)
-KpChargingNumber = dualpidpcm_ns.class_("KpChargingNumber", number.Number, cg.Component)
-KiChargingNumber = dualpidpcm_ns.class_("KiChargingNumber", number.Number, cg.Component)
-KdChargingNumber = dualpidpcm_ns.class_("KdChargingNumber", number.Number, cg.Component)
 
-KpDischargingNumber = dualpidpcm_ns.class_("KpDischargingNumber", number.Number, cg.Component)
-KiDischargingNumber = dualpidpcm_ns.class_("KiDischargingNumber", number.Number, cg.Component)
-KdDischargingNumber = dualpidpcm_ns.class_("KdDischargingNumber", number.Number, cg.Component)
+
+KpNumber = dualpidpcm_ns.class_("KpNumber", number.Number, cg.Component)
+KiNumber = dualpidpcm_ns.class_("KiNumber", number.Number, cg.Component)
+KdNumber = dualpidpcm_ns.class_("KdNumber", number.Number, cg.Component)
+
+
+
+# KpChargingNumber = dualpidpcm_ns.class_("KpChargingNumber", number.Number, cg.Component)
+# KiChargingNumber = dualpidpcm_ns.class_("KiChargingNumber", number.Number, cg.Component)
+# KdChargingNumber = dualpidpcm_ns.class_("KdChargingNumber", number.Number, cg.Component)
+
+# KpDischargingNumber = dualpidpcm_ns.class_("KpDischargingNumber", number.Number, cg.Component)
+# KiDischargingNumber = dualpidpcm_ns.class_("KiDischargingNumber", number.Number, cg.Component)
+# KdDischargingNumber = dualpidpcm_ns.class_("KdDischargingNumber", number.Number, cg.Component)
 
 OutputMinChargingNumber = dualpidpcm_ns.class_("OutputMinChargingNumber", number.Number, cg.Component)
 OutputMaxChargingNumber = dualpidpcm_ns.class_("OutputMaxChargingNumber", number.Number, cg.Component)
@@ -40,13 +48,18 @@ OutputMaxDischargingNumber = dualpidpcm_ns.class_("OutputMaxDischargingNumber", 
 CONF_SETPOINT = "setpoint"
 CONF_STARTING_BATTERY_VOLTAGE = "starting_battery_voltage"
 
-CONF_KP_CHARGING = "kp_charging"
-CONF_KI_CHARGING = "ki_charging"
-CONF_KD_CHARGING = "kd_charging"
 
-CONF_KP_DISCHARGING = "kp_discharging"
-CONF_KI_DISCHARGING = "ki_discharging"
-CONF_KD_DISCHARGING = "kd_discharging"
+CONF_KP = "kp"
+CONF_KI = "ki"
+CONF_KD = "kd"
+
+# CONF_KP_CHARGING = "kp_charging"
+# CONF_KI_CHARGING = "ki_charging"
+# CONF_KD_CHARGING = "kd_charging"
+
+# CONF_KP_DISCHARGING = "kp_discharging"
+# CONF_KI_DISCHARGING = "ki_discharging"
+# CONF_KD_DISCHARGING = "kd_discharging"
 
 CONF_OUTPUT_MIN_CHARGING = "output_min_charging"
 CONF_OUTPUT_MAX_CHARGING = "output_max_charging"
@@ -73,36 +86,53 @@ CONFIG_SCHEMA = {
         unit_of_measurement=UNIT_VOLT,
         entity_category=ENTITY_CATEGORY_CONFIG
     ).extend(cv.COMPONENT_SCHEMA),
+
+
+    cv.Optional(CONF_KP): number.number_schema(
+        KpNumber,
+        entity_category=ENTITY_CATEGORY_CONFIG
+    ).extend(cv.COMPONENT_SCHEMA),
+    
+    cv.Optional(CONF_KI): number.number_schema(
+        KiNumber,
+        entity_category=ENTITY_CATEGORY_CONFIG
+    ).extend(cv.COMPONENT_SCHEMA),
+    
+    cv.Optional(CONF_KD): number.number_schema(
+        KdNumber,
+        entity_category=ENTITY_CATEGORY_CONFIG
+    ).extend(cv.COMPONENT_SCHEMA),
+
         
-    cv.Optional(CONF_KP_CHARGING): number.number_schema(
-        KpChargingNumber,
-        entity_category=ENTITY_CATEGORY_CONFIG
-    ).extend(cv.COMPONENT_SCHEMA),
+    # cv.Optional(CONF_KP_CHARGING): number.number_schema(
+    #     KpChargingNumber,
+    #     entity_category=ENTITY_CATEGORY_CONFIG
+    # ).extend(cv.COMPONENT_SCHEMA),
     
-    cv.Optional(CONF_KI_CHARGING): number.number_schema(
-        KiChargingNumber,
-        entity_category=ENTITY_CATEGORY_CONFIG
-    ).extend(cv.COMPONENT_SCHEMA),
+    # cv.Optional(CONF_KI_CHARGING): number.number_schema(
+    #     KiChargingNumber,
+    #     entity_category=ENTITY_CATEGORY_CONFIG
+    # ).extend(cv.COMPONENT_SCHEMA),
     
-    cv.Optional(CONF_KD_CHARGING): number.number_schema(
-        KdChargingNumber,
-        entity_category=ENTITY_CATEGORY_CONFIG
-    ).extend(cv.COMPONENT_SCHEMA),
+    # cv.Optional(CONF_KD_CHARGING): number.number_schema(
+    #     KdChargingNumber,
+    #     entity_category=ENTITY_CATEGORY_CONFIG
+    # ).extend(cv.COMPONENT_SCHEMA),
     
-    cv.Optional(CONF_KP_DISCHARGING): number.number_schema(
-        KpDischargingNumber,
-        entity_category=ENTITY_CATEGORY_CONFIG
-    ).extend(cv.COMPONENT_SCHEMA),
+    # cv.Optional(CONF_KP_DISCHARGING): number.number_schema(
+    #     KpDischargingNumber,
+    #     entity_category=ENTITY_CATEGORY_CONFIG
+    # ).extend(cv.COMPONENT_SCHEMA),
     
-    cv.Optional(CONF_KI_DISCHARGING): number.number_schema(
-        KiDischargingNumber,
-        entity_category=ENTITY_CATEGORY_CONFIG
-    ).extend(cv.COMPONENT_SCHEMA),
+    # cv.Optional(CONF_KI_DISCHARGING): number.number_schema(
+    #     KiDischargingNumber,
+    #     entity_category=ENTITY_CATEGORY_CONFIG
+    # ).extend(cv.COMPONENT_SCHEMA),
     
-    cv.Optional(CONF_KD_DISCHARGING): number.number_schema(
-        KdDischargingNumber,
-        entity_category=ENTITY_CATEGORY_CONFIG
-    ).extend(cv.COMPONENT_SCHEMA),    
+    # cv.Optional(CONF_KD_DISCHARGING): number.number_schema(
+    #     KdDischargingNumber,
+    #     entity_category=ENTITY_CATEGORY_CONFIG
+    # ).extend(cv.COMPONENT_SCHEMA),    
 
     cv.Optional(CONF_OUTPUT_MIN_CHARGING): number.number_schema(
         OutputMinChargingNumber,
@@ -152,53 +182,80 @@ async def to_code(config):
         await cg.register_parented(n, dualpidpcm_component)
         cg.add(dualpidpcm_component.set_starting_battery_voltage_number(n))
 
-  if kp_charging_config := config.get(CONF_KP_CHARGING):
-        n = await number.new_number(
-            kp_charging_config, min_value=0.0, max_value=10.0, step=0.1
-        )
-        await cg.register_component(n, kp_charging_config)
-        await cg.register_parented(n, dualpidpcm_component)
-        cg.add(dualpidpcm_component.set_kp_charging_number(n)) 
-  
-  if ki_charging_config := config.get(CONF_KI_CHARGING):
-        n = await number.new_number(
-            ki_charging_config, min_value=0.0, max_value=10.0, step=0.1
-        )
-        await cg.register_component(n, ki_charging_config)
-        await cg.register_parented(n, dualpidpcm_component)
-        cg.add(dualpidpcm_component.set_ki_charging_number(n))
 
-  if kd_charging_config := config.get(CONF_KD_CHARGING):
+    if kp_config := config.get(CONF_KP):
         n = await number.new_number(
-            kd_charging_config, min_value=0.0, max_value=10.0, step=0.1
+            kp_config, min_value=0.0, max_value=10.0, step=0.1
         )
-        await cg.register_component(n, kd_charging_config)
+        await cg.register_component(n, kp_config)
         await cg.register_parented(n, dualpidpcm_component)
-        cg.add(dualpidpcm_component.set_kd_charging_number(n))
+        cg.add(dualpidpcm_component.set_kp_number(n)) 
+  
+  if ki_config := config.get(CONF_KI):
+        n = await number.new_number(
+            ki_config, min_value=0.0, max_value=10.0, step=0.1
+        )
+        await cg.register_component(n, ki_config)
+        await cg.register_parented(n, dualpidpcm_component)
+        cg.add(dualpidpcm_component.set_ki_number(n))
+
+  if kd_config := config.get(CONF_KD):
+        n = await number.new_number(
+            kd_config, min_value=0.0, max_value=10.0, step=0.1
+        )
+        await cg.register_component(n, kd_config)
+        await cg.register_parented(n, dualpidpcm_component)
+        cg.add(dualpidpcm_component.set_kd_number(n))
+
+
+
+  # if kp_charging_config := config.get(CONF_KP_CHARGING):
+  #       n = await number.new_number(
+  #           kp_charging_config, min_value=0.0, max_value=10.0, step=0.1
+  #       )
+  #       await cg.register_component(n, kp_charging_config)
+  #       await cg.register_parented(n, dualpidpcm_component)
+  #       cg.add(dualpidpcm_component.set_kp_charging_number(n)) 
+  
+  # if ki_charging_config := config.get(CONF_KI_CHARGING):
+  #       n = await number.new_number(
+  #           ki_charging_config, min_value=0.0, max_value=10.0, step=0.1
+  #       )
+  #       await cg.register_component(n, ki_charging_config)
+  #       await cg.register_parented(n, dualpidpcm_component)
+  #       cg.add(dualpidpcm_component.set_ki_charging_number(n))
+
+  # if kd_charging_config := config.get(CONF_KD_CHARGING):
+  #       n = await number.new_number(
+  #           kd_charging_config, min_value=0.0, max_value=10.0, step=0.1
+  #       )
+  #       await cg.register_component(n, kd_charging_config)
+  #       await cg.register_parented(n, dualpidpcm_component)
+  #       cg.add(dualpidpcm_component.set_kd_charging_number(n))
         
-  if kp_discharging_config := config.get(CONF_KP_DISCHARGING):
-        n = await number.new_number(
-            kp_discharging_config, min_value=0.0, max_value=10.0, step=0.1
-        )
-        await cg.register_component(n, kp_discharging_config)
-        await cg.register_parented(n, dualpidpcm_component)
-        cg.add(dualpidpcm_component.set_kp_discharging_number(n)) 
+  # if kp_discharging_config := config.get(CONF_KP_DISCHARGING):
+  #       n = await number.new_number(
+  #           kp_discharging_config, min_value=0.0, max_value=10.0, step=0.1
+  #       )
+  #       await cg.register_component(n, kp_discharging_config)
+  #       await cg.register_parented(n, dualpidpcm_component)
+  #       cg.add(dualpidpcm_component.set_kp_discharging_number(n)) 
   
-  if ki_discharging_config := config.get(CONF_KI_DISCHARGING):
-        n = await number.new_number(
-            ki_discharging_config, min_value=0.0, max_value=10.0, step=0.1
-        )
-        await cg.register_component(n, ki_discharging_config)
-        await cg.register_parented(n, dualpidpcm_component)
-        cg.add(dualpidpcm_component.set_ki_discharging_number(n))
+  # if ki_discharging_config := config.get(CONF_KI_DISCHARGING):
+  #       n = await number.new_number(
+  #           ki_discharging_config, min_value=0.0, max_value=10.0, step=0.1
+  #       )
+  #       await cg.register_component(n, ki_discharging_config)
+  #       await cg.register_parented(n, dualpidpcm_component)
+  #       cg.add(dualpidpcm_component.set_ki_discharging_number(n))
 
-  if kd_discharging_config := config.get(CONF_KD_DISCHARGING):
-        n = await number.new_number(
-            kd_discharging_config, min_value=0.0, max_value=10.0, step=0.1
-        )
-        await cg.register_component(n, kd_discharging_config)
-        await cg.register_parented(n, dualpidpcm_component)
-        cg.add(dualpidpcm_component.set_kd_discharging_number(n))        
+  # if kd_discharging_config := config.get(CONF_KD_DISCHARGING):
+  #       n = await number.new_number(
+  #           kd_discharging_config, min_value=0.0, max_value=10.0, step=0.1
+  #       )
+  #       await cg.register_component(n, kd_discharging_config)
+  #       await cg.register_parented(n, dualpidpcm_component)
+  #       cg.add(dualpidpcm_component.set_kd_discharging_number(n))        
 
   if output_min_charging_config := config.get(CONF_OUTPUT_MIN_CHARGING):
         n = await number.new_number(
