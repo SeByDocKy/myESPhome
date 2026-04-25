b2bea8d7956226e724a0eb279badc730c816eea6
SeByDocKy <sebydocky@hotmail.com>
2026-04-25 14:36:19 +0200

    Update dualpidpcm.cpp

--- components/dualpidpcm/dualpidpcm.cpp
+++ components/dualpidpcm/dualpidpcm.cpp
@@ -70,7 +70,7 @@ namespace dualpidpcm {
 
 
 	this->olb_  = this->oneutral_ - this->lb_;
-	this->ulb_  = this->oneutral_ - this->ub_;  
+	this->oub_  = this->oneutral_ - this->ub_;  
 	  
     this->pid_computed_callback_.call();
 
