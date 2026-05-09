2540b13f8340d5c689506ca5df017a40b00ca3dc
SeByDocKy <sebydocky@hotmail.com>
2026-05-09 22:27:21 +0200

    Update dualpid.cpp

--- components/dualpid/dualpid.cpp
+++ components/dualpid/dualpid.cpp
@@ -535,7 +535,7 @@ void DUALPIDComponent::pid_update() {
 
         case 1: {  // CHARGE — output ∈ [0, elb] → Oc ∈ [Ocmax, 0]
             // output = elb → Oc = 0,  output = 0 → Oc = max
-            if (this->output_discharging_ > HMS_MIN_LEVEL && this->current_activation_) {
+            if ( (this->output_discharging_ > HMS_MIN_LEVEL) && this->current_activation_) {
                 this->set_discharging_level(HMS_MIN_LEVEL);
                 this->output_discharging_ = HMS_MIN_LEVEL;
             }
@@ -548,7 +548,7 @@ void DUALPIDComponent::pid_update() {
 
         case 2: {  // DISCHARGE — output ∈ [eub, 1] → Od ∈ [0, Odmax]
             // Sécurité : forcer le R48 à 0 si encore actif
-            if (this->output_charging_ > 0.0f && this->current_activation_) {
+            if ( (this->output_charging_ > 0.0f) && this->current_activation_) {
                 this->set_charging_level(0.0f);
                 this->output_charging_ = 0.0f;
             }
