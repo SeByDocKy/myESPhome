4ecee5af9a355560a5bf9abaeccd8440288cc6a6
SeByDocKy <sebydocky@hotmail.com>
2026-05-09 19:30:47 +0200

    Update dualpid.cpp

--- components/dualpid/dualpid.cpp
+++ components/dualpid/dualpid.cpp
@@ -451,9 +451,16 @@ void DUALPIDComponent::pid_update() {
             // else if (!in_startup && this->error_ > (Pmin_ch * DEADBAND_FACTOR)) {
             //     this->current_mode_ = 0;   // → IDLE, qui basculera en DISCHARGE
             // }
-            if (!in_startup && this->error_ > (Pmin_ch * DEADBAND_FACTOR)) {
-               this->current_mode_ = 0;
-            }
+
+            
+            // if (!in_startup && this->error_ > (Pmin_ch * DEADBAND_FACTOR)) {
+            //    this->current_mode_ = 0;
+            // }
+
+            
+            if (!in_startup && (this->output_charging_ <= this->current_output_min_charging_ + 0.01f) &&  (this->error_ > (Pmin_ch * DEADBAND_FACTOR))) {
+                this->current_mode_ = 0;
+            }
             break;
 
         case 2:  // DISCHARGE
@@ -462,9 +469,17 @@ void DUALPIDComponent::pid_update() {
             // }
             // error franchement négatif → surplus suffisant → IDLE
             // else if (!in_startup && this->error_ < -(Pmin_ch * DEADBAND_FACTOR)) {
-            if (!in_startup && this->error_ < -(Pmin_ch * DEADBAND_FACTOR)) {    
-                this->current_mode_ = 0;   // → IDLE, qui basculera en CHARGE
-            }
+            
+            // if (!in_startup && this->error_ < -(Pmin_ch * DEADBAND_FACTOR)) {    
+            //     this->current_mode_ = 0;   // → IDLE, qui basculera en CHARGE
+            // }
+
+            if (!in_startup && (this->output_discharging_ <= this->current_output_min_discharging_ + 0.01f) && (this->error_ < -(Pmin_ch * DEADBAND_FACTOR))) {    
+                this->current_mode_ = 0;   // → IDLE, qui basculera en CHARGE
+            }
+
+
+            
             break;
     }
 
