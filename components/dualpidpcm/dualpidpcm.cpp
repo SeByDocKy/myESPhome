007a12f21f7f779edc62b89e370cbc207a77f26a
SeByDocKy <sebydocky@hotmail.com>
2026-07-15 09:05:54 +0200

    Update dualpidpcm.cpp

--- components/dualpidpcm/dualpidpcm.cpp
+++ components/dualpidpcm/dualpidpcm.cpp
@@ -445,9 +445,10 @@ void DUALPIDPCMComponent::pid_update() {
 
     if(this->current_feedforward_){
       if(trigger_ff && !in_startup && std::abs(pending_jump) > 0.001f){
-        // tmp += pending_jump;
-        tmp += (pending_jump / 2.0f);  
+        tmp += pending_jump;
+        // tmp += (pending_jump / 2.0f);  
         tmp = std::min(std::max(tmp, this->output_min_), this->output_max_);
+        this->previous_output_ = 0.0f;
       }   
         // if (!in_startup && !this->ff_locked_ && std::abs(pending_jump) > 0.001f) {
         //     tmp += pending_jump;
