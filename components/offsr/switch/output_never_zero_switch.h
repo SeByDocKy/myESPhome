1909d5a2e737ec2ab18c305e1a46283089b16132
SeByDocKy <sebydocky@hotmail.com>
2026-07-25 12:00:57 +0200

    Refactor namespace declaration in output_never_zero_switch.h

--- components/offsr/switch/output_never_zero_switch.h
+++ components/offsr/switch/output_never_zero_switch.h
@@ -3,8 +3,8 @@
 #include "esphome/components/switch/switch.h"
 #include "../offsr.h"
 
-namespace esphome {
-namespace offsr {
+namespace esphome::offsr {
+// namespace offsr {
 
 class OutputNeverZeroSwitch : public switch_::Switch, public Component, public Parented<OFFSRComponent> {
  public:
@@ -15,5 +15,5 @@ class OutputNeverZeroSwitch : public switch_::Switch, public Component, public P
   ESPPreferenceObject pref_;
 };
 
-}  // namespace offsr
-}  // namespace esphome
+// }  // namespace offsr
+}  // namespace esphome::offsr
