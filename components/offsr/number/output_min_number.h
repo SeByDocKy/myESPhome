f04772e7d871fd99ee4d691bb53787a537443a51
SeByDocKy <sebydocky@hotmail.com>
2026-07-25 12:18:20 +0200

    Refactor namespace for OutputMinNumber class
    Updated namespace declaration for OutputMinNumber class.

--- components/offsr/number/output_min_number.h
+++ components/offsr/number/output_min_number.h
@@ -3,8 +3,8 @@
 #include "esphome/components/number/number.h"
 #include "../offsr.h"
 
-namespace esphome {
-namespace offsr {
+namespace esphome::offsr {
+// namespace offsr {
 
 class OutputMinNumber : public number::Number, public Component, public Parented<OFFSRComponent> {
  public:
@@ -15,5 +15,5 @@ class OutputMinNumber : public number::Number, public Component, public Parented
   ESPPreferenceObject pref_;
 };
 
-}  // namespace offsr
-}  // namespace esphome
+// }  // namespace offsr
+}  // namespace esphome::offsr
