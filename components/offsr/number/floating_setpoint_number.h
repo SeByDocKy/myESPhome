6b2437b64e48e1882536c9ad471d39705c5628e4
SeByDocKy <sebydocky@hotmail.com>
2026-07-25 12:11:59 +0200

    Refactor namespace for FloatingSetpointNumber
    Updated namespace declaration for FloatingSetpointNumber class.

--- components/offsr/number/floating_setpoint_number.h
+++ components/offsr/number/floating_setpoint_number.h
@@ -3,8 +3,8 @@
 #include "esphome/components/number/number.h"
 #include "../offsr.h"
 
-namespace esphome {
-namespace offsr {
+namespace esphome::offsr {
+// namespace offsr {
 
 class FloatingSetpointNumber : public number::Number, public Component, public Parented<OFFSRComponent> {
  public:
@@ -15,5 +15,5 @@ class FloatingSetpointNumber : public number::Number, public Component, public P
   ESPPreferenceObject pref_;
 };
 
-}  // namespace offsr
-}  // namespace esphome
+// }  // namespace offsr
+}  // namespace esphome::offsr
