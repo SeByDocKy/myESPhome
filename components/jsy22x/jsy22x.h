4763bcc222c2ecb77535f47244ad1c597e9904d5
SeByDocKy <sebydocky@hotmail.com>
2026-07-25 10:19:42 +0200

    Update jsy22x.h

--- components/jsy22x/jsy22x.h
+++ components/jsy22x/jsy22x.h
@@ -30,7 +30,11 @@ class JSY22X : public PollingComponent, public modbus::ModbusDevice {
   
   void setup() override;  
   void update() override;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+  void on_response(std::span<const uint8_t> request_pdu, std::span<const uint8_t> response_pdu) override;
+  #else
   void on_modbus_data(const std::vector<uint8_t> &data) override;
+  #endif
   void dump_config() override;
   void reset_energy();
   void read_register04();
