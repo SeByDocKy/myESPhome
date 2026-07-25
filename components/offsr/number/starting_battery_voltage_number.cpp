b280fe59adb1167f6febcb2259c21d97877ad6ff
SeByDocKy <sebydocky@hotmail.com>
2026-07-25 12:21:01 +0200

    Refactor namespace declaration for StartingBatteryVoltageNumber

--- components/offsr/number/starting_battery_voltage_number.cpp
+++ components/offsr/number/starting_battery_voltage_number.cpp
@@ -1,7 +1,7 @@
 #include "starting_battery_voltage_number.h"
 
-namespace esphome {
-namespace offsr {
+namespace esphome::offsr {
+// namespace offsr {
 
 void StartingBatteryVoltageNumber::setup() {
   float value;
@@ -17,5 +17,5 @@ void StartingBatteryVoltageNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-}  // namespace offsr
-}  // namespace esphome
+// }  // namespace offsr
+}  // namespace esphome::offsr
