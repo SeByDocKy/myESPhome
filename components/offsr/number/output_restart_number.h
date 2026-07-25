fc2384ca101844da36ba4153d998209f7bf892f3
SeByDocKy <sebydocky@hotmail.com>
2026-07-25 12:19:19 +0200

    Update namespace structure in output_restart_number.h
    Refactor namespace declaration for OutputRestartNumber class.

--- components/offsr/number/output_restart_number.h
+++ components/offsr/number/output_restart_number.h
@@ -3,8 +3,8 @@
 #include "esphome/components/number/number.h"
 #include "../offsr.h"
 
-namespace esphome {
-namespace offsr {
+namespace esphome::offsr {
+// namespace offsr {
 
 class OutputRestartNumber : public number::Number, public Component, public Parented<OFFSRComponent> {
  public:
@@ -15,5 +15,5 @@ class OutputRestartNumber : public number::Number, public Component, public Pare
   ESPPreferenceObject pref_;
 };
 
-}  // namespace offsr
-}  // namespace esphome
+// }  // namespace offsr
+}  // namespace esphome::offsr
