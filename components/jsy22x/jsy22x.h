d285594585eb36d81ae879d23f45008cfa8d3904
SeByDocKy <sebydocky@hotmail.com>
2026-07-25 10:24:20 +0200

    Update jsy22x.h

--- components/jsy22x/jsy22x.h
+++ components/jsy22x/jsy22x.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/modbus/modbus.h"
+#include "esphome/core/version.h"
 
 #include <vector>
 
