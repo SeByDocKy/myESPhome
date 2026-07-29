95c2839110d1d7fdfddc651953c95dcf5ea475c8
SeByDocKy <sebydocky@hotmail.com>
2026-07-29 20:37:40 +0200

    Add files via upload

---
+++ components/dualpidpcm/number/self_consumption_number.cpp
@@ -0,0 +1,21 @@
+#include "feedforward_threshold_number.h"
+
+namespace esphome::dualpidpcm {
+// namespace dualpidpcm {
+
+void SelfConsumptionNumber::setup() {	
+	float value;
+	this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+	if (!this->pref_.load(&value)) value = this->parent_->get_self_consumption();
+	this->parent_->set_self_consumption(value);
+	this->publish_state(value);
+}
+
+void SelfConsumptionNumber::control(float value) {
+  this->publish_state(value);
+  this->parent_->set_self_consumption(value);
+  this->pref_.save(&value);
+}
+
+// }  // namespace dualpidpcm
+}  // namespace esphome
---
+++ components/dualpidpcm/number/self_consumption_number.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "esphome/components/number/number.h"
+#include "../dualpidpcm.h"
+
+namespace esphome::dualpidpcm {
+// namespace dualpidpcm {
+
+class SelfConsumptionNumber : public number::Number, public Component, public Parented<DUALPIDPCMComponent> {
+ public:
+  void setup() override;
+
+ protected:
+  void control(float value) override;
+  ESPPreferenceObject pref_;
+};
+
+// }  // namespace dualpidpcm
+}  // namespace esphome::
+
