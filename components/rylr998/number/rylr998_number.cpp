f8cb04661a1f7910b822cf1160ea7e7c75bd7392
SeByDocKy <sebydocky@hotmail.com>
2026-08-06 17:28:15 +0200

    Update rylr998_number.cpp

--- components/rylr998/number/rylr998_number.cpp
+++ components/rylr998/number/rylr998_number.cpp
@@ -10,7 +10,8 @@ static const char *const TAG = "rylr998.number";
 void RYLR998TxPowerNumber::setup() {
 
   uint8_t value;
-  this->pref_ = global_preferences->make_preference<uint8_t>(this->get_object_id_hash());
+  // this->pref_ = global_preferences->make_preference<uint8_t>(this->get_object_id_hash()); 
+  this->pref_ = global_preferences->make_preference<uint8_t>(this->get_entity_key());
   if (!this->pref_.load(&value)) value = this->parent_->get_tx_power();
   this->parent_->apply_tx_power(value);
   this->publish_state(value);
