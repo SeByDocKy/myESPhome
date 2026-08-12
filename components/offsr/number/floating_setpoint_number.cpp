47d01bef2a701d5625c5e03dcaa95d0a93a20935
SeByDocKy <sebydocky@hotmail.com>
2026-08-12 15:01:13 +0200

    Update floating_setpoint_number.cpp

--- components/offsr/number/floating_setpoint_number.cpp
+++ components/offsr/number/floating_setpoint_number.cpp
@@ -1,11 +1,15 @@
+#include "esphome/core/version.h"
 #include "floating_setpoint_number.h"
 
 namespace esphome::offsr {
-// namespace offsr {
 
 void FloatingSetpointNumber::setup() {
   float value;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 9, 0)
+  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&value)) value = this->parent_->get_floating_setpoint();
   this->parent_->set_floating_setpoint(value);
   this->publish_state(value);	
@@ -18,5 +22,4 @@ void FloatingSetpointNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-// }  // namespace offsr
 }  // namespace esphome::offsr
