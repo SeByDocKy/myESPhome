61d0ea56fde51ca86657039dd73a67eda3e19c1c
SeByDocKy <sebydocky@hotmail.com>
2026-08-12 15:18:25 +0200

    Update reverse_switch.cpp

--- components/offsr/switch/reverse_switch.cpp
+++ components/offsr/switch/reverse_switch.cpp
@@ -1,10 +1,15 @@
+#include "esphome/core/version.h"
 #include "reverse_switch.h"
 
 namespace esphome::offsr {
-// namespace offsr {
+
 void ReverseSwitch::setup() {
   bool state;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 9, 0)
+  this->pref_ = global_preferences->make_preference<bool>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<bool>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&state)) state = this->parent_->get_reverse();
   this->publish_state(state);
   this->parent_->set_reverse(state);
@@ -16,7 +21,6 @@ void ReverseSwitch::write_state(bool state) {
   this->pref_.save(&state);
 }
 
-// }  // namespace offsr
 }  // namespace esphome::offsr
 
 
