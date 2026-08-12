46e6e9b7af91d6f0dfbe9d302b4ac27684a11180
SeByDocKy <sebydocky@hotmail.com>
2026-08-12 19:17:16 +0200

    Update ESPHome version check in setup method

--- components/offsr/number/absorbing_setpoint_number.cpp
+++ components/offsr/number/absorbing_setpoint_number.cpp
@@ -5,7 +5,7 @@ namespace esphome::offsr {
 
 void AbsorbingSetpointNumber::setup() {
   float value;
-  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 9, 0)
   this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
   #else	
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
