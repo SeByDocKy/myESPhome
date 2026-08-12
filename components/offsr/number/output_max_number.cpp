2b02bc4c2302775d4ffc6f023de4f6557f49d97d
SeByDocKy <sebydocky@hotmail.com>
2026-08-12 15:09:26 +0200

    Update output_max_number.cpp

--- components/offsr/number/output_max_number.cpp
+++ components/offsr/number/output_max_number.cpp
@@ -1,11 +1,15 @@
+#include "esphome/core/version.h"
 #include "output_max_number.h"
 
 namespace esphome::offsr {
-// namespace offsr {
 
 void OutputMaxNumber::setup() {
   float value;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 9, 0)
+  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #else	
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif	
   if (!this->pref_.load(&value)) value = this->parent_->get_output_max();
   this->parent_->set_output_max(value);
   this->publish_state(value);	
@@ -18,5 +22,4 @@ void OutputMaxNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-// }  // namespace offsr
 }  // namespace esphome::offsr
