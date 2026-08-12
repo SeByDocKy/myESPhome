f5b6a999c1cd6ab7c924cac2839a602723acfaa1
SeByDocKy <sebydocky@hotmail.com>
2026-08-12 19:10:11 +0200

    Refactor DeltaIdleDischargingNumber setup method
    Updated preference handling to support version check for ESPHome.

--- components/dualpidpcm/number/delta_idle_discharging_number.cpp
+++ components/dualpidpcm/number/delta_idle_discharging_number.cpp
@@ -1,10 +1,15 @@
+#include "esphome/core/version.h"
 #include "delta_idle_discharging_number.h"
 
 namespace esphome::dualpidpcm {
 
 void DeltaIdleDischargingNumber::setup() {	
 	float value;
+	#if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+    this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+    #else
 	this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+	#endif
 	if (!this->pref_.load(&value)) value = this->parent_->get_delta_idle_discharging();
 	this->parent_->set_delta_idle_discharging(value);
 	this->publish_state(value);
