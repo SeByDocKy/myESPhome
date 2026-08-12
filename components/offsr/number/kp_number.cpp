3dd6383b9e8bf3e0f878449489f99e46b0325b99
SeByDocKy <sebydocky@hotmail.com>
2026-08-12 15:08:40 +0200

    Update kp_number.cpp

--- components/offsr/number/kp_number.cpp
+++ components/offsr/number/kp_number.cpp
@@ -4,7 +4,11 @@
 namespace esphome::offsr {
 void KpNumber::setup() {
   float value;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 9, 0)
+  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&value)) value = this->parent_->get_kp();
   this->parent_->set_kp(value);
   this->publish_state(value);
