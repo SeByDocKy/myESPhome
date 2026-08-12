8cd1470dcab4983170c9af3a6fc8ac8357c3e222
SeByDocKy <sebydocky@hotmail.com>
2026-08-12 19:16:59 +0200

    Refactor KpNumber setup for version compatibility

--- components/dualpidpcm/number/kp_number.cpp
+++ components/dualpidpcm/number/kp_number.cpp
@@ -1,10 +1,15 @@
+#include "esphome/core/version.h"
 #include "kp_number.h"
 
-namespace esphome {
-namespace dualpidpcm {
+namespace esphome::dualpidpcm {
+
 void KpNumber::setup() {
   float value;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 9, 0)
+  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&value)) value = this->parent_->get_kp();
   this->parent_->set_kp(value);
   this->publish_state(value);
@@ -16,6 +21,5 @@ void KpNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-}  // namespace dualpidpcm
 }  // namespace esphome
 
