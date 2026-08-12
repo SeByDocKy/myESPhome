6d5bbbb873875031289a2d4e3340cbbf8de8b3f5
SeByDocKy <sebydocky@hotmail.com>
2026-08-12 14:55:11 +0200

    Update charging_setpoint_number.cpp

--- components/offsr/number/charging_setpoint_number.cpp
+++ components/offsr/number/charging_setpoint_number.cpp
@@ -1,13 +1,16 @@
+#include "esphome/core/version.h"
 #include "charging_setpoint_number.h"
 
-namespace esphome::offsr {
-// namespace offsr {
 
-// static const char *const TAG = "OFFSR.number";
+namespace esphome::offsr {
 
 void ChargingSetpointNumber::setup() {	
 	float value;
+	#if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 9, 0)
+    this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+    #else
 	this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+	#endif
 	if (!this->pref_.load(&value)) value = this->parent_->get_charging_setpoint();
 	this->parent_->set_charging_setpoint(value);
 	this->publish_state(value);
@@ -19,5 +22,4 @@ void ChargingSetpointNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-// }  // namespace offsr
 }  // namespace esphome::offsr
