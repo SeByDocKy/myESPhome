167586f92351adc3c6ad08efc22bc6a8c3bc3e05
SeByDocKy <sebydocky@hotmail.com>
2026-08-12 15:16:12 +0200

    Refactor ManualOverrideSwitch for version compatibility
    Updated preference handling based on ESPHome version.

--- components/offsr/switch/manual_override_switch.cpp
+++ components/offsr/switch/manual_override_switch.cpp
@@ -1,11 +1,15 @@
+#include "esphome/core/version.h"
 #include "manual_override_switch.h"
 
 namespace esphome::offsr {
-// namespace offsr {
 
 void ManualOverrideSwitch::setup() {
   bool state;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 9, 0)
+  this->pref_ = global_preferences->make_preference<bool>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<bool>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&state)) state = this->parent_->get_manual_override();
   this->parent_->set_manual_override(state);
   this->publish_state(state);	
@@ -17,5 +21,4 @@ void ManualOverrideSwitch::write_state(bool state) {
   this->pref_.save(&state);
 }
 
-// }  // namespace offsr
 }  // namespace esphome::offsr
