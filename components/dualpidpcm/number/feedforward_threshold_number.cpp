82f8f3c0cb280d6abf0f09fd15e07f3c0eb6979e
SeByDocKy <sebydocky@hotmail.com>
2026-08-12 19:10:50 +0200

    Modify FeedforwardthresholdNumber for ESPHome versioning
    Updated preference initialization to support new ESPHome version.

--- components/dualpidpcm/number/feedforward_threshold_number.cpp
+++ components/dualpidpcm/number/feedforward_threshold_number.cpp
@@ -1,9 +1,14 @@
+#include "absorbing_setpoint_number.h"
 #include "feedforward_threshold_number.h"
 
 namespace esphome::dualpidpcm {
 void FeedforwardthresholdNumber::setup() {	
 	float value;
+	#if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+    this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+    #else
 	this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+	#endif
 	if (!this->pref_.load(&value)) value = this->parent_->get_feedforward_threshold();
 	this->parent_->set_feedforward_threshold(value);
 	this->publish_state(value);
