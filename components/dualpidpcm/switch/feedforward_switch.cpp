04485d7e2629914e7871e207e8c3b80f7fd65e68
SeByDocKy <sebydocky@hotmail.com>
2026-08-12 18:59:23 +0200

    Modify FeedforwardSwitch for ESPHome version compatibility
    Updated preference initialization to support new ESPHome version.

--- components/dualpidpcm/switch/feedforward_switch.cpp
+++ components/dualpidpcm/switch/feedforward_switch.cpp
@@ -4,7 +4,11 @@
 namespace esphome::dualpidpcm {
 void FeedforwardSwitch::setup() {
   bool state;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 9, 0)
+  this->pref_ = global_preferences->make_preference<bool>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<bool>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&state)) state = this->parent_->get_feedforward();
   this->publish_state(state);
   this->parent_->set_feedforward(state);
