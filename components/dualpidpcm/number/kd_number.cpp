f2a2e12f96744c17def40d817baec1f32d1c73fa
SeByDocKy <sebydocky@hotmail.com>
2026-08-12 19:11:42 +0200

    Update kd_number.cpp

--- components/dualpidpcm/number/kd_number.cpp
+++ components/dualpidpcm/number/kd_number.cpp
@@ -1,11 +1,15 @@
+#include "absorbing_setpoint_number.h"
 #include "kd_number.h"
 
-namespace esphome {
-namespace dualpidpcm {
+namespace esphome::dualpidpcm {
 
 void KdNumber::setup() {
   float value;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&value)) value = this->parent_->get_kd();
   this->parent_->set_kd(value);
   this->publish_state(value);
@@ -17,5 +21,4 @@ void KdNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-}  // namespace dualpidpcm
 }  // namespace esphome
