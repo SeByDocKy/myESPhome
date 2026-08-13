68cc4a930be69b1432ef00d4726997512b333010
SeByDocKy <sebydocky@hotmail.com>
2026-08-13 14:11:51 +0200

    Refactor PidModeSwitch to support versioning

--- components/dualpid/switch/pid_mode_switch.cpp
+++ components/dualpid/switch/pid_mode_switch.cpp
@@ -1,11 +1,15 @@
+#include "esphome/core/version.h"
 #include "pid_mode_switch.h"
 
-namespace esphome {
-namespace dualpid {
+namespace esphome::dualpid {
 	
 void PidModeSwitch::setup() {
   bool state;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 9, 0)
+  this->pref_ = global_preferences->make_preference<bool>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<bool>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&state)) state = this->parent_->get_pid_mode();
   this->parent_->set_pid_mode(state);
   this->publish_state(state);
@@ -16,6 +20,5 @@ void PidModeSwitch::write_state(bool state) {
   this->pref_.save(&state);
 }
 
-}  // namespace dualpid
 }  // namespace esphome
 
