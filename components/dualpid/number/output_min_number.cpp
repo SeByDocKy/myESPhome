1fc93d7a4c585acc9753cf4e2ad6c3d7a99ac9fa
SeByDocKy <sebydocky@hotmail.com>
2026-08-13 20:15:10 +0200

    Refactor OutputMinNumber setup for version compatibility

--- components/dualpid/number/output_min_number.cpp
+++ components/dualpid/number/output_min_number.cpp
@@ -1,11 +1,15 @@
+#include "esphome/core/version.h"
 #include "output_min_number.h"
 
-namespace esphome {
-namespace dualpid {
+namespace esphome::dualpid {
 
 void OutputMinNumber::setup() {
   float value;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&value)) value = this->parent_->get_output_min();
   this->parent_->set_output_min(value*0.01f);
   this->publish_state(value);
@@ -17,7 +21,6 @@ void OutputMinNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-}  // namespace dualpid
 }  // namespace esphome
 
 
