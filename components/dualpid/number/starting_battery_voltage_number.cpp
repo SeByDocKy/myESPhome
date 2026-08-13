efe76d57fed692bd5bb30828ee8235e31b6ca08c
SeByDocKy <sebydocky@hotmail.com>
2026-08-13 20:20:25 +0200

    Refactor StartingBatteryVoltageNumber for version compatibility
    Updated preference handling based on ESPHome version.

--- components/dualpid/number/starting_battery_voltage_number.cpp
+++ components/dualpid/number/starting_battery_voltage_number.cpp
@@ -1,11 +1,15 @@
+#include "esphome/core/version.h"
 #include "starting_battery_voltage_number.h"
 
-namespace esphome {
-namespace dualpid {
+namespace esphome::dualpid {
 
 void StartingBatteryVoltageNumber::setup() {
   float value;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&value)) value = this->parent_->get_starting_battery_voltage();
   this->parent_->set_starting_battery_voltage(value);
   this->publish_state(value);
@@ -17,5 +21,4 @@ void StartingBatteryVoltageNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-}  // namespace dualpid
 }  // namespace esphome
