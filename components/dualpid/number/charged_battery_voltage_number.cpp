866b50ad11dd4a3e30f7deb9e31c9967d963f1fa
SeByDocKy <sebydocky@hotmail.com>
2026-08-13 18:45:50 +0200

    Refactor ChargedBatteryVoltageNumber setup for versioning
    Updated ChargedBatteryVoltageNumber to use entity key for preferences if ESPHome version is 2026.8.0 or newer.

--- components/dualpid/number/charged_battery_voltage_number.cpp
+++ components/dualpid/number/charged_battery_voltage_number.cpp
@@ -1,11 +1,15 @@
+#include "esphome/core/version.h"
 #include "charged_battery_voltage_number.h"
 
-namespace esphome {
-namespace dualpid {
+namespace esphome::dualpid {
 
 void ChargedBatteryVoltageNumber::setup() {
   float value;
-  this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #else
+	this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+	#endif
   if (!this->pref_.load(&value)) value = this->parent_->get_charged_battery_voltage();
   this->parent_->set_charged_battery_voltage(value);
   this->publish_state(value);	
@@ -17,5 +21,4 @@ void ChargedBatteryVoltageNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-}  // namespace dualpid
 }  // namespace esphome
