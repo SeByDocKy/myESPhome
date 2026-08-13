254b575c27be5d2e98dbd65018bceccf68f119a8
SeByDocKy <sebydocky@hotmail.com>
2026-08-13 19:23:15 +0200

    Update kd_charging_number.h

--- components/dualpid/number/kd_charging_number.h
+++ components/dualpid/number/kd_charging_number.h
@@ -3,8 +3,7 @@
 #include "esphome/components/number/number.h"
 #include "../dualpid.h"
 
-namespace esphome {
-namespace dualpid {
+namespace esphome::dualpid {
 
 class KdChargingNumber : public number::Number, public Component, public Parented<DUALPIDComponent> {
  public:
@@ -15,5 +14,4 @@ class KdChargingNumber : public number::Number, public Component, public Parente
   ESPPreferenceObject pref_;
 };
 
-}  // namespace dualpid
 }  // namespace esphome
