901c19d48b67a7c59aefd50016483cf4a2622a19
SeByDocKy <sebydocky@hotmail.com>
2026-08-13 18:47:19 +0200

    Refactor ChargingEpointNumber for version compatibility

--- components/dualpid/number/charging_epoint_number.cpp
+++ components/dualpid/number/charging_epoint_number.cpp
@@ -1,11 +1,15 @@
+#include "esphome/core/version.h"
 #include "charging_epoint_number.h"
 
-namespace esphome {
-namespace dualpid {
+namespace esphome::dualpid {
 
 void ChargingEpointNumber::setup() {	
 	float value;
+	#if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+    this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+    #else
 	this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+	#endif
 	if (!this->pref_.load(&value)) value = this->parent_->get_charging_epoint();
 	this->parent_->set_charging_epoint(value*0.01f);
 	this->publish_state(value);
@@ -17,7 +21,6 @@ void ChargingEpointNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-}  // namespace dualpid
 }  // namespace esphome
 
 
