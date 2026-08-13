dacae13addbce15d5221815499c9bc2c65b0879e
SeByDocKy <sebydocky@hotmail.com>
2026-08-13 20:04:24 +0200

    Refactor OutputMaxDischargingNumber for version compatibility
    Updated preference handling based on ESPHome version.

--- components/dualpid/number/output_max_discharging_number.cpp
+++ components/dualpid/number/output_max_discharging_number.cpp
@@ -1,11 +1,15 @@
+#include "esphome/core/version.h"
 #include "output_max_discharging_number.h"
 
-namespace esphome {
-namespace dualpid {
+namespace esphome::dualpid {
 
 void OutputMaxDischargingNumber::setup() {
   float value;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&value)) value = this->parent_->get_output_max_discharging();
   this->parent_->set_output_max_discharging(value*0.01f);
   this->publish_state(value);
@@ -17,7 +21,6 @@ void OutputMaxDischargingNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-}  // namespace dualpid
 }  // namespace esphome
 
 
