44971c0878f9ff1bfa3d29b5eb993836eaf06326
SeByDocKy <sebydocky@hotmail.com>
2026-08-13 19:26:06 +0200

    Modify KdDischargingNumber setup for version compatibility
    Updated preference handling based on ESPHome version.

--- components/dualpid/number/kd_discharging_number.cpp
+++ components/dualpid/number/kd_discharging_number.cpp
@@ -5,7 +5,11 @@ namespace esphome::dualpid {
 
 void KdDischargingNumber::setup() {
   float value;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&value)) value = this->parent_->get_kd_discharging();
   this->parent_->set_kd_discharging(value);
   this->publish_state(value);
