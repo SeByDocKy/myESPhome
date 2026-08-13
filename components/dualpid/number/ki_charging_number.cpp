0ec3df3c1176a9dcd8594b6cd500bdce777dceb9
SeByDocKy <sebydocky@hotmail.com>
2026-08-13 19:28:25 +0200

    Refactor KiChargingNumber for version compatibility
    Updated preference handling to support new versioning.

--- components/dualpid/number/ki_charging_number.cpp
+++ components/dualpid/number/ki_charging_number.cpp
@@ -1,10 +1,15 @@
+#include "esphome/core/version.h"
 #include "ki_charging_number.h"
 
-namespace esphome {
-namespace dualpid {
+namespace esphome::dualpid {
+
 void KiChargingNumber::setup() {
   float value;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&value)) value = this->parent_->get_ki_charging();
   this->parent_->set_ki_charging(value);
   this->publish_state(value);	
@@ -16,5 +21,4 @@ void KiChargingNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-}  // namespace dualpid
 }  // namespace esphome
