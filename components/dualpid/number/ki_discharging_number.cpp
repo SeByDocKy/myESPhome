b54e78c933551783404a770c1b98afbbb1f44f23
SeByDocKy <sebydocky@hotmail.com>
2026-08-13 19:40:04 +0200

    Refactor KiDischargingNumber setup for version compatibility
    Updated setup method to use entity key for preferences in newer versions.

--- components/dualpid/number/ki_discharging_number.cpp
+++ components/dualpid/number/ki_discharging_number.cpp
@@ -1,10 +1,15 @@
+#include "esphome/core/version.h"
 #include "ki_discharging_number.h"
 
-namespace esphome {
-namespace dualpid {
+namespace esphome::dualpid {
+
 void KiDischargingNumber::setup() {
   float value;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&value)) value = this->parent_->get_ki_discharging();
   this->parent_->set_ki_discharging(value);
   this->publish_state(value);	
@@ -16,6 +21,5 @@ void KiDischargingNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-}  // namespace dualpid
 }  // namespace esphome
 
