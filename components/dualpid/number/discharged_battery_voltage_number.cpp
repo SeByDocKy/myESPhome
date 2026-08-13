cf86fa5fb8f2ccfa3c2fb8769da2cb4520cb357d
SeByDocKy <sebydocky@hotmail.com>
2026-08-13 18:49:08 +0200

    Refactor DischargedBatteryVoltageNumber methods

--- components/dualpid/number/discharged_battery_voltage_number.cpp
+++ components/dualpid/number/discharged_battery_voltage_number.cpp
@@ -8,8 +8,8 @@ void DischargedBatteryVoltageNumber::setup() {
   #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
   this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
   #else
-	this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
-	#endif
+  this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&value)) value = this->parent_->get_discharged_battery_voltage();
   this->parent_->set_discharged_battery_voltage(value);
   this->publish_state(value);
