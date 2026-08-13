7a11180a3902506284c0156cabe47d518942f09d
SeByDocKy <sebydocky@hotmail.com>
2026-08-13 19:44:06 +0200

    Update KpDischargingNumber setup for version compatibility

--- components/dualpid/number/kp_discharging_number.cpp
+++ components/dualpid/number/kp_discharging_number.cpp
@@ -1,11 +1,15 @@
+#include "esphome/core/version.h"
 #include "kp_discharging_number.h"
 
-namespace esphome {
-namespace dualpid {
+namespace esphome::dualpid {
 
 void KpDischargingNumber::setup() {
   float value;
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #else
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&value)) value = this->parent_->get_kp_discharging();
   this->parent_->set_kp_discharging(value);
   this->publish_state(value);
@@ -17,7 +21,6 @@ void KpDischargingNumber::control(float value) {
   this->pref_.save(&value);
 }
 
-}  // namespace dualpid
 }  // namespace esphome
 
 
