c235186656d7ea3e21734a1fa66f79b76beb854b
SeByDocKy <sebydocky@hotmail.com>
2026-08-14 18:00:55 +0200

    Update KpDischargingNumber for ESPHome version check

--- components/dualpid/number/kp_discharging_number.cpp
+++ components/dualpid/number/kp_discharging_number.cpp
@@ -5,9 +5,9 @@ namespace esphome::dualpid {
 
 void KpDischargingNumber::setup() {
   float value;
-  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 9, 0)
   // this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
-	this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
   #else
   this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
   #endif
