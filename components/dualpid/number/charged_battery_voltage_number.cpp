21db95acb7a4fa16c4975b5a86c09e3556f0c210
SeByDocKy <sebydocky@hotmail.com>
2026-08-14 17:55:45 +0200

    Update ChargedBatteryVoltageNumber setup method

--- components/dualpid/number/charged_battery_voltage_number.cpp
+++ components/dualpid/number/charged_battery_voltage_number.cpp
@@ -5,11 +5,12 @@ namespace esphome::dualpid {
 
 void ChargedBatteryVoltageNumber::setup() {
   float value;
-  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 8, 0)
-  this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  #if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 9, 0)
+  // this->pref_ = global_preferences->make_preference<float>(this->get_entity_key());
+  this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
   #else
-	this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
-	#endif
+  this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
+  #endif
   if (!this->pref_.load(&value)) value = this->parent_->get_charged_battery_voltage();
   this->parent_->set_charged_battery_voltage(value);
   this->publish_state(value);	
